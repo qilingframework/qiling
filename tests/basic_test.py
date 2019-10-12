65db89732aa2f6b336ed2b7f0d8fb96af2ead30d
klks <klks84@gmail.com>
2019-10-12 06:32:07 -0700

    Added check for arch. Introducing tests

---
+++ tests/basic_test.py
@@ -0,0 +1,11 @@
+import unittest
+import sys
+sys.path.append("..")
+from qiling import *
+
+class BasicTest(unittest.TestCase):
+    def test_1(self):
+        pass
+        
+if __name__ == "__main__":
+    unittest.main()
