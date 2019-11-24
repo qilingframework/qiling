cf54d15336aeac794cf509a5b08b55b940d423e1
xwings <kj@xandora.net>
2019-11-24 23:05:07 +0800

    deleted some duplicated examples and add more test

--- tests/test_elf.py
+++ tests/test_elf.py
@@ -12,6 +12,32 @@ class ELFTest(unittest.TestCase):
     def test_elf_linux_x8664(self):
         ql = Qiling(["../examples/rootfs/x8664_linux/bin/tester","1234test", "12345678", "bin/x8664_hello"],  "../examples/rootfs/x8664_linux", output="debug")
         ql.run()
+
+
+    def test_elf_linux_x86(self):
+        ql = Qiling(["../examples/rootfs/x86_linux/bin/x86_hello"], "../examples/rootfs/x86_linux", output="debug")
+        ql.run()
+
+
+    def test_elf_linux_arm(self):     
+        ql = Qiling(["../examples/rootfs/arm_linux/bin/arm_hello"], "../examples/rootfs/arm_linux", output = "default")
+        ql.run()
+
+
+    def test_elf_linux_arm64(self):     
+        ql = Qiling(["../examples/rootfs/arm64_linux/bin/arm64_hello"], "../examples/rootfs/arm64_linux", output = "default")
+        ql.run()
+
+
+    def test_elf_linux_mips32el(self):     
+        ql = Qiling(["../examples/rootfs/mips32el_linux/bin/mips32el_hello"], "../examples/rootfs/mips32el_linux")
+        ql.run()  
         
+
+    def test_elf_freebsd_x8664(self):     
+        ql = Qiling(["../examples/rootfs/x8664_freebsd/bin/x8664_hello_asm"], "../examples/rootfs/x8664_freebsd", output = "default")
+        ql.run()  
+
+
 if __name__ == "__main__":
     unittest.main()
