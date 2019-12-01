25fa21ac995e1f6f5b51247666ab01276f84abd2
Quang Nguyen <quangnh89@gmail.com>
2019-12-02 00:09:35 +0700

    update Contributors

--- examples/adcache_x86_windows_debug.py
+++ examples/adcache_x86_windows_debug.py
@@ -3,12 +3,7 @@
 # Cross Platform and Multi Architecture Advanced Binary Emulation Framework
 # Built on top of Unicorn emulator (www.unicorn-engine.org)
 #
-# LAU kaijern (xwings) <kj@qiling.io>
-# NGUYEN Anh Quynh <aquynh@gmail.com>
-# DING tianZe (D1iv3) <dddliv3@gmail.com>
-# SUN bowen (w1tcher) <w1tcher.bupt@gmail.com>
-# CHEN huitao (null) <null@qiling.io>
-# YU tong (sp1ke) <spikeinhouse@gmail.com>
+# Nguyen Hong Quang (quangnh89) <quangnh89@gmail.com>
 
 import sys
 
@@ -19,4 +14,3 @@ if __name__ == "__main__":
     sc = open("shellcodes/win32_https_download.bin",'rb').read()
     ql = Qiling(shellcoder = sc, archtype = "x86", ostype = "windows", rootfs="rootfs/x86_windows", output = "debug")
     ql.run()
-
