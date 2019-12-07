f62d0882d715557604f4ea45620fc0fd7787ae08
xwings <kj@xandora.net>
2019-12-07 21:06:19 +0800

    work around for mips32el static test

--- tests/test_elf.py
+++ tests/test_elf.py
@@ -3,14 +3,22 @@
 # Cross Platform and Multi Architecture Advanced Binary Emulation Framework
 # Built on top of Unicorn emulator (www.unicorn-engine.org) 
 
-import string
-import random
-import sys,unittest
+import sys,unittest, subprocess, string, random
 sys.path.append("..")
 from qiling import *
 from qiling.exception import *
 
 class ELFTest(unittest.TestCase):
+
+    def test_elf_freebsd_x8664(self):     
+        ql = Qiling(["../examples/rootfs/x8664_freebsd/bin/x8664_hello_asm"], "../examples/rootfs/x8664_freebsd", output = "disasm")
+        ql.run()  
+
+
+    # Not Stable, not suitable to use it as test
+    # def test_multithread_elf_linux_x86(self):
+    #    ql = Qiling(["../examples/rootfs/x86_linux/bin/x86_multithreading"], "../examples/rootfs/x86_linux", output="debug")
+    #    ql.run()
 
     def test_elf_linux_x8664(self):
         ql = Qiling(["../examples/rootfs/x8664_linux/bin/tester","1234test", "12345678", "bin/x8664_hello"],  "../examples/rootfs/x8664_linux", output="debug")
@@ -42,22 +50,17 @@ class ELFTest(unittest.TestCase):
         ql.run()    
 
 
-    def test_elf_linux_arm64(self):     
+    def test_elf_linux_arm64(self):
         ql = Qiling(["../examples/rootfs/arm64_linux/bin/arm64_hello"], "../examples/rootfs/arm64_linux", output = "debug")
         ql.run()
 
 
-    def test_elf_linux_arm64_static(self):     
+    def test_elf_linux_arm64_static(self):    
         ql = Qiling(["../examples/rootfs/arm64_linux/bin/arm64_hello_static"], "../examples/rootfs/arm64_linux", output = "default")
         ql.run()
 
 
-    def test_elf_linux_mips32el_static(self):  
-        ql = Qiling(["../examples/rootfs/mips32el_linux/bin/mips32el_hello_static"], "../examples/rootfs/mips32el_linux")
-        ql.run()    
-
-
-    def test_elf_linux_mips32el(self):  
+    def test_elf_linux_mips32el(self):
         def random_generator(size=6, chars=string.ascii_uppercase + string.digits):
             return ''.join(random.choice(chars) for x in range(size))
 
@@ -65,15 +68,12 @@ class ELFTest(unittest.TestCase):
         ql.run()  
 
 
-    def test_elf_freebsd_x8664(self):     
-        ql = Qiling(["../examples/rootfs/x8664_freebsd/bin/x8664_hello_asm"], "../examples/rootfs/x8664_freebsd", output = "disasm")
-        ql.run()  
-
-
-    # # Not Stable, not suitable to use it as test
-    # #def test_multithread_elf_linux_x86(self):
-    # #    ql = Qiling(["../examples/rootfs/x86_linux/bin/x86_multithreading"], "../examples/rootfs/x86_linux", output="debug")
-    # #    ql.run()
+    def test_elf_linux_mips32el_static(self): 
+        create = [sys.executable, 'hello_mips32el_linux_debug.py']
+        try:
+            subprocess.check_output(create,stderr=subprocess.STDOUT)
+        except subprocess.CalledProcessError as e:    
+            raise RuntimeError("command '{}' return with error (code {}): {}".format(e.cmd, e.returncode, e.output))    
 
 
 if __name__ == "__main__":
