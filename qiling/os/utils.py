92209ff066d4c986dde842f4a5d40d1f61f2e624
Jhe <ucg.jhe@gmail.com>
2019-12-21 20:03:33 +0800

    fix open_flags issue for mips32el

--- qiling/os/utils.py
+++ qiling/os/utils.py
@@ -428,7 +428,7 @@ def open_flag_mapping(flags, ql):
         f = mac_open_flags
         t = linux_open_flags
 
-    if ql.platform == None or ql.platform == ql.ostype:
+    if ql.platform == None or ql.platform == ql.ostype and ql.arch not in (QL_MIPS32EL, QL_ARM, QL_ARM64):
         return flags
         
     return flag_mapping(flags, open_flags_name, f, t)
