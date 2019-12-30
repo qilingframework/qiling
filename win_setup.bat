e4b903a2993a39dda3f056815806786f6f6f248d
chfl4gs <55137784+chfl4gs@users.noreply.github.com>
2019-12-30 21:33:08 +0800

    Update win_setup.bat

--- win_setup.bat
+++ win_setup.bat
@@ -6,7 +6,7 @@ md registry
 REM 
 REM  Registry
 REM 
-xcopy /f /y C:\Users\Default\NTUSER.DAT registry\NTUSER.DAT
+echo f | xcopy /f /y C:\Users\Default\NTUSER.DAT registry\NTUSER.DAT
 reg save hklm\system registry\SYSTEM
 reg save hklm\security registry\SECURITY
 reg save hklm\software registry\SOFTWARE
