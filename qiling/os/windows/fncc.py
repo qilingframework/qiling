404665ba46924c271a64b010e5eacce74c826d5a
Jeremy Humble <jeremy.humble@morphick.com>
2020-01-08 16:06:30 -0500

    Added another missing import

--- qiling/os/windows/fncc.py
+++ qiling/os/windows/fncc.py
@@ -5,6 +5,7 @@
 import struct
 from unicorn.x86_const import *
 from qiling.os.utils import *
+from qiling.os.fncc import *
 from qiling.os.windows.utils import *
 from qiling.arch.filetype import *
 
