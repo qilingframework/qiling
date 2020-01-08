73beb9fb876c3fbf8cb86c3abf9706ed307913c6
Jeremy Humble <jeremy.humble@morphick.com>
2020-01-08 13:56:00 -0500

    Added missing fncc STDCALL/CDECL imports to windows hooks modules. And fixed x64 fastcall parameter parsing

--- qiling/os/windows/dlls/advapi32.py
+++ qiling/os/windows/dlls/advapi32.py
@@ -4,6 +4,7 @@
 # Built on top of Unicorn emulator (www.unicorn-engine.org) 
 import struct
 from qiling.os.windows.fncc import *
+from qiling.os.fncc import *
 from qiling.os.windows.utils import *
 from qiling.os.windows.handle import *
 from qiling.os.windows.const import *
