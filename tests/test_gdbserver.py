8dd987df3a52c8f042acefe50f7d9f88fc65a7b1
Wu ChenXu <34511237+kabeor@users.noreply.github.com>
2020-02-18 18:20:51 +0800

    Update test_gdbserver.py

--- tests/test_gdbserver.py
+++ tests/test_gdbserver.py
@@ -1,17 +1,22 @@
+#!/usr/bin/env python3
+
+import sys,unittest
+sys.path.append("..")
 from qiling import *
 
+class dbgTest(unittest.TestCase):
+    def test_gdb_x86(self):
+        ql = Qiling(["./examples/rootfs/x86_windows/bin/x86_hello.exe"], "./examples/rootfs/x86_windows/bin")
+        ql.gdb = ":9999"
+        ql.run()
+        del ql
 
-def test_gdb_x86():
-    ql = Qiling(["./examples/rootfs/x86_windows/bin/x86_hello.exe"], "./examples/rootfs/x86_windows/bin")
-    ql.gdb = ":9999"
-    ql.run()
-
-def test_gdb_x8664():
-    ql = Qiling(["./examples/rootfs/x8664_windows/bin/x8664_hello.exe"], "./examples/rootfs/x8664_windows/bin")
-    ql.gdb = ":9999"
-    ql.run()
+    def test_gdb_x8664(self):
+        ql = Qiling(["./examples/rootfs/x8664_windows/bin/x8664_hello.exe"], "./examples/rootfs/x8664_windows/bin")
+        ql.gdb = ":9999"
+        ql.run()
+        del ql
 
 
 if __name__ == "__main__":
-    test_gdb_x86()
-    test_gdb_x8664()
+    unittest.main()
