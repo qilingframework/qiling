e7182cf80397dc668c198c25767d87648973edf5
Quang Nguyễn <quangnh89@gmail.com>
2020-03-04 01:04:49 +0700

    Due to antivirus softwares scan this shellcode and detect it as malware, we should use create password-protected archive to protect it. Password to open this file: infected

--- examples/adcache_x86_windows_debug.py
+++ examples/adcache_x86_windows_debug.py
@@ -4,11 +4,15 @@
 # Built on top of Unicorn emulator (www.unicorn-engine.org)
 
 import sys
+from zipfile import ZipFile
 
 sys.path.append("..")
 from qiling import *
 
 if __name__ == "__main__":
-    sc = open("shellcodes/win32_https_download.bin",'rb').read()
-    ql = Qiling(shellcoder = sc, archtype = "x86", ostype = "windows", rootfs="rootfs/x86_windows", output = "debug")
-    ql.run()
+    zip_reader = ZipFile("shellcodes/win32_https_download.zip")
+    with zip_reader.open('win32_https_download.bin', 'r', b'infected') as f:
+        sc = f.read()
+        ql = Qiling(shellcoder = sc, archtype = "x86", ostype = "windows",
+                    rootfs="rootfs/x86_windows", output = "debug")
+        ql.run()
