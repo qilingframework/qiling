ae6c0f73e5c1d76100d9ed9e150e9d8c158c348f
kabeor <1597915586@qq.com>
2020-03-06 12:12:23 +0800

    update gdbserver-mips32 support

--- qiling/gdbserver/gdblistener.py
+++ qiling/gdbserver/gdblistener.py
@@ -139,10 +139,13 @@ class GDBSession(object):
                         tmp = self.ql.addr_to_str(r)
                         s += tmp
 
-                if self.ql.arch == QL_MIPS32EL:
+                if self.ql.arch in (QL_MIPS32, QL_MIPS32EL):
                     for reg in registers_mips[:38]:
                         r = self.ql.uc.reg_read(reg)
-                        tmp = self.ql.addr_to_str(r)
+                        if self.ql.arch == QL_MIPS32:
+                            tmp = self.ql.addr_to_str(r, endian="little")
+                        else:
+                            tmp = self.ql.addr_to_str(r)
                         s += tmp
 
                 self.send(s)
@@ -183,7 +186,7 @@ class GDBSession(object):
                         self.ql.uc.reg_write(registers_arm64[count], reg_data)
                         count += 1
 
-                elif self.ql.arch == QL_MIPS32EL:
+                elif self.ql.arch in (QL_MIPS32, QL_MIPS32EL):
                     for i in range(0, len(subcmd), 8):
                         reg_data = subcmd[i:i+7]
                         reg_data = int(reg_data, 16)
@@ -265,13 +268,15 @@ class GDBSession(object):
                             reg_value = 0
                             reg_value = self.ql.addr_to_str(reg_value)
 
-                    if self.ql.arch == QL_MIPS32EL:
+                    if self.ql.arch in (QL_MIPS32, QL_MIPS32EL):
                         if reg_index <= 37:
                             reg_value = self.ql.uc.reg_read(registers_mips[reg_index - 1])
                         else:
                             reg_value = 0
-                        reg_value = self.ql.addr_to_str(reg_value)
-
+                        if self.ql.arch == QL_MIPS32:
+                            reg_value = self.ql.addr_to_str(reg_value, endian="little")
+                        else:
+                            reg_value = self.ql.addr_to_str(reg_value)
                     self.send(reg_value)
                 except:
                     self.close()
@@ -306,9 +311,12 @@ class GDBSession(object):
                     reg_data = int.from_bytes(struct.pack('<Q', reg_data), byteorder='big')
                     self.ql.uc.reg_write(registers_arm64[reg_index], reg_data)
 
-                if self.ql.arch == QL_MIPS32EL:
+                if self.ql.arch in (QL_MIPS32, QL_MIPS32EL):
                     reg_data = int(reg_data, 16)
-                    reg_data = int.from_bytes(struct.pack('<I', reg_data), byteorder='big')
+                    if self.ql.arch == QL_MIPS32:
+                        reg_data = int.from_bytes(struct.pack('<I', reg_data), byteorder='little')
+                    else:
+                        reg_data = int.from_bytes(struct.pack('<I', reg_data), byteorder='big')
                     self.ql.uc.reg_write(registers_mips[reg_index], reg_data)
 
                 self.ql.nprint("gdb> write to register %x with %x" % (registers_x8664[reg_index], reg_data))
