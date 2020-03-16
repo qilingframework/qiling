9713bf663241d5b5bf0baa38364755f928560463
0ssigeno <simone.berni2@studio.unibo.it>
2020-03-16 10:41:37 +0100

    Implemented IsWow64Process

--- qiling/os/windows/dlls/kernel32.py
+++ qiling/os/windows/dlls/kernel32.py
@@ -1516,3 +1516,20 @@ def hook_LCMapStringEx(ql, address, params):
     return 1
 
 
+# BOOL IsWow64Process(
+#   HANDLE hProcess,
+#   PBOOL  Wow64Process
+# );
+@winapi(cc=STDCALL, params={
+    "hProcess": HANDLE,
+    "Wow64Process": POINTER
+})
+def hook_IsWow64Process(ql, address, params):
+    pointer = params["Wow64Process"]
+    false=0x0.to_bytes(length=ql.pointersize, byteorder='little')
+    true=0x1.to_bytes(length=ql.pointersize, byteorder='little')
+    if ql.archbit == 32:
+        ql.uc.mem_write(pointer, false)
+    else:
+        raise QlErrorNotImplemented("[!] API not implemented")
+    return 1
