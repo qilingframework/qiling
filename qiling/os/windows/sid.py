d0f2346fae7b33002bc045d5a7732250bfb27129
0ssigeno <simone.berni2@studio.unibo.it>
2020-03-27 13:14:48 +0100

    Fixed struct

--- qiling/os/windows/sid.py
+++ qiling/os/windows/sid.py
@@ -27,14 +27,13 @@ class Sid:
     # https://docs.microsoft.com/it-it/windows/win32/api/winnt/ns-winnt-sid
     # Identf Authority
     # https://docs.microsoft.com/en-us/openspecs/windows_protocols/ms-dtyp/c6ce4275-3d90-4890-ab3a-514745e4637e
-    # TODO add subauthority
     def __init__(self, ql):
+        # TODO find better documentation
         self.struct = {
-            "Revision": 0x12.to_bytes(length=1, byteorder="little"),  # ADD
-            "SubAuthorityCount": 0x2.to_bytes(length=1, byteorder="little"),  # Administrator and user
-            "IdentifierAuthority": 0x0.to_bytes(length=6, byteorder="little"),
-            "SubAuthority": 0x12345678.to_bytes(length=ql.pointersize, byteorder="little") + 0x87654321.to_bytes(
-                length=ql.pointersize, byteorder="little")
+            "Revision": 0x1.to_bytes(length=1, byteorder="little"),  # ADD
+            "SubAuthorityCount": 0x1.to_bytes(length=1, byteorder="little"),
+            "IdentifierAuthority": 0x5.to_bytes(length=6, byteorder="little"),
+            "SubAuthority": 0x12345678.to_bytes(length=ql.pointersize, byteorder="little")
         }
         values = b"".join(self.struct.values())
         self.addr = ql.heap.mem_alloc(len(values))
