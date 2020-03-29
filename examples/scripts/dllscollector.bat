84df7e37f38f4540fe2c981df653c22e6dc53ab3
Simone Berni <simone.berni2@studio.unibo.it>
2020-03-29 21:41:23 +0200

    Correct caps on XpsPrint.dll

--- examples/scripts/dllscollector.bat
+++ examples/scripts/dllscollector.bat
@@ -50,7 +50,7 @@ if exist %WINDIR%\SysWOW64\vcruntime140.dll xcopy /f /y %WINDIR%\SysWOW64\vcrunt
 if exist %WINDIR%\SysWOW64\winhttp.dll xcopy /f /y %WINDIR%\SysWOW64\winhttp.dll "examples\rootfs\x86_windows\Windows\SysWOW64\"
 if exist %WINDIR%\SysWOW64\wininet.dll xcopy /f /y %WINDIR%\SysWOW64\wininet.dll "examples\rootfs\x86_windows\Windows\SysWOW64\"
 if exist %WINDIR%\SysWOW64\ws2_32.dll xcopy /f /y %WINDIR%\SysWOW64\ws2_32.dll "examples\rootfs\x86_windows\Windows\SysWOW64\"
-if exist %WINDIR%\SysWOW64\xpsprint.dll xcopy /f /y %WINDIR%\SysWOW64\xpsprint.dll "examples\rootfs\x86_windows\Windows\SysWOW64\"
+if exist %WINDIR%\SysWOW64\XpsPrint.dll xcopy /f /y %WINDIR%\SysWOW64\XpsPrint.dll "examples\rootfs\x86_windows\Windows\SysWOW64\"
 if exist %WINDIR%\SysWOW64\msvcr120_clr0400.dll echo f | xcopy /f /y %WINDIR%\SysWOW64\msvcr120_clr0400.dll "examples\rootfs\x86_windows\Windows\SysWOW64\msvcr110.dll"
 if exist %WINDIR%\SysWOW64\downlevel\api-ms-win-crt-stdio-l1-1-0.dll xcopy /f /y %WINDIR%\SysWOW64\downlevel\api-ms-win-crt-stdio-l1-1-0.dll "examples\rootfs\x86_windows\Windows\SysWOW64\"
 if exist %WINDIR%\SysWOW64\downlevel\api-ms-win-crt-runtime-l1-1-0.dll xcopy /f /y %WINDIR%\SysWOW64\downlevel\api-ms-win-crt-runtime-l1-1-0.dll "examples\rootfs\x86_windows\Windows\SysWOW64\"
