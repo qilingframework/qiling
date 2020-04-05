f7e67bfc722383297156ee7d1653d25cf54f8da7
w1tcher <w1tcher@ubuntu>
2020-04-06 00:12:20 +0800

    Fix the bug that ctrl-c cannot terminate newselect.

--- qiling/os/posix/syscall/select.py
+++ qiling/os/posix/syscall/select.py
@@ -86,6 +86,8 @@ def ql_syscall__newselect(ql, _newselect_nfds, _newselect_readfds, _newselect_wr
             for i in ans[2]:
                 tmp_buf = set_fd_set(tmp_buf, tmp_e_map[i])
             ql.mem.write(_newselect_exceptfds, tmp_buf)
+    except KeyboardInterrupt:
+        raise
     except:
         if ql.output in (QL_OUT_DEBUG, QL_OUT_DUMP):
             raise
