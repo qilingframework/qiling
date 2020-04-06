b4a8fd6bda6394be91eddc12a32b06783c251069
0ssigeno <simone.berni2@studio.unibo.it>
2020-04-06 10:19:12 +0200

    Create function to randomize configuration.cfg. We have to decide where to insert it, for now is in utils.py

--- qiling/os/windows/utils.py
+++ qiling/os/windows/utils.py
@@ -6,6 +6,8 @@ import struct
 from unicorn.x86_const import *
 from qiling.os.utils import *
 from qiling.const import *
+import random
+import string as st
 
 
 def ql_x86_windows_hook_mem_error(ql, addr, size, value):
@@ -96,3 +98,43 @@ def printf(ql, address, fmt, params_addr, name, wstring=False):
     ql.nprint(output)
     ql.stdout.write(bytes(stdout + "\n", 'utf-8'))
     return len(stdout), stdout
+
+
+def randomize_config_value(ql, key, subkey):
+    # https://en.wikipedia.org/wiki/Volume_serial_number
+    # https://www.digital-detective.net/documents/Volume%20Serial%20Numbers.pdf
+    if key == "VOLUME" and subkey == "serial_number":
+        month = random.randint(0, 12)
+        day = random.randint(0, 30)
+        first = hex(month)[2:] + hex(day)[2:]
+        seconds = random.randint(0, 60)
+        milli = random.randint(0, 100)
+        second = hex(seconds)[2:] + hex(milli)[2:]
+        first_half = int(first, 16) + int(second, 16)
+        hour = random.randint(0, 24)
+        minute = random.randint(0, 60)
+        third = hex(hour)[2:] + hex(minute)[2:]
+        year = random.randint(2000, 2020)
+        second_half = int(third, 16) + year
+        result = int(hex(first_half)[2:] + hex(second_half)[2:], 16)
+        ql.config[key][subkey] = result
+    elif key == "USER" and subkey == "user":
+        length = random.randint(0, 15)
+        new_name = ""
+        for i in range(length):
+            new_name += random.choice(st.ascii_lowercase + st.ascii_uppercase)
+        old_name = ql.config[key][subkey]
+        # update paths
+        ql.config[key][subkey] = new_name
+        for path in ql.config["PATHS"]:
+            val = ql.config["PATHS"][path].replace(old_name, new_name)
+            ql.config["PATHS"][path] = val
+            print(ql.config["PATHS"][path])
+    elif key == "SYSTEM" and subkey == "computer_name":
+        length = random.randint(0, 15)
+        new_name = ""
+        for i in range(length):
+            new_name += random.choice(st.ascii_lowercase + st.ascii_uppercase)
+        ql.config[key][subkey] = new_name
+    else:
+        raise QlErrorNotImplemented("[!] API not implemented")
