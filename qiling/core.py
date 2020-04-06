6790ede6b2d054da96db98011351e7c04df9667d
Jhe <ucg.jhe@gmail.com>
2020-04-06 11:27:43 +0800

    move syscall_filter to run-time setting, change syscall_filter logic to whether starts with syscall name or not

--- qiling/core.py
+++ qiling/core.py
@@ -50,7 +50,6 @@ class Qiling:
             stdout=0,
             stderr=0,
             output=None,
-            syscall_filter=None,
             verbose=0,
             log_console=True,
             log_dir=None,
@@ -61,7 +60,6 @@ class Qiling:
     ):
         # Define during ql=Qiling()
         self.output = output
-        self.syscall_filter = syscall_filter
         self.verbose = verbose
         self.ostype = ostype
         self.archtype = archtype
@@ -156,16 +154,6 @@ class Qiling:
         # Looger's configuration
         _logger = ql_setup_logging_stream(self)
 
-        # setup syscall filter
-        if self.syscall_filter != None and self.output == QL_OUT_DEFAULT:
-
-            class Syscall_filter(logging.Filter):
-                def __init__(self, syscall_names):
-                    self.syscall_list = syscall_names.split(',')
-                def filter(self, record):
-                    return any((each in record.getMessage() for each in self.syscall_list))
-
-            _logger.addFilter(Syscall_filter(self.syscall_filter))
 
         if self.log_dir is not None and type(self.log_dir) == str:
 
@@ -296,6 +284,20 @@ class Qiling:
         loader_shellcode(self)
 
     def run(self):
+
+        # setup syscall filter for logger
+        if self.syscall_filter != None and self.output == QL_OUT_DEFAULT:
+
+            _logger = self.log_file_fd
+
+            class Syscall_filter(logging.Filter):
+                def __init__(self, syscall_names):
+                    self.syscall_list = syscall_names.split(',')
+                def filter(self, record):
+                    return any((record.getMessage().startswith(each) for each in self.syscall_list))
+
+            _logger.addFilter(Syscall_filter(self.syscall_filter))
+
         # debugger init
         if self.debugger is not None:
             try:
