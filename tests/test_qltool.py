10fc5fda0a50b4f4d974c6c60e396abe836854c0
Jhe <ucg.jhe@gmail.com>
2020-04-07 14:52:15 +0800

    add testing strace filter in test_qltool

--- tests/test_qltool.py
+++ tests/test_qltool.py
@@ -23,6 +23,14 @@ def testshellcode():
     except subprocess.CalledProcessError as e:    
         raise RuntimeError("command '{}' return with error (code {}): {}".format(e.cmd, e.returncode, e.output))    
 
+def teststrace_filter():
+    create = ["../qltool", "run", "-f", "../examples/rootfs/mips32el_linux/bin/mips32el_hello", "--rootfs", "../examples/rootfs/mips32el_linux", "--strace", "-e", "brk"]
+    try:
+        subprocess.check_output(create, stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError as e:
+        raise RuntimeError("command '{}' return with error (code {}): {}".format(e.cmd, e.retruncode, e.output))
+
 if __name__ == "__main__":
     testexec_args()
     testshellcode()
+    teststrace_filter()
