05575f6b243f7dce9cdfc932c560b26b0b798675
chfl4gs <chbsd64@gmail.com>
2020-04-07 21:17:07 +0800

    fix libcache.dylib path

--- examples/scripts/collectdylib.sh
+++ examples/scripts/collectdylib.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cp /usr/lib/dyld examples/rootfs/x8664_macos/usr/lib/;
+cp /usr/lib/system/libcache.dylib examples/rootfs/x8664_macos/usr/lib/;
 find /usr/lib -type d -exec mkdir -p qiling/examples/rootfs/x8664_macos{} \;
 find /usr/lib -type f -name "*.dylib" -not -name "libobjc.A.dylib" -exec install -v {} examples/rootfs/x8664_macos{} \;
