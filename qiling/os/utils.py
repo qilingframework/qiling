cfebfd5193fb06bd87dafe2a3476f23bdd6dfdfb
w1tcher <w1tcher@ubuntu>
2020-04-13 10:42:25 +0800

    Remove multi-threaded useless code.

--- qiling/os/utils.py
+++ qiling/os/utils.py
@@ -272,7 +272,7 @@ def ql_asm2bytes(ql, archtype, runcode, arm_thumb):
 
 
 def ql_transform_to_link_path(ql, path):
-    if ql.thread_management != None:
+    if ql.multithread == True:
         cur_path = ql.thread_management.cur_thread.get_current_path()
     else:
         cur_path = ql.current_path
@@ -302,7 +302,7 @@ def ql_transform_to_link_path(ql, path):
 
 
 def ql_transform_to_real_path(ql, path):
-    if ql.thread_management != None:
+    if ql.multithread == True:
         cur_path = ql.thread_management.cur_thread.get_current_path()
     else:
         cur_path = ql.current_path
@@ -341,7 +341,7 @@ def ql_transform_to_real_path(ql, path):
 
 
 def ql_transform_to_relative_path(ql, path):
-    if ql.thread_management != None:
+    if ql.multithread == True:
         cur_path = ql.thread_management.cur_thread.get_current_path()
     else:
         cur_path = ql.current_path
@@ -383,7 +383,7 @@ def ql_real_to_vm_abspath(ql, path):
 
 
 def ql_get_vm_current_path(ql):
-    if ql.thread_management is not None:
+    if ql.multithread == True:
         return ql.thread_management.cur_thread.get_current_path()
     else:
         return ql.current_path
