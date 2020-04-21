eeee4c5a3527899222f31a9de372d86da47d0d57
0ssigeno <simone.berni2@studio.unibo.it>
2020-04-21 13:58:33 +0200

    Adding import of const

--- tests/test_pe_nu.py
+++ tests/test_pe_nu.py
@@ -13,6 +13,7 @@ from qiling import *
 from qiling.exception import *
 from qiling.os.windows.fncc import *
 from qiling.os.windows.utils import *
+from unicorn.x86_const import *
 
 X86_WIN = unhexlify(
     'fce8820000006089e531c0648b50308b520c8b52148b72280fb74a2631ffac3c617c022c20c1cf0d01c7e2f252578b52108b4a3c8b4c1178e34801d1518b592001d38b4918e33a498b348b01d631ffacc1cf0d01c738e075f6037df83b7d2475e4588b582401d3668b0c4b8b581c01d38b048b01d0894424245b5b61595a51ffe05f5f5a8b12eb8d5d6a01eb2668318b6f87ffd5bbf0b5a25668a695bd9dffd53c067c0a80fbe07505bb4713726f6a0053ffd5e8d5ffffff63616c6300')
@@ -155,7 +156,6 @@ def test_pe_win_al_khaser():
 
     # The hooks are to remove the prints to file. It crashes. will debug why in the future
     def results(ql):
-        UC_X86_REG_EBX = 21
 
         if ql.register(UC_X86_REG_EBX) == 1:
             print("[=] BAD")
