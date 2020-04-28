f70ce129105af7bb010da0ecccdfa237c8196704
chfl4gs <chbsd64@gmail.com>
2020-04-28 23:11:27 +0800

    Update classifier for pypi publishing

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 4 - Production/Beta',
+        'Development Status :: 4 - Production/Stable',
 
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
