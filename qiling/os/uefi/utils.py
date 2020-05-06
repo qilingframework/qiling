1d570fc902b4d1eb86d683d902bc94e748526d65
Itai Liba <liba2k@gmail.com>
2020-05-06 09:19:14 -0700

    More cleanup.

--- qiling/os/uefi/utils.py
+++ qiling/os/uefi/utils.py
@@ -27,11 +27,13 @@ def write_int32(ql, address, num):
         ql.mem.write(address, struct.pack('<I',(num)))
     else:
         ql.mem.write(address, struct.pack('>I',(num)))
+
 def write_int64(ql, address, num):
     if ql.archendian == QL_ENDIAN.EL:
         ql.mem.write(address, struct.pack('<Q',(num)))
     else:
         ql.mem.write(address, struct.pack('>Q',(num)))
+
 def read_int64(ql, address):
     if ql.archendian == QL_ENDIAN.EL:
         return struct.unpack('<Q', ql.mem.read(address, 8))[0]
