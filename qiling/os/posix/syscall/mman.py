0a75df63566c71964b24fca81e0b82f7734fd404
John Martin <jimartin361@gmail.com>
2020-06-08 16:14:16 -0400

    took out debugging prints

--- qiling/os/posix/syscall/mman.py
+++ qiling/os/posix/syscall/mman.py
@@ -227,10 +227,8 @@ def ql_syscall_mmap2(ql, mmap2_addr, mmap2_length, mmap2_prot, mmap2_flags, mmap
     if need_mmap:
         ql.dprint(D_INFO, "[+] log mmap2 - mapping needed")
         try:
-            ql.dprint(D_INFO, "trying unicorn map %x %x"%(mmap_base,((mmap2_length + 0x1000 - 1) // 0x1000) * 0x1000))
             ql.mem.map(mmap_base, ((mmap2_length + 0x1000 - 1) // 0x1000) * 0x1000)
         except:
-            ql.dprint(D_INFO, "unicorn mapping failed %x %x"%(mmap_base,((mmap2_length + 0x1000 - 1) // 0x1000) * 0x1000))
             ql.mem.show_mapinfo()
             raise
 
