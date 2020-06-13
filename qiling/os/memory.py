2988c0f61238bb3c383909387d291c8ad0ce0082
Assaf Carlsbad <carlsbad@sentinelone.com>
2020-06-13 20:29:30 +0300

    Expose the ability to explicitly validate the integrity of the sanitized pool canaries.

--- qiling/os/memory.py
+++ qiling/os/memory.py
@@ -461,10 +461,12 @@ class QlMemoryHeap:
         return None
 
 class QlSanitizedMemoryHeap():
-    def __init__(self, ql, heap, fault_rate=0):
+    def __init__(self, ql, heap, fault_rate=0, canary_byte=b'\xCD'):
         self.ql = ql
         self.heap = heap
         self.fault_rate = fault_rate
+        self.canary_byte = canary_byte
+        self.canaries = []
 
     @staticmethod
     def bo_handler(ql, access, addr, size, value):
@@ -497,20 +499,23 @@ class QlSanitizedMemoryHeap():
     def alloc(self, size):
         chance = random.randint(1, 100)
         if chance <= self.fault_rate:
-            # Fail the allocation
+            # Fail the allocation.
             return 0
 
         # Add 8 bytes to the requested size so as to accomodate the canaries.
         addr = self.heap.alloc(size + 8)
+        self.ql.mem.write(addr, self.canary_byte * (size + 8))
 
         # Install canary hooks for overflow/underflow detection.
-        (underflow_begin, underflow_end) = (addr, addr + 3)
-        self.ql.hook_mem_write(self.bo_handler, begin=underflow_begin, end=underflow_end)
-        self.ql.hook_mem_read(self.oob_handler, begin=underflow_begin, end=underflow_end)
+        underflow_canary = (addr, addr + 3)
+        self.ql.hook_mem_write(self.bo_handler, begin=underflow_canary[0], end=underflow_canary[1])
+        self.ql.hook_mem_read(self.oob_handler, begin=underflow_canary[0], end=underflow_canary[1])
+        self.canaries.append(underflow_canary)
 
-        (overflow_begin, overflow_end) = (addr + 4 + size, addr + 4 + size + 3)
-        self.ql.hook_mem_write(self.bo_handler, begin=overflow_begin, end=overflow_end)
-        self.ql.hook_mem_read(self.oob_handler, begin=overflow_begin, end=overflow_end)
+        overflow_canary = (addr + 4 + size, addr + 4 + size + 3)
+        self.ql.hook_mem_write(self.bo_handler, begin=overflow_canary[0], end=overflow_canary[1])
+        self.ql.hook_mem_read(self.oob_handler, begin=overflow_canary[0], end=overflow_canary[1])
+        self.canaries.append(overflow_canary)
 
         return (addr + 4)
 
@@ -519,12 +524,26 @@ class QlSanitizedMemoryHeap():
 
     def free(self, addr):
         chunk = self.heap._find(addr - 4)
-        if chunk:
-            # Install the UAF hook.
-            self.ql.hook_mem_valid(self.uaf_handler, begin=addr, end=addr+chunk.size-8)
-            # Make sure the chunk won't be re-used by the underlying heap.
-            self.heap.chunks.remove(chunk)
-            return True
-        else:
+
+        if not chunk:
             self.bad_free_handler(self.ql, addr)
             return False
+
+        # Install the UAF canary hook.
+        self.ql.mem.write(addr, self.canary_byte * (chunk.size - 8))
+        uaf_canary = (addr, addr + chunk.size - 8)
+        self.ql.hook_mem_valid(self.uaf_handler, begin=addr, end=addr+chunk.size-8)
+        self.canaries.append(uaf_canary)
+
+        # Make sure the chunk won't be re-used by the underlying heap.
+        self.heap.chunks.remove(chunk)
+        return True
+
+    def validate(self):
+        for (canary_begin, canary_end) in self.canaries:
+            size = canary_end - canary_begin + 1
+            canary = self.ql.mem.read(canary_begin, size)
+            if canary.count(self.canary_byte) != len(canary):
+                return False
+        return True
+                
