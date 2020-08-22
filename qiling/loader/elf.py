e3047937080d48a7bcf6d28f92ae4a78cee43766
kabeor <1597915586@qq.com>
2020-08-22 19:31:39 +0800

    clean code

--- qiling/loader/elf.py
+++ qiling/loader/elf.py
@@ -12,13 +12,6 @@ from qiling.exception import *
 from .loader import QlLoader
 from qiling.os.linux.function_hook import FunctionHook
 
-PT_LOAD = 1
-PT_DYNAMIC = 2
-PT_INTERP = 3
-
-# ET_REL = 1
-# ET_EXEC = 2
-# ET_DYN = 3
 
 AT_NULL = 0
 AT_IGNORE = 1
@@ -52,298 +45,28 @@ class ELFParse():
         self.path = os.path.abspath(path)
         self.ql = ql
 
-        # with open(path, "rb") as f:
         self.f = open(path, "rb")
         elfdata = self.f.read()
         self.elffile = ELFFile(self.f)
 
         self.elfdata = elfdata.ljust(52, b'\x00')        
 
-        # self.ident = self.getident()
-
-        # if self.ident[ : 4] != b'\x7fELF':
         if self.elffile.e_ident_raw[ : 4] != b'\x7fELF':
             raise QlErrorELFFormat("[!] ERROR: NOT a ELF")
 
         self.elfhead = self.parse_header()
 
-    # def getident(self):
-    #     return self.elfdata[0 : 19]
-
     def getelfdata(self, offest, size):
         return self.elfdata[offest : offest + size]
-
-    def parse_header32(self):
-        # typedef struct elf32_hdr{
-        # unsigned char	e_ident[EI_NIDENT];
-        # Elf32_Half	e_type;
-        # Elf32_Half	e_machine;
-        # Elf32_Word	e_version;
-        # Elf32_Addr	e_entry;  /* Entry point */
-        # Elf32_Off	e_phoff;
-        # Elf32_Off	e_shoff;
-        # Elf32_Word	e_flags;
-        # Elf32_Half	e_ehsize;
-        # Elf32_Half	e_phentsize;
-        # Elf32_Half	e_phnum;
-        # Elf32_Half	e_shentsize;
-        # Elf32_Half	e_shnum;
-        # Elf32_Half	e_shstrndx;
-        # } Elf32_Ehdr;
-
-        # /* 32-bit ELF base types. */
-        # typedef uint32_t Elf32_Addr;
-        # typedef uint16_t Elf32_Half;
-        # typedef uint32_t Elf32_Off;
-        # typedef int32_t  Elf32_Sword;
-        # typedef uint32_t Elf32_Word;
-
-        data = self.elfdata
-        index = 0
-        head = {}
-        head['e_ident']         = data[index : index + 16];                     index += 16
-        head['e_type']          = self.ql.unpack16(data[index : index + 2]);    index += 2
-        head['e_machine']       = self.ql.unpack16(data[index : index + 2]);    index += 2
-        head['e_version']       = self.ql.unpack32(data[index : index + 4]);    index += 4
-        head['e_entry']         = self.ql.unpack32(data[index : index + 4]);    index += 4
-        head['e_phoff']         = self.ql.unpack32(data[index : index + 4]);    index += 4
-        head['e_shoff']         = self.ql.unpack32(data[index : index + 4]);    index += 4
-        head['e_flags']         = self.ql.unpack32(data[index : index + 4]);    index += 4
-        head['e_ehsize']        = self.ql.unpack16(data[index : index + 2]);    index += 2
-        head['e_phentsize']     = self.ql.unpack16(data[index : index + 2]);    index += 2
-        head['e_phnum']         = self.ql.unpack16(data[index : index + 2]);    index += 2
-        head['e_shentsize']     = self.ql.unpack16(data[index : index + 2]);    index += 2
-        head['e_shnum']         = self.ql.unpack16(data[index : index + 2]);    index += 2
-        head['e_shstrndx']      = self.ql.unpack16(data[index : index + 2]);    index += 2
-        return head
-
-    def parse_header64(self):
-        # typedef struct elf64_hdr {
-        # unsigned char	e_ident[16];		/* ELF "magic number" */
-        # Elf64_Half e_type;
-        # Elf64_Half e_machine;
-        # Elf64_Word e_version;
-        # Elf64_Addr e_entry;		/* Entry point virtual address */
-        # Elf64_Off e_phoff;		/* Program header table file offset */
-        # Elf64_Off e_shoff;		/* Section header table file offset */
-        # Elf64_Word e_flags;
-        # Elf64_Half e_ehsize;
-        # Elf64_Half e_phentsize;
-        # Elf64_Half e_phnum;
-        # Elf64_Half e_shentsize;
-        # Elf64_Half e_shnum;
-        # Elf64_Half e_shstrndx;
-        # } Elf64_Ehdr;
-
-        # /* 64-bit ELF base types. */
-        # typedef uint64_t Elf64_Addr;
-        # typedef uint16_t Elf64_Half;
-        # typedef int16_t	 Elf64_SHalf;
-        # typedef uint64_t Elf64_Off;
-        # typedef int32_t	 Elf64_Sword;
-        # typedef uint32_t Elf64_Word;
-        # typedef uint64_t Elf64_Xword;
-        # typedef int64_t  Elf64_Sxword;
-
-        data = self.elfdata
-        index = 0
-        head = {}
-        head['e_ident']         = data[index : index + 16];                 index += 16
-        head['e_type']          = self.ql.unpack16(data[index : index + 2]);     index += 2
-        head['e_machine']       = self.ql.unpack16(data[index : index + 2]);     index += 2
-        head['e_version']       = self.ql.unpack32(data[index : index + 4]);     index += 4
-        head['e_entry']         = self.ql.unpack64(data[index : index + 8]);     index += 8
-        head['e_phoff']         = self.ql.unpack64(data[index : index + 8]);     index += 8
-        head['e_shoff']         = self.ql.unpack64(data[index : index + 8]);     index += 8
-        head['e_flags']         = self.ql.unpack32(data[index : index + 4]);     index += 4
-        head['e_ehsize']        = self.ql.unpack16(data[index : index + 2]);     index += 2
-        head['e_phentsize']     = self.ql.unpack16(data[index : index + 2]);     index += 2
-        head['e_phnum']         = self.ql.unpack16(data[index : index + 2]);     index += 2
-        head['e_shentsize']     = self.ql.unpack16(data[index : index + 2]);     index += 2
-        head['e_shnum']         = self.ql.unpack16(data[index : index + 2]);     index += 2
-        head['e_shstrndx']      = self.ql.unpack16(data[index : index + 2]);     index += 2
-        return head
     
     def parse_header(self):
         return dict(self.elffile.header)
-        # if self.ql.archbit == 64:
-        #     return self.parse_header64()
-        # elif self.ql.archbit == 32:
-        #     return self.parse_header32()
-
-    def parse_section_header32(self):
-        # typedef struct elf32_shdr {
-        # Elf32_Word	sh_name;
-        # Elf32_Word	sh_type;
-        # Elf32_Word	sh_flags;
-        # Elf32_Addr	sh_addr;
-        # Elf32_Off	sh_offset;
-        # Elf32_Word	sh_size;
-        # Elf32_Word	sh_link;
-        # Elf32_Word	sh_info;
-        # Elf32_Word	sh_addralign;
-        # Elf32_Word	sh_entsize;
-        # } Elf32_Shdr;
-
-        # /* 32-bit ELF base types. */
-        # typedef uint32_t Elf32_Addr;
-        # typedef uint16_t Elf32_Half;
-        # typedef uint32_t Elf32_Off;
-        # typedef int32_t  Elf32_Sword;
-        # typedef uint32_t Elf32_Word;
-
-        Ssize = self.elfhead['e_shentsize']
-        Snum = self.elfhead['e_shnum']
-        Sdata = self.elfdata[self.elfhead['e_shoff'] : self.elfhead['e_shoff'] + Snum * Ssize]
-        
-        for i in range(Snum):
-            S = {}
-            S['sh_name']        = self.ql.unpack32(Sdata[i * Ssize : i * Ssize + 4])
-            S['sh_type']        = self.ql.unpack32(Sdata[i * Ssize + 4 : i * Ssize + 8])
-            S['sh_flags']       = self.ql.unpack32(Sdata[i * Ssize + 8 : i * Ssize + 12])
-            S['sh_addr']        = self.ql.unpack32(Sdata[i * Ssize + 12 : i * Ssize + 16])
-            S['sh_offset']      = self.ql.unpack32(Sdata[i * Ssize + 16 : i * Ssize + 20])
-            S['sh_size']        = self.ql.unpack32(Sdata[i * Ssize + 20 : i * Ssize + 24])
-            S['sh_link']        = self.ql.unpack32(Sdata[i * Ssize + 24 : i * Ssize + 28])
-            S['sh_info']        = self.ql.unpack32(Sdata[i * Ssize + 28 : i * Ssize + 32])
-            S['sh_addralign']   = self.ql.unpack32(Sdata[i * Ssize + 32 : i * Ssize + 36])
-            S['sh_entsize']     = self.ql.unpack32(Sdata[i * Ssize + 36 : i * Ssize + 40])
-            S['data']           = self.elfdata[S['sh_offset'] : S['sh_offset'] + S['sh_size']]
-            yield S
-        return
-
-    def parse_section_header64(self):
-        # typedef struct elf64_shdr {
-        # Elf64_Word sh_name;		/* Section name, index in string tbl */
-        # Elf64_Word sh_type;		/* Type of section */
-        # Elf64_Xword sh_flags;		/* Miscellaneous section attributes */
-        # Elf64_Addr sh_addr;		/* Section virtual addr at execution */
-        # Elf64_Off sh_offset;		/* Section file offset */
-        # Elf64_Xword sh_size;		/* Size of section in bytes */
-        # Elf64_Word sh_link;		/* Index of another section */
-        # Elf64_Word sh_info;		/* Additional section information */
-        # Elf64_Xword sh_addralign;	/* Section alignment */
-        # Elf64_Xword sh_entsize;	/* Entry size if section holds table */
-        # } Elf64_Shdr;
-
-        # /* 64-bit ELF base types. */
-        # typedef uint64_t Elf64_Addr;
-        # typedef uint16_t Elf64_Half;
-        # typedef int16_t	 Elf64_SHalf;
-        # typedef uint64_t Elf64_Off;
-        # typedef int32_t	 Elf64_Sword;
-        # typedef uint32_t Elf64_Word;
-        # typedef uint64_t Elf64_Xword;
-        # typedef int64_t  Elf64_Sxword;
-
-        Ssize = self.elfhead['e_shentsize']
-        Snum = self.elfhead['e_shnum']
-        Sdata = self.elfdata[self.elfhead['e_shoff'] : self.elfhead['e_shoff'] + Snum * Ssize]
-
-        for i in range(Snum):
-            S = {}
-            S['sh_name']        = self.ql.unpack32(Sdata[i * Ssize : i * Ssize + 4])
-            S['sh_type']        = self.ql.unpack32(Sdata[i * Ssize + 4 : i * Ssize + 8])
-            S['sh_flags']       = self.ql.unpack64(Sdata[i * Ssize + 8 : i * Ssize + 16])
-            S['sh_addr']        = self.ql.unpack64(Sdata[i * Ssize + 16 : i * Ssize + 24])
-            S['sh_offset']      = self.ql.unpack64(Sdata[i * Ssize + 24 : i * Ssize + 32])
-            S['sh_size']        = self.ql.unpack64(Sdata[i * Ssize + 32 : i * Ssize + 40])
-            S['sh_link']        = self.ql.unpack32(Sdata[i * Ssize + 40 : i * Ssize + 44])
-            S['sh_info']        = self.ql.unpack32(Sdata[i * Ssize + 44 : i * Ssize + 48])
-            S['sh_addralign']   = self.ql.unpack64(Sdata[i * Ssize + 48 : i * Ssize + 56])
-            S['sh_entsize']     = self.ql.unpack64(Sdata[i * Ssize + 56 : i * Ssize + 64])
-            S['data']           = self.elfdata[S['sh_offset'] : S['sh_offset'] + S['sh_size']]
-            yield S
-        return
 
     def parse_sections(self):
         return self.elffile.iter_sections()
-        # if self.ql.archbit == 64:
-        #     return self.parse_section_header64()
-        # elif self.ql.archbit == 32:
-        #     return self.parse_section_header32()
-
-    def parse_program_header32(self):
-        # typedef struct elf32_phdr{
-        # Elf32_Word	p_type;
-        # Elf32_Off	p_offset;
-        # Elf32_Addr	p_vaddr;
-        # Elf32_Addr	p_paddr;
-        # Elf32_Word	p_filesz;
-        # Elf32_Word	p_memsz;
-        # Elf32_Word	p_flags;
-        # Elf32_Word	p_align;
-        # } Elf32_Phdr;
-
-        # /* 32-bit ELF base types. */
-        # typedef uint32_t Elf32_Addr;
-        # typedef uint16_t Elf32_Half;
-        # typedef uint32_t Elf32_Off;
-        # typedef int32_t  Elf32_Sword;
-        # typedef uint32_t Elf32_Word;
-
-        Psize = int(self.elfhead['e_phentsize'])
-        Pnum = int(self.elfhead['e_phnum'])
-        Pdata = self.elfdata[self.elfhead['e_phoff'] : self.elfhead['e_phoff'] + Pnum * Psize]
-
-        for i in range(Pnum):
-            P = {}
-            P['p_type']     = self.ql.unpack32(Pdata[i * Psize : i * Psize + 4 ])
-            P['p_offset']   = self.ql.unpack32(Pdata[i * Psize + 4 : i * Psize + 8 ])
-            P['p_vaddr']    = self.ql.unpack32(Pdata[i * Psize + 8 : i * Psize + 12 ])
-            P['p_paddr']    = self.ql.unpack32(Pdata[i * Psize + 12 : i * Psize + 16 ])
-            P['p_filesz']   = self.ql.unpack32(Pdata[i * Psize + 16 : i * Psize + 20 ])
-            P['p_memsz']    = self.ql.unpack32(Pdata[i * Psize + 20 : i * Psize + 24 ])
-            P['p_flags']    = self.ql.unpack32(Pdata[i * Psize + 24 : i * Psize + 28 ])
-            P['p_align']    = self.ql.unpack32(Pdata[i * Psize + 28 : i * Psize + 32])
-            yield P
-        return
-
-    def parse_program_header64(self):
-        # typedef struct elf64_phdr {
-        # Elf64_Word p_type;
-        # Elf64_Word p_flags;
-        # Elf64_Off p_offset;		/* Segment file offset */
-        # Elf64_Addr p_vaddr;		/* Segment virtual address */
-        # Elf64_Addr p_paddr;		/* Segment physical address */
-        # Elf64_Xword p_filesz;		/* Segment size in file */
-        # Elf64_Xword p_memsz;		/* Segment size in memory */
-        # Elf64_Xword p_align;		/* Segment alignment, file & memory */
-        # } Elf64_Phdr;
-
-        # /* 64-bit ELF base types. */
-        # typedef uint64_t Elf64_Addr;
-        # typedef uint16_t Elf64_Half;
-        # typedef int16_t	 Elf64_SHalf;
-        # typedef uint64_t Elf64_Off;
-        # typedef int32_t	 Elf64_Sword;
-        # typedef uint32_t Elf64_Word;
-        # typedef uint64_t Elf64_Xword;
-        # typedef int64_t  Elf64_Sxword;
-
-        Psize = self.elfhead['e_phentsize']
-        Pnum = self.elfhead['e_phnum']
-        Pdata = self.elfdata[self.elfhead['e_phoff'] : self.elfhead['e_phoff'] + Pnum * Psize]
-
-        for i in range(Pnum):
-            P = {}
-            P['p_type']     = self.ql.unpack32(Pdata[i * Psize : i * Psize + 4 ])
-            P['p_flags']    = self.ql.unpack32(Pdata[i * Psize + 4 : i * Psize + 8 ])
-            P['p_offset']   = self.ql.unpack64(Pdata[i * Psize + 8 : i * Psize + 16 ])
-            P['p_vaddr']    = self.ql.unpack64(Pdata[i * Psize + 16 : i * Psize + 24 ])
-            P['p_paddr']    = self.ql.unpack64(Pdata[i * Psize + 24 : i * Psize + 32 ])
-            P['p_filesz']   = self.ql.unpack64(Pdata[i * Psize + 32 : i * Psize + 40 ])
-            P['p_memsz']    = self.ql.unpack64(Pdata[i * Psize + 40 : i * Psize + 48 ])
-            P['p_align']    = self.ql.unpack64(Pdata[i * Psize + 48 : i * Psize + 56])
-            yield P
-        return
 
     def parse_segments(self):
         return self.elffile.iter_segments()
-        # if self.ql.archbit == 64:
-        #     return self.parse_program_header64()
-        # elif self.ql.archbit == 32:
-        #     return self.parse_program_header32()
 
 
 class QlLoaderELF(QlLoader, ELFParse):
