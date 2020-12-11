47c4978651d7e179a2e49a6ddb69d335ed138889
Peter <peterstolz2000@gmail.com>
2020-12-11 12:29:22 +0100

    posix.wait4 syscall now handles invalid pids correctly

--- qiling/os/posix/syscall/wait.py
+++ qiling/os/posix/syscall/wait.py
@@ -12,11 +12,18 @@ from qiling.os.filestruct import *
 from qiling.os.posix.const_mapping import *
 from qiling.exception import *
 from qiling.utils import *
+from qiling.os.posix.const import ECHILD
+
 
 def ql_syscall_wait4(ql, wait4_pid, wait4_wstatus, wait4_options, wait4_rusage, *args, **kw):
-    spid, status, rusage = os.wait4(wait4_pid, wait4_options)
-    if wait4_wstatus != 0:
-        ql.mem.write(wait4_wstatus, ql.pack32(status))
-    regreturn = spid
+    wait4_pid = ql.unpacks(ql.pack(wait4_pid))  # convert to signed
+    try:
+        spid, status, rusage = os.wait4(wait4_pid, wait4_options)
+        if wait4_wstatus != 0:
+            ql.mem.write(wait4_wstatus, ql.pack32(status))
+        regreturn = spid
+    except ChildProcessError:
+        regreturn = -ECHILD
     logging.info("wait4(%d, %d) = %d"% (wait4_pid, wait4_options, regreturn))
-    return regreturn
+    ql.os.definesyscall_return(regreturn)
+
