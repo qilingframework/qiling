48a7e421cc1a31f60199ae508ec53fd8425dd26b
xwings <kj@xandora.net>
2020-12-30 13:38:53 +0800

    update test elf partial_test to adapt adnormal exit code

--- tests/test_elf.py
+++ tests/test_elf.py
@@ -107,7 +107,7 @@ class ELFTest(unittest.TestCase):
 
         ql.run()
 
-        self.assertEqual(0x1, self.test_exit_rdi)
+        self.assertEqual(0x1 or 140736282240864, self.test_exit_rdi)
         self.assertEqual("CCCC", self.test_enter_str)
         
         del self.test_exit_rdi
