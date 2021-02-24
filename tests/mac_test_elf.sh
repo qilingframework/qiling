e8e14fe91ee8a71f6627389e66e8c08c65856e2a
Kai Jern Lau <xwings@kamino.lan>
2021-02-24 23:35:50 +0800

    remove unwated test file

--- tests/mac_test_elf.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# 
-# Cross Platform and Multi Architecture Advanced Binary Emulation Framework
-#
-
-python3.9 ./test_posix.py && python3.9 test_android.py && python3.9 ./test_debugger.py && python3.9 ./test_uefi.py && python3.9 ./test_shellcode.py && python3.9 ./test_edl.py
