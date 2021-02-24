c0d86dbd246d54ddf1b91b790e35624c1e68050a
elicn <elichn@gmail.com>
2021-02-24 03:15:45 +0200

    Enrich QlCC iface with more abstract method

--- qiling/refactored/cc/__init__.py
+++ qiling/refactored/cc/__init__.py
@@ -17,28 +17,95 @@ class QlCC:
 
 		self.ql = ql
 
-	def getRawParam(self, index: int) -> int:
-		"""Read argument's raw value.
-		It is the caller responsibility to make sure the argument exists.
-
-		Args:
-			index: argument index to read
-
-		Returns: argument raw value
+	@staticmethod
+	def getNumSlots(argbits: int) -> int:
+		"""Get the number of slots allocated for an argument of width `argbits`.
 		"""
 
 		raise NotImplementedError
 
-	def setRawParam(self, index: int, value: int) -> None:
-		"""Replace argument's raw value.
-		It is the caller responsibility to make sure the argument exists.
+	def getRawParam8(self, slot: int) -> int:
+		"""Read a 8 bits value from the specified argument slot.
+
+		Note that argument slots and argument indexes are not the same. Though they often correlate
+		to each other, some implementations might use more than one slot to represent a sigle argument.
 
 		Args:
-			index: argument index to replace
-			value: new raw value to write
+			slot: argument slot to read
+
+		Returns: 8 bits raw value
 		"""
 
 		raise NotImplementedError
+
+	def getRawParam16(self, slot: int) -> int:
+		"""Read a 16 bits value from the specified argument slot.
+
+		Note that argument slots and argument indexes are not the same. Though they often correlate
+		to each other, some implementations might use more than one slot to represent a sigle argument.
+
+		Args:
+			slot: argument slot to read
+
+		Returns: 16 bits raw value
+		"""
+
+		raise NotImplementedError
+
+	def getRawParam32(self, slot: int) -> int:
+		"""Read a 32 bits value from the specified argument slot.
+
+		Note that argument slots and argument indexes are not the same. Though they often correlate
+		to each other, some implementations might use more than one slot to represent a sigle argument.
+
+		Args:
+			slot: argument slot to read
+
+		Returns: 32 bits raw value
+		"""
+
+		raise NotImplementedError
+
+	def getRawParam64(self, slot: int) -> int:
+		"""Read a 64 bits value from the specified argument slot.
+
+		Note that argument slots and argument indexes are not the same. Though they often correlate
+		to each other, some implementations might use more than one slot to represent a sigle argument.
+
+		Args:
+			slot: argument slot to read
+
+		Returns: 64 bits raw value
+		"""
+
+		raise NotImplementedError
+
+	def getRawParam(self, slot: int) -> int:
+		"""Read a value of native size from the specified argument slot.
+
+		Note that argument slots and argument indexes are not the same. Though they often correlate
+		to each other, some implementations might use more than one slot to represent a sigle argument.
+
+		Args:
+			slot: argument slot to read
+
+		Returns: raw value
+		"""
+
+		raise NotImplementedError
+
+	# def setRawParam(self, slot: int, value: int) -> None:
+	# 	"""Replace the value in the specified argument slot.
+	# 
+	# 	Note that argument slots and argument indexes are not the same. Though they often correlate
+	# 	to each other, some implementations might use more than one slot to represent a sigle argument.
+	# 
+	# 	Args:
+	# 		slot: argument slot to read
+	# 		value: new raw value to write
+	# 	"""
+	# 
+	# 	raise NotImplementedError
 
 	def getReturnValue(self) -> int:
 		"""Get function return value.
