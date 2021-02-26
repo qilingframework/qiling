26a297a3fb1293fa4d973288001d9e18145cab78
elicn <elichn@gmail.com>
2021-02-26 06:05:50 +0200

    Refactor MacOS fncc

--- qiling/os/macos/fncc.py
+++ qiling/os/macos/fncc.py
@@ -3,30 +3,17 @@
 # Cross Platform and Multi Architecture Advanced Binary Emulation Framework
 #
 
-from unicorn.x86_const import *
+from qiling import Qiling
+from qiling.const import QL_INTERCEPT
 
-DWORD = 1
-UINT = 1
-ULONG = 1
-INT = 1
-BOOL = 1
-SIZE_T = 1
-BYTE = 1
-CHAR = 1
-UCHAR = 1
-USHORT = 1
-SHORT = 1
-ULONGLONG = 2
-POINTER = 3
-STRING = 4
-BOOLEAN = 8
-BOOL = 8
-
-def macos_kernel_api(param_num=None, params=None, passthru=False):
+def macos_kernel_api(param_num=None, params={}, passthru=False):
     def decorator(func):
-        def wrapper(*args, **kwargs):
-            ql = args[0]
-            return ql.os.x8664_fastcall(param_num, params, func, args, kwargs, passthru)
-        return wrapper
-    return decorator
+        def wrapper(ql: Qiling, pc: int, api_name: str):
+            onenter = ql.os.user_defined_api[QL_INTERCEPT.ENTER].get(api_name)
+            onexit = ql.os.user_defined_api[QL_INTERCEPT.EXIT].get(api_name)
 
+            return ql.os.call(pc, func, params, onenter, onexit, passthru=passthru)
+
+        return wrapper
+
+    return decorator
