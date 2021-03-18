ecbb21c9e23547055af50baf308c1fc0a60bd6ed
elicn <elichn@gmail.com>
2021-03-18 06:19:16 +0200

    Cleanup Linux syscalls namespace

--- qiling/os/linux/syscall.py
+++ qiling/os/linux/syscall.py
@@ -3,15 +3,11 @@
 # Cross Platform and Multi Architecture Advanced Binary Emulation Framework
 #
 
-from unicorn.arm_const import *
-from unicorn.x86_const import *
-from unicorn.mips_const import *
-
+from qiling import Qiling
 from qiling.arch.x86_const import *
-from qiling.const import *
+from qiling.const import QL_ARCH
 
-
-def ql_syscall_set_thread_area(ql, u_info_addr, *args, **kw):
+def ql_syscall_set_thread_area(ql: Qiling, u_info_addr, *args, **kw):
     if ql.archtype == QL_ARCH.X86:
         GDT_ENTRY_TLS_MIN = 12
         GDT_ENTRY_TLS_MAX = 14
@@ -26,7 +22,7 @@ def ql_syscall_set_thread_area(ql, u_info_addr, *args, **kw):
         if index == -1:
             index = ql.os.gdtm.get_free_idx(12)
 
-        if index == -1 or index < 12 or index > 14:
+        if index == -1 or index < GDT_ENTRY_TLS_MIN or index > GDT_ENTRY_TLS_MAX:
             ql.log.warning(f"Wrong index {index} from address {hex(u_info_addr)}")
             return -1
         else:
@@ -41,7 +37,7 @@ def ql_syscall_set_thread_area(ql, u_info_addr, *args, **kw):
         ql.reg.v0 = 0
         ql.reg.a3 = 0
         ql.log.debug ("set_thread_area(0x%x)" % u_info_addr)
-    
+
     return 0
 
 
