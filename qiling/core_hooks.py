4dab6fa74402562751fce565381baf26105d7aa0
kabeor <1597915586@qq.com>
2021-04-07 10:07:35 +0800

    fix custom engine import issue

--- qiling/core_hooks.py
+++ qiling/core_hooks.py
@@ -15,7 +15,7 @@ from .core_hooks_types import Hook, HookAddr, HookIntr, HookRet
 from .utils import catch_KeyboardInterrupt
 from .const import QL_HOOK_BLOCK
 from .exception import QlErrorCoreHook
-
+# from .engine.engine_hooks import engine_hook_address, engine_hook_del, engine_hook_insn, ql_engine_hooks
 
 # Don't assume self is Qiling.
 class QlCoreHooks:
@@ -44,8 +44,8 @@ class QlCoreHooks:
         self.hook_mem_read_after_fuc = None
         self.hook_insn_invalid_fuc = None
 
-        if self.custom_engine:
-            from .engine.engine_hooks import engine_hook_address, engine_hook_del, engine_hook_insn, ql_engine_hooks
+        # if self.custom_engine:
+        #     from .engine.engine_hooks import engine_hook_address, engine_hook_del, engine_hook_insn, ql_engine_hooks
 
     ########################
     # Callback definitions #
@@ -245,6 +245,7 @@ class QlCoreHooks:
 
     def hook_code(self, callback, user_data=None, begin=1, end=0):
         if self.custom_engine:
+            from .engine.engine_hooks import ql_engine_hooks
             return ql_engine_hooks(self, 'ENGINE_HOOK_CODE', callback, user_data, begin, end)
         return self.ql_hook(UC_HOOK_CODE, callback, user_data, begin, end)
 
@@ -285,6 +286,7 @@ class QlCoreHooks:
         h = HookAddr(callback, address, user_data)
         
         if self.custom_engine:
+            from .engine.engine_hooks import engine_hook_address
             return engine_hook_address(self, 'ENGINE_HOOK_ADDR', h, address)
 
         if address not in self._addr_hook_fuc.keys():
@@ -320,6 +322,7 @@ class QlCoreHooks:
 
     def hook_insn(self, callback, arg1, user_data=None, begin=1, end=0):
         if self.custom_engine:
+            from .engine.engine_hooks import engine_hook_insn
             return engine_hook_insn(self, 'ENGINE_HOOK_INSN', callback, arg1, user_data, begin, end)
         return self.ql_hook(UC_HOOK_INSN, callback, user_data, begin, end, arg1)
 
@@ -335,6 +338,7 @@ class QlCoreHooks:
             hook_type, h = args
 
         if self.custom_engine: 
+            from .engine.engine_hooks import engine_hook_del
             return engine_hook_del(hook_type, h)
 
         base_type = [
