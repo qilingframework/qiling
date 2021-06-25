4d4cce2a49ddb9b1834872fc18a64cdffc9284a2
Daniel Moos <daniel.moos@gmail.com>
2021-06-25 16:09:13 +0200

    Start implementation of sys_conf message handling
    The first message type of the System Manager is sys_conf. This code handles
    the sys_conf(GET, STRING, LIBPATH) case. This is needed to run dynamically
    linked programs because the loader issuses this call to get the folder where
    he searches for the required libraries.
    The path /usr/lib of the rootfs is hard-coded here.

--- qiling/os/qnx/message.py
+++ qiling/os/qnx/message.py
@@ -39,6 +39,60 @@ def ql_qnx_msg_mem_map(ql, coid, smsg, sparts, rmsg, rparts, *args, **kw):
 
     return 0
 
+def ql_qnx_msg_sys_conf(ql, coid, smsg, sparts, rmsg, rparts, *args, **kw):
+    (type_, subtype_, cmd_, name_, spare, value) = unpack("<HHiiiq", get_message_body(ql, smsg, sparts))
+
+    # services/system/public/sys/sysmsg.h
+    if subtype_ == 0:
+        subtype = "_SYS_SUB_GET"
+    elif subtype_ == 1:
+        subtype = "_SYS_SUB_SET"
+    else:
+        raise NotImplementedError("subtype not implemented")
+
+    # lib/c/public/sys/conf.h
+    if cmd_ == (1 << 20):
+        # checking for string
+        cmd = "_CONF_STR"
+    elif cmd_ == (2 << 20):
+        # checking for number
+        cmd = "_CONF_NUM"
+    else:
+        raise NotImplementedError("cmd type not implemented")
+
+    # lib/c/public/confname.h
+    if name_ == 200:
+        # search path for dynamic loader, e.g. /usr/lib
+        name = "_CS_LIBPATH"
+    elif name_ == 201:
+        name = "_CS_DOMAIN"
+    elif name_ == 202:
+        name = "_CS_RESOLVE"
+    elif name_ == 203:
+        name = "_CS_TIMEZONE"
+    elif name_ == 204:
+        name = "_CS_LOCALE"
+    else:
+        raise NotImplementedError("cmd type not implemented")
+
+    # output syscall with decoded arguments
+    ql.log.debug("sys_conf(subtype=%s, cmd=%s, name=%s, spare=%d, value=%d)" % (subtype, cmd, name, spare, value))
+
+    # sys_conf(_SYS_SUB_GET, _CONF_STR, _CS_LIBPATH)
+    if subtype_ == 0 and cmd_ == (1 << 20) and name_ == 200:
+        libpath = "/usr/lib"
+        # first iov_t
+        iov_base = ql.unpack32(ql.mem.read(rmsg, 4))
+        iov_len = ql.unpack32(ql.mem.read(rmsg + 4, 4))
+        ql.mem.write(iov_base, pack("<IIIiq", 0, 0, 0, 0, len(libpath)))
+        if value != 0:
+            # second iov_t
+            iov_base = ql.unpack32(ql.mem.read(rmsg + 8, 4))
+            iov_len = ql.unpack32(ql.mem.read(rmsg + 12, 4))
+            ql.mem.write(iov_base, pack("<s", libpath.encode("utf-8")))
+
+    return 0
+
 def ql_syscall_connect_attach(ql, nd, pid, chid, index, flags, *args, **kw):
     return 42
 
