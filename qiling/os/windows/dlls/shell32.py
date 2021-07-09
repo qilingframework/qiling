576d2c8e72ce935843756c31358db40d650cf455
elicn <elichn@gmail.com>
2021-07-09 11:06:53 +0300

    Refactor shell32

--- qiling/os/windows/dlls/shell32.py
+++ qiling/os/windows/dlls/shell32.py
@@ -3,20 +3,28 @@
 # Cross Platform and Multi Architecture Advanced Binary Emulation Framework
 #
 
-import struct, time, os
+import os
+from typing import Sequence
 
-
+from qiling import Qiling
+from qiling.os.windows.api import *
 from qiling.os.windows.const import *
-from qiling.os.const import *
 from qiling.os.windows.fncc import *
-from qiling.os.windows.utils import *
-from qiling.os.windows.thread import *
-from qiling.os.windows.handle import *
-from qiling.exception import *
-from qiling.os.windows.structs import *
-from qiling.const import *
 
-dllname = 'shell32_dll'
+from qiling.os.windows.handle import Handle
+from qiling.os.windows.thread import QlWindowsThread
+from qiling.exception import QlErrorNotImplemented
+from qiling.os.windows.structs import ShellExecuteInfoA
+
+def _SHGetFileInfo(ql: Qiling, address: int, params) -> int:
+    uFlags = params["uFlags"]
+
+    if uFlags == SHGFI_LARGEICON:
+        # TODO: is this a valid DWORD_PTR ?
+        return 1
+
+    ql.log.debug(uFlags)
+    raise QlErrorNotImplemented("API not implemented")
 
 # DWORD_PTR SHGetFileInfoA(
 #   LPCSTR     pszPath,
@@ -25,10 +33,15 @@ dllname = 'shell32_dll'
 #   UINT        cbFileInfo,
 #   UINT        uFlags
 # );
-@winsdkapi(cc=STDCALL, dllname=dllname)
-def hook_SHGetFileInfoA(ql, address, params):
-    return hook_SHGetFileInfoW.__wrapped__(ql, address, params)
-
+@winsdkapi_new(cc=STDCALL, params={
+    'pszPath'          : LPCSTR,
+    'dwFileAttributes' : DWORD,
+    'psfi'             : POINTER,
+    'cbFileInfo'       : UINT,
+    'uFlags'           : UINT
+})
+def hook_SHGetFileInfoA(ql: Qiling, address: int, params):
+    return _SHGetFileInfo(ql, address, params)
 
 # DWORD_PTR SHGetFileInfoW(
 #   LPCWSTR     pszPath,
@@ -37,41 +50,42 @@ def hook_SHGetFileInfoA(ql, address, params):
 #   UINT        cbFileInfo,
 #   UINT        uFlags
 # );
-@winsdkapi(cc=STDCALL, dllname=dllname)
-def hook_SHGetFileInfoW(ql, address, params):
-    flags = params["uFlags"]
-    if flags == SHGFI_LARGEICON:
-        return 1
-    else:
-        ql.log.debug(flags)
-        raise QlErrorNotImplemented("API not implemented")
+@winsdkapi_new(cc=STDCALL, params={
+    'pszPath'          : LPCWSTR,
+    'dwFileAttributes' : DWORD,
+    'psfi'             : POINTER,
+    'cbFileInfo'       : UINT,
+    'uFlags'           : UINT
+})
+def hook_SHGetFileInfoW(ql: Qiling, address: int, params):
+    return _SHGetFileInfo(ql, address, params)
 
+def _ShellExecute(ql: Qiling, obj: ShellExecuteInfoA):
+    def __wstr(shellex: Sequence):
+        return ql.os.utils.read_wstring(shellex[0])
 
-def _ShellExecute(ql, obj: ShellExecuteInfoA):
+    ql.log.debug(f'Target executed a shell command!')
+    ql.log.debug(f' | Operation  : "{__wstr(obj.verb)}"')
+    ql.log.debug(f' | Parameters : "{__wstr(obj.params)}"')
+    ql.log.debug(f' | File       : "{__wstr(obj.file)}"')
+    ql.log.debug(f' | Directory  : "{__wstr(obj.dir)}"')
 
-    operation = ql.os.utils.read_wstring(obj.verb[0]) if obj.verb[0] != 0 else ""
-    params = ql.os.utils.read_wstring(obj.params[0]) if obj.params[0] != 0 else ""
-    file = ql.os.utils.read_wstring(obj.file[0]) if obj.file[0] != 0 else ""
-    directory = ql.os.utils.read_wstring(obj.dir[0]) if obj.dir[0] != 0 else ""
-
-    ql.log.debug("Target executed a shell command!")
-    ql.log.debug("Operation: %s " % operation)
-    ql.log.debug("Parameters: %s " % params)
-    ql.log.debug("File: %s " % file)
-    ql.log.debug("Directory: %s " % directory)
     if obj.show[0] == SW_HIDE:
-        ql.log.debug("With an hidden window")
+        ql.log.debug(" | With an hidden window")
+
     process = QlWindowsThread(ql, status=0, isFake=True)
     handle = Handle(obj=process)
     ql.os.handle_manager.append(handle)
-    return handle
 
+    return handle
 
 # BOOL ShellExecuteExW(
 #   SHELLEXECUTEINFOA *pExecInfo
 # );
-@winsdkapi(cc=STDCALL, dllname=dllname)
-def hook_ShellExecuteExW(ql, address, params):
+@winsdkapi_new(cc=STDCALL, params={
+    'pExecInfo' : POINTER
+})
+def hook_ShellExecuteExW(ql: Qiling, address: int, params):
     pointer = params["pExecInfo"]
 
     shellInfo = ShellExecuteInfoA(ql)
@@ -83,8 +97,8 @@ def hook_ShellExecuteExW(ql, address, params):
     shellInfo.instApp[0] = 0x21
     shellInfo.process[0] = handle.id
     shellInfo.write(pointer)
-    return 1
 
+    return 1
 
 # HINSTANCE ShellExecuteW(
 #   HWND    hwnd,
@@ -94,13 +108,26 @@ def hook_ShellExecuteExW(ql, address, params):
 #   LPCWSTR lpDirectory,
 #   INT     nShowCmd
 # );
-@winsdkapi(cc=STDCALL, dllname=dllname, replace_params_type={'LPCWSTR': 'POINTER'})
-def hook_ShellExecuteW(ql, address, params):
-    shellInfo = ShellExecuteInfoA(ql, hwnd=params["hwnd"], lpVerb=params["lpOperation"], lpFile=params["lpFile"],
-                                  lpParams=params["lpParameters"], lpDir=params["lpDirectory"], show=params["nShowCmd"])
-    _ = _ShellExecute(ql, shellInfo)
-    return 33
+@winsdkapi_new(cc=STDCALL, params={
+    'hwnd'         : HWND,
+    'lpOperation'  : POINTER, # LPCWSTR
+    'lpFile'       : POINTER, # LPCWSTR
+    'lpParameters' : POINTER, # LPCWSTR
+    'lpDirectory'  : POINTER, # LPCWSTR
+    'nShowCmd'     : INT
+})
+def hook_ShellExecuteW(ql: Qiling, address: int, params):
+    _ShellExecute(ql, ShellExecuteInfoA(
+        ql,
+        hwnd=params["hwnd"],
+        lpVerb=params["lpOperation"],
+        lpFile=params["lpFile"],
+        lpParams=params["lpParameters"],
+        lpDir=params["lpDirectory"],
+        show=params["nShowCmd"]
+    ))
 
+    return 33
 
 # BOOL SHGetSpecialFolderPathW(
 #   HWND   hwnd,
@@ -108,25 +135,35 @@ def hook_ShellExecuteW(ql, address, params):
 #   int    csidl,
 #   BOOL   fCreate
 # );
-@winsdkapi(cc=STDCALL, dllname=dllname)
-def hook_SHGetSpecialFolderPathW(ql, address, params):
+@winsdkapi_new(cc=STDCALL, params={
+    'hwnd'    : HWND,
+    'pszPath' : LPWSTR,
+    'csidl'   : INT,
+    'fCreate' : BOOL
+})
+def hook_SHGetSpecialFolderPathW(ql: Qiling, address: int, params):
     directory_id = params["csidl"]
     dst = params["pszPath"]
+
     if directory_id == CSIDL_COMMON_APPDATA:
         path = str(ql.os.userprofile + "AppData\\")
         # We always create the directory
         appdata_dir = path.split("C:\\")[1].replace("\\", "/")
         ql.log.debug("dir path: %s" % path)
+
         path_emulated = os.path.join(ql.rootfs, appdata_dir)
         ql.log.debug("emulated path: %s" % path_emulated)
+
         ql.mem.write(dst, (path + "\x00").encode("utf-16le"))
+
         # FIXME: Somehow winodws path is wrong
         if not os.path.exists(path_emulated):
             try:
                 os.makedirs(path_emulated, 0o755)
                 ql.log.debug("os.makedirs completed")
             except OSError:
-                ql.log.debug("os.makedirs fail")
+                ql.log.debug("os.makedirs failed")
     else:
         raise QlErrorNotImplemented("API not implemented")
+
     return 1
