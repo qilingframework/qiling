cc3e02303d046fd3af5cb51888206f370bc9d8f7
elicn <elichn@gmail.com>
2021-07-09 10:39:53 +0300

    Refactor mscoree

--- qiling/os/windows/dlls/mscoree.py
+++ qiling/os/windows/dlls/mscoree.py
@@ -2,21 +2,18 @@
 #
 # Cross Platform and Multi Architecture Advanced Binary Emulation Framework
 #
-import struct
-import base64
-from qiling.os.windows.fncc import *
-from qiling.os.const import *
-from qiling.os.windows.utils import *
-from qiling.os.windows.handle import *
-from qiling.os.windows.const import *
 
-dllname = 'mscoree_dll'
+from qiling import Qiling
+from qiling.os.windows.api import *
+from qiling.os.windows.fncc import *
 
 # void STDMETHODCALLTYPE CorExitProcess (
 #   int  exitCode
 # );
-@winsdkapi(cc=STDCALL, dllname=dllname, replace_params_type={'int': 'DWORD'})
-def hook_CorExitProcess(ql, address, params):
+@winsdkapi_new(cc=STDCALL, params={
+    'exitCode' : INT
+})
+def hook_CorExitProcess(ql: Qiling, address: int, params):
     ql.emu_stop()
     ql.os.PE_RUN = False
 
