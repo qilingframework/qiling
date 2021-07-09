3df1376d0d8ec0fa8c9f0ae0105e769804b27bb5
elicn <elichn@gmail.com>
2021-07-09 08:33:35 +0300

    Refactor ntdll

--- qiling/os/windows/dlls/ntdll.py
+++ qiling/os/windows/dlls/ntdll.py
@@ -3,59 +3,74 @@
 # Cross Platform and Multi Architecture Advanced Binary Emulation Framework
 #
 
-import struct
-
-
-from qiling.os.windows.const import *
+from qiling import Qiling
+from qiling.os.windows.api import *
 from qiling.os.windows.fncc import *
-from qiling.os.const import *
-from qiling.os.windows.utils import *
-from qiling.os.windows.thread import *
-from qiling.os.windows.handle import *
-from qiling.exception import *
-import qiling.os.windows.structs
 
-dllname = 'ntdll_dll'
+from qiling.const import QL_ARCH
+from qiling.exception import *
+from qiling.os.const import *
+from qiling.os.windows.const import *
+from qiling.os.windows.utils import *
+from qiling.os.windows.handle import *
+from qiling.os.windows import structs
 
 # void *memcpy(
 #    void *dest,
 #    const void *src,
 #    size_t count
 # );
-@winsdkapi(cc=CDECL, dllname=dllname, replace_params={"dest": POINTER, "src": POINTER, "count": UINT})
-def hook_memcpy(ql, address, params):
+@winsdkapi_new(cc=CDECL, params={
+    'dest'  : POINTER,
+    'src'   : POINTER,
+    'count' : UINT
+})
+def hook_memcpy(ql: Qiling, address: int, params):
+    dest = params['dest']
+    src = params['src']
+    count = params['count']
+
     try:
-        data = bytes(ql.mem.read(params['src'], params['count']))
-        ql.mem.write(params['dest'], data)
+        data = bytes(ql.mem.read(src, count))
+        ql.mem.write(dest, data)
     except Exception as e:
         ql.log.exception("")
-    return params['dest']
 
+    return dest
 
-def _QueryInformationProcess(ql, address, params):
+def _QueryInformationProcess(ql: Qiling, address: int, params):
     flag = params["ProcessInformationClass"]
     dst = params["ProcessInformation"]
     pt_res = params["ReturnLength"]
+
     if flag == ProcessDebugFlags:
         value = b"\x01" * 0x4
+
     elif flag == ProcessDebugPort:
         value = b"\x00" * 0x4
+
     elif flag == ProcessDebugObjectHandle:
         return STATUS_PORT_NOT_SET
+
     elif flag == ProcessBasicInformation:
-        pbi = qiling.os.windows.structs.ProcessBasicInformation(ql, exitStatus=0,
-                                                                pebBaseAddress=ql.os.heap_base_address, affinityMask=0,
-                                                                basePriority=0,
-                                                                uniqueId=ql.os.profile.getint("KERNEL", "pid"),
-                                                                parentPid=ql.os.profile.getint("KERNEL", "parent_pid"))
+        pbi = structs.ProcessBasicInformation(ql,
+            exitStatus=0,
+            pebBaseAddress=ql.os.heap_base_address, affinityMask=0,
+            basePriority=0,
+            uniqueId=ql.os.profile.getint("KERNEL", "pid"),
+            parentPid=ql.os.profile.getint("KERNEL", "parent_pid")
+        )
+
         addr = ql.os.heap.alloc(pbi.size)
         pbi.write(addr)
         value = addr.to_bytes(ql.pointersize, "little")
     else:
         ql.log.debug(str(flag))
         raise QlErrorNotImplemented("API not implemented")
+
     ql.log.debug("The target is checking the debugger via QueryInformationProcess ")
     ql.mem.write(dst, value)
+
     if pt_res != 0:
         ql.mem.write(pt_res, 0x8.to_bytes(1, byteorder="little"))
 
@@ -68,14 +83,17 @@ def _QueryInformationProcess(ql, address, params):
 #   _In_      ULONG            ProcessInformationLength,
 #   _Out_opt_ PULONG           ReturnLength
 # );
-@winsdkapi(cc=CDECL, dllname=dllname,
-           replace_params={"ProcessHandle": HANDLE, "ProcessInformationClass": INT, "ProcessInformation": POINTER,
-                      "ProcessInformationLength": UINT, "ReturnLength": POINTER})
-def hook_ZwQueryInformationProcess(ql, address, params):
+@winsdkapi_new(cc=CDECL, params={
+    'ProcessHandle'            : HANDLE,
+    'ProcessInformationClass'  : PROCESSINFOCLASS,
+    'ProcessInformation'       : PVOID,
+    'ProcessInformationLength' : ULONG,
+    'ReturnLength'             : PULONG
+})
+def hook_ZwQueryInformationProcess(ql: Qiling, address: int, params):
     # TODO have no idea if is cdecl or stdcall
 
     _QueryInformationProcess(ql, address, params)
-
 
 # __kernel_entry NTSTATUS NtQueryInformationProcess(
 #   IN HANDLE           ProcessHandle,
@@ -84,16 +102,23 @@ def hook_ZwQueryInformationProcess(ql, address, params):
 #   IN ULONG            ProcessInformationLength,
 #   OUT PULONG          ReturnLength
 # );
-@winsdkapi(cc=STDCALL, dllname=dllname)
-def hook_NtQueryInformationProcess(ql, address, params):
+@winsdkapi_new(cc=STDCALL, params={
+    'ProcessHandle'            : HANDLE,
+    'ProcessInformationClass'  : PROCESSINFOCLASS,
+    'ProcessInformation'       : PVOID,
+    'ProcessInformationLength' : ULONG,
+    'ReturnLength'             : PULONG
+})
+def hook_NtQueryInformationProcess(ql: Qiling, address: int, params):
     # TODO have no idea if is cdecl or stdcall
 
     _QueryInformationProcess(ql, address, params)
 
-def _QuerySystemInformation(ql, address, params):
+def _QuerySystemInformation(ql: Qiling, address: int, params):
     siClass = params["SystemInformationClass"]
     pt_res = params["ReturnLength"]
     dst = params["SystemInformation"]
+
     if (siClass == SystemBasicInformation):
         bufferLength = params["SystemInformationLength"]
 
@@ -102,7 +127,7 @@ def _QuerySystemInformation(ql, address, params):
             QL_ARCH.X8664: 0x7FFFFFFEFFFF
         }[ql.archtype]
 
-        sbi = qiling.os.windows.structs.SystemBasicInforation(
+        sbi = structs.SystemBasicInforation(
             ql,
             Reserved=0,
             TimerResolution=156250,
@@ -114,20 +139,22 @@ def _QuerySystemInformation(ql, address, params):
             MinimumUserModeAddress=0x10000,
             MaximumUserModeAddress=max_uaddr,
             ActiveProcessorsAffinityMask=0x3F,
-            NumberOfProcessors=0x6)
+            NumberOfProcessors=0x6
+        )
 
         if (bufferLength==sbi.size):
             sbi.write(dst)
+
             if pt_res != 0:
                 ql.mem.write(pt_res, sbi.size.to_bytes(1, byteorder="little"))
         else:
             if pt_res != 0:
                 ql.mem.write(pt_res, sbi.size.to_bytes(1, byteorder="little"))
+
             return STATUS_INFO_LENGTH_MISMATCH
     else:
         ql.log.debug(str(siClass))
         raise QlErrorNotImplemented("API not implemented")
-
 
     return STATUS_SUCCESS
 
@@ -137,10 +164,13 @@ def _QuerySystemInformation(ql, address, params):
 #   IN ULONG                    SystemInformationLength,
 #   OUT PULONG                  ReturnLength
 # );
-@winsdkapi(cc=STDCALL, dllname=dllname,
-                replace_params={"SystemInformationClass": UINT, "SystemInformation": POINTER, "SystemInformationLength": SIZE_T,
-                    "ReturnLength": POINTER})
-def hook_NtQuerySystemInformation(ql, address, params):
+@winsdkapi_new(cc=STDCALL, params={
+    'SystemInformationClass'  : SYSTEM_INFORMATION_CLASS,
+    'SystemInformation'       : PVOID,
+    'SystemInformationLength' : ULONG,
+    'ReturnLength'            : PULONG
+})
+def hook_NtQuerySystemInformation(ql: Qiling, address: int, params):
     # In minwindef.h
     # #define WINAPI      __stdcall
 
@@ -152,10 +182,13 @@ def hook_NtQuerySystemInformation(ql, address, params):
 #   IN ULONG                    SystemInformationLength,
 #   OUT PULONG                  ReturnLength
 # );
-@winsdkapi(cc=STDCALL, dllname=dllname,
-                replace_params={"SystemInformationClass": UINT, "SystemInformation": POINTER, "SystemInformationLength": SIZE_T,
-                    "ReturnLength": POINTER})
-def hook_ZwQuerySystemInformation(ql, address, params):
+@winsdkapi_new(cc=STDCALL, params={
+    'SystemInformationClass'  : SYSTEM_INFORMATION_CLASS,
+    'SystemInformation'       : PVOID,
+    'SystemInformationLength' : ULONG,
+    'ReturnLength'            : PULONG
+})
+def hook_ZwQuerySystemInformation(ql: Qiling, address: int, params):
     # In minwindef.h
     # #define WINAPI      __stdcall
 
@@ -167,14 +200,17 @@ def hook_ZwQuerySystemInformation(ql, address, params):
 #     ObjectAttributes: POBJECT_ATTRIBUTES,
 #     Flags: ULONG
 # ) -> NTSTATUS
-@winsdkapi(cc=STDCALL, dllname=dllname, replace_params={"DebugObjectHandle": HANDLE, "DesiredAccess": INT,
-                                                   "ObjectAttributes": POINTER, "Flags": ULONGLONG})
-def hook_ZwCreateDebugObject(ql, address, params):
+@winsdkapi_new(cc=STDCALL, params={
+    'DebugObjectHandle' : PHANDLE,
+    'DesiredAccess'     : ACCESS_MASK,
+    'ObjectAttributes'  : POBJECT_ATTRIBUTES,
+    'Flags'             : ULONG
+})
+def hook_ZwCreateDebugObject(ql: Qiling, address: int, params):
     # FIXME: find documentation, almost none was found online, and create the correct object
     handle = Handle(id=params["DebugObjectHandle"])
     ql.os.handle_manager.append(handle)
     return STATUS_SUCCESS
-
 
 # __kernel_entry NTSYSCALLAPI NTSTATUS NtQueryObject(
 #   HANDLE                   Handle,
@@ -183,36 +219,45 @@ def hook_ZwCreateDebugObject(ql, address, params):
 #   ULONG                    ObjectInformationLength,
 #   PULONG                   ReturnLength
 # );
-@winsdkapi(cc=STDCALL, dllname=dllname)
-def hook_ZwQueryObject(ql, address, params):
+@winsdkapi_new(cc=STDCALL, params={
+    'Handle'                  : HANDLE,
+    'ObjectInformationClass'  : OBJECT_INFORMATION_CLASS,
+    'ObjectInformation'       : PVOID,
+    'ObjectInformationLength' : ULONG,
+    'ReturnLength'            : PULONG
+})
+def hook_ZwQueryObject(ql: Qiling, address: int, params):
     infoClass = params["ObjectInformationClass"]
     dest = params["ObjectInformation"]
     size_dest = params["ReturnLength"]
     string = "DebugObject".encode("utf-16le")
+
     string_addr = ql.os.heap.alloc(len(string))
     ql.log.debug(str(string_addr))
     ql.log.debug(str(string))
     ql.mem.write(string_addr, string)
-    us = qiling.os.windows.structs.UnicodeString(ql, length=len(string), maxLength=len(string),
-                                                 buffer=string_addr)
+    us = structs.UnicodeString(ql, len(string), len(string), string_addr)
 
     if infoClass == ObjectTypeInformation:
-        res = qiling.os.windows.structs.ObjectTypeInformation(ql, us, 1, 1)
+        res = structs.ObjectTypeInformation(ql, us, 1, 1)
+
     elif infoClass == ObjectAllTypesInformation:
         # FIXME: there is an error in how these structs are read by al-khaser. Have no idea on where, so we are
         #  bypassing it
-        # oti = qiling.os.windows.structs.ObjectTypeInformation(ql, us, 1, 1)
-        # res = qiling.os.windows.structs.ObjectAllTypesInformation(ql, 2, oti)
+        # oti = structs.ObjectTypeInformation(ql, us, 1, 1)
+        # res = structs.ObjectAllTypesInformation(ql, 2, oti)
         return 1
+
     else:
         raise QlErrorNotImplemented("API not implemented")
+
     if dest != 0 and params["Handle"] != 0:
         res.write(dest)
+
     if size_dest != 0:
         ql.mem.write(size_dest, res.size.to_bytes(4, "little"))
 
     return STATUS_SUCCESS
-
 
 # NTSYSAPI NTSTATUS NTAPI NtSetInformatonProcess(
 #   _In_      HANDLE           ProcessHandle,
@@ -220,42 +265,58 @@ def hook_ZwQueryObject(ql, address, params):
 #   _In_      PVOID            ProcessInformation
 #   _In_      ULONG            ProcessInformationLength
 # );
-@winsdkapi(cc=STDCALL, dllname=dllname,
-            replace_params={"ProcessHandle": HANDLE, "ProcessInformationClass": INT, "ProcessInformation": POINTER,
-                        "ProcessInformationLength": UINT})
-def hook_ZwSetInformationProcess(ql, address, params):
+@winsdkapi_new(cc=STDCALL, params={
+    'ProcessHandle'            : HANDLE,
+    'ProcessInformationClass'  : PROCESSINFOCLASS,
+    'ProcessInformation'       : PVOID,
+    'ProcessInformationLength' : ULONG
+})
+def hook_ZwSetInformationProcess(ql: Qiling, address: int, params):
     _SetInformationProcess(ql, address, params)
 
-def hook_NtSetInformationProcess(ql, address, params):
+@winsdkapi_new(cc=STDCALL, params={
+    'ProcessHandle'            : HANDLE,
+    'ProcessInformationClass'  : PROCESSINFOCLASS,
+    'ProcessInformation'       : PVOID,
+    'ProcessInformationLength' : ULONG
+})
+def hook_NtSetInformationProcess(ql: Qiling, address: int, params):
     _SetInformationProcess(ql, address, params)
 
-def _SetInformationProcess(ql, address, params):
+def _SetInformationProcess(ql: Qiling, address: int, params):
     process = params["ProcessHandle"]
     flag = params["ProcessInformationClass"]
     dst = params["ProcessInformation"]
     pt_res = params["ReturnLength"]
-    
+
     if flag == ProcessDebugFlags:
         value = b"\x01" * 0x4
+
     elif flag == ProcessDebugPort:
         value = b"\x00" * 0x4
+
     elif flag == ProcessDebugObjectHandle:
         return STATUS_PORT_NOT_SET
+
     elif flag == ProcessBreakOnTermination:
             ql.log.debug("The target may be attempting modify a the 'critical' flag of the process")  
+
     elif flag  == ProcessExecuteFlags:
         ql.log.debug("The target may be attempting to modify DEP for the process")
+
         if dst != 0:
             ql.mem.write(dst, 0x0.to_bytes(1, byteorder="little"))
 
     elif flag == ProcessBasicInformation:
-        pbi = qiling.os.windows.structs.ProcessBasicInformation(
-            ql, exitStatus=0,
+        pbi = structs.ProcessBasicInformation(
+            ql,
+            exitStatus=0,
             pebBaseAddress=ql.os.heap_base_address, affinityMask=0,
             basePriority=0,
             uniqueId=ql.os.profile.getint("KERNEL", "pid"),
             parentPid=ql.os.profile.geting("KERNEL", "parent_pid")
         )
+
         ql.log.debug("The target may be attempting to modify the PEB debug flag")
         addr = ql.os.heap.alloc(pbi.size)
         pbi.write(addr)
@@ -264,6 +325,8 @@ def _SetInformationProcess(ql, address, params):
         ql.log.debug(str(flag))
         raise QlErrorNotImplemented("API not implemented")
 
+    # TODO: value is never used after assignment
+
     return STATUS_SUCCESS
 
 # NTSYSAPI
@@ -271,66 +334,75 @@ def _SetInformationProcess(ql, address, params):
 # NTAPI
 # NtYieldExecution(
 #  );
-@winsdkapi(cc=STDCALL, dllname=dllname)
-def hook_ZwYieldExecution(ql, address, params):
+@winsdkapi_new(cc=STDCALL, params={})
+def hook_ZwYieldExecution(ql: Qiling, address: int, params):
     # FIXME: offer timeslice of this thread
     return STATUS_NO_YIELD_PERFORMED
-
 
 # NTSTATUS LdrGetProcedureAddress(
 #  IN HMODULE              ModuleHandle,
 #  IN PANSI_STRING         FunctionName OPTIONAL,
 #  IN WORD                 Oridinal OPTIONAL,
 #  OUT PVOID               *FunctionAddress );
-@winsdkapi(cc=STDCALL, dllname=dllname, replace_params={"ModuleHandle": POINTER,
-    "FunctionName": STRING, "Ordinal": UINT, "FunctionAddress": POINTER})
-def hook_LdrGetProcedureAddress(ql, address, params):
-    if params['FunctionName']:
-        identifier = bytes(params["lpProcName"], 'ascii')
-    else:
-        identifier = params['Ordinal']
+@winsdkapi_new(cc=STDCALL, params={
+    'ModuleHandle'    : HMODULE,
+    'FunctionName'    : PANSI_STRING,
+    'Ordinal'         : WORD,
+    'FunctionAddress' : POINTER
+})
+def hook_LdrGetProcedureAddress(ql: Qiling, address: int, params):
+    ModuleHandle = params['ModuleHandle']
+    FunctionName = params['FunctionName']
+    Ordinal = params['Ordinal']
+    FunctionAddress = params['FunctionAddress']
+
     # Check if dll is loaded
-    try:
-        dll_name = [key for key, value in ql.loader.dlls.items() if value == params['ModuleHandle']][0]
-    except IndexError as ie:
-        ql.log.info('Failed to import function "%s" with handle 0x%X' % (lpProcName, params['ModuleHandle']))
+    dll_name = next((key for key, value in ql.loader.dlls.items() if value == ModuleHandle), None)
+
+    if dll_name is None:
+        ql.log.debug(f'Could not find specified handle {ModuleHandle} in loaded DLL')
         return 0
+
+    identifier = bytes(FunctionName, 'ascii') if FunctionName else Ordinal
 
     if identifier in ql.loader.import_address_table[dll_name]:
         addr = ql.loader.import_address_table[dll_name][identifier]
-        ql.mem.write(addr.to_bytes(length=ql.pointersize, byteorder='little'), params['FunctionAddress'])
+        ql.mem.write(addr.to_bytes(length=ql.pointersize, byteorder='little'), FunctionAddress)
         return 0
 
     return 0xFFFFFFFF
-
 
 # NTSYSAPI PVOID RtlAllocateHeap(
 #  PVOID  HeapHandle,
 #  ULONG  Flags,
 #  SIZE_T Size
 # );
-@winsdkapi(cc=STDCALL, dllname=dllname, replace_params={"HeapHandle": POINTER,
-    "Flags": UINT,"Size": SIZE_T})
-def hook_RtlAllocateHeap(ql, address, params):
-    ret = ql.os.heap.alloc(params["Size"])
-    return ret
-
+@winsdkapi_new(cc=STDCALL, params={
+    'HeapHandle' : PVOID,
+    'Flags'      : ULONG,
+    'Size'       : SIZE_T
+})
+def hook_RtlAllocateHeap(ql: Qiling, address: int, params):
+    return ql.os.heap.alloc(params["Size"])
 
 # wchar_t* wcsstr( const wchar_t* dest, const wchar_t* src );
-@winsdkapi(cc=STDCALL, dllname=dllname, replace_params={"dest": POINTER, "src": WSTRING})
-def hook_wcsstr(ql, address, params):
+@winsdkapi_new(cc=STDCALL, params={
+    'dest' : POINTER, # WSTRING
+    'src'  : WSTRING
+})
+def hook_wcsstr(ql: Qiling, address: int, params):
     dest = params["dest"]
-    value = ql.os.utils.read_wstring(dest)
-    params["dest"] = value
     src = params["src"]
-    if src in value:
-        pos = value.index(src)
-        return dest + pos
+
+    dest_str = ql.os.utils.read_wstring(dest)
+
+    if src in dest_str:
+        return dest + dest_str.index(src)
+
     return 0
 
-
 # HANDLE CsrGetProcessId();
-@winsdkapi(cc=STDCALL, dllname=dllname)
-def hook_CsrGetProcessId(ql, address, params):
+@winsdkapi_new(cc=STDCALL, params={})
+def hook_CsrGetProcessId(ql: Qiling, address: int, params):
     pid = ql.os.profile["PROCESSES"].getint("csrss.exe", fallback=12345)
     return pid
