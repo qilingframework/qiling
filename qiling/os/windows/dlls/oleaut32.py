668acb816638704c6fc285474aa68b967188d6bf
elicn <elichn@gmail.com>
2021-07-13 17:11:28 +0300

    Refactor oleaut32

--- qiling/os/windows/dlls/oleaut32.py
+++ qiling/os/windows/dlls/oleaut32.py
@@ -3,17 +3,9 @@
 # Cross Platform and Multi Architecture Advanced Binary Emulation Framework
 #
 
-import struct
-import time
-from qiling.os.windows.const import *
-from qiling.os.const import *
+from qiling import Qiling
+from qiling.os.windows.api import *
 from qiling.os.windows.fncc import *
-from qiling.os.windows.utils import *
-from qiling.os.windows.thread import *
-from qiling.os.windows.handle import *
-from qiling.exception import *
-
-dllname = 'oleaut32_dll'
 
 # TODO this file is VERY experimental.
 
@@ -21,63 +13,79 @@ dllname = 'oleaut32_dll'
 #   const OLECHAR *strIn,
 #   UINT          ui
 # );
-@winsdkapi(cc=STDCALL, dllname=dllname)
-def hook_SysAllocStringLen(ql, address, params):
-    addr = ql.os.heap.alloc(params["ui"] + 1)
-    if params["strIn"] != 0:
-        ql.mem.write(addr, params["strIn"][:params["ui"]].encode("utf-16le"))
-    return addr
+@winsdkapi_new(cc=STDCALL, params={
+    'strIn' : POINTER, # OLECHAR
+    'ui'    : UINT
+})
+def hook_SysAllocStringLen(ql: Qiling, address: int, params):
+    strIn = params["strIn"]
+    ui = params["ui"]
 
+    addr = ql.os.heap.alloc(ui + 1)
+
+    if strIn:
+        ql.mem.write(addr, strIn[:ui].encode("utf-16le"))
+
+    return addr
 
 # void SysFreeString(
 #   BSTR bstrString
 # );
-@winsdkapi(cc=STDCALL, dllname=dllname)
-def hook_SysFreeString(ql, address, params):
-    addr = params["strIn"]
-    if addr != 0:
-        ql.os.heap.free(addr)
-        content = ql.os.utils.read_wstring(addr)
+@winsdkapi_new(cc=STDCALL, params={
+    'bstrString' : POINTER # BSTR
+})
+def hook_SysFreeString(ql: Qiling, address: int, params):
+    bstrString = params["bstrString"]
 
-        params["strIn"] = content
-    return 0
+    if bstrString:
+        # params["strIn"] = ql.os.utils.read_wstring(bstrString)
 
+        ql.os.heap.free(bstrString)
 
 # UINT SysStringLen(
 #   BSTR pbstr
 # );
-@winsdkapi(cc=STDCALL, dllname=dllname)
-def hook_SysStringLen(ql, address, params):
+@winsdkapi_new(cc=STDCALL, params={
+    'pbstr' : BSTR
+})
+def hook_SysStringLen(ql: Qiling, address: int, params):
     string = params["pbstr"]
-    if string != 0:
-        return len(string)
-    return 0
 
+    return 0 if string == 0 else len(string)
 
 # INT SysReAllocStringLen(
 #   BSTR          *pbstr,
 #   const OLECHAR *psz,
 #   unsigned int  len
 # );
-@winsdkapi(cc=STDCALL, dllname=dllname)
-def hook_SysReAllocStringLen(ql, address, params):
+@winsdkapi_new(cc=STDCALL, params={
+    'pbstr' : BSTR,
+    'psz'   : OLECHAR,
+    'len'   : UINT
+})
+def hook_SysReAllocStringLen(ql: Qiling, address: int, params):
     content = params["psz"]
     size = params["len"]
     addr = ql.os.heap.alloc(size + 1)
+
     ql.mem.write(addr, content[:size].encode("utf-16le"))
     ql.mem.write(params["pbstr"], addr.to_bytes(ql.pointersize, byteorder="little"))
-    return 1
 
+    return 1
 
 # BSTR SysAllocString(
 #   const OLECHAR *psz
 # );
-@winsdkapi(cc=STDCALL, dllname=dllname)
-def hook_SysAllocString(ql, address, params):
+@winsdkapi_new(cc=STDCALL, params={
+    'psz' : OLECHAR
+})
+def hook_SysAllocString(ql: Qiling, address: int, params):
     string = params["psz"]
-    if string == 0:
+
+    if not string:
         return 0
-    size = len(string)
-    addr = ql.os.heap.alloc(size + 1)
+
+    addr = ql.os.heap.alloc(len(string) + 1)
     ql.mem.write(addr, string.encode("utf-16le"))
+
     return addr
