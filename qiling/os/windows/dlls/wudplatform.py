3e10f66851337057c4b8e68876594e11296cf58e
elicn <elichn@gmail.com>
2021-07-13 16:57:20 +0300

    Refactor wudplatform

--- qiling/os/windows/dlls/wudplatform.py
+++ qiling/os/windows/dlls/wudplatform.py
@@ -3,25 +3,20 @@
 # Cross Platform and Multi Architecture Advanced Binary Emulation Framework
 #
 
-import struct
-from qiling.os.windows.const import *
+from qiling import Qiling
+from qiling.os.windows.api import *
 from qiling.os.windows.fncc import *
-from qiling.os.const import *
-from qiling.os.windows.utils import *
-from qiling.os.windows.thread import *
-from qiling.os.windows.handle import *
-from qiling.exception import *
 
-@winsdkapi(cc=STDCALL)
-def hook_WudfIsUserDebuggerPresent(ql, address, params):
+@winsdkapi_new(cc=STDCALL, params={})
+def hook_WudfIsUserDebuggerPresent(ql: Qiling, address: int, params):
     return 0
 
 
-@winsdkapi(cc=STDCALL)
-def hook_WudfIsAnyDebuggerPresent(ql, address, params):
+@winsdkapi_new(cc=STDCALL, params={})
+def hook_WudfIsAnyDebuggerPresent(ql: Qiling, address: int, params):
     return 0
 
 
-@winsdkapi(cc=STDCALL)
-def hook_WudfIsKernelDebuggerPresent(ql, address, params):
+@winsdkapi_new(cc=STDCALL, params={})
+def hook_WudfIsKernelDebuggerPresent(ql: Qiling, address: int, params):
     return 0
