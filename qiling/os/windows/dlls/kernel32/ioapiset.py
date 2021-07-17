a10a44ed74e50846900a5c0401efd14d8954cac9
elicn <elichn@gmail.com>
2021-07-17 17:58:16 +0300

    Refactor kernel32 (ioapiset)

--- qiling/os/windows/dlls/kernel32/ioapiset.py
+++ qiling/os/windows/dlls/kernel32/ioapiset.py
@@ -3,18 +3,10 @@
 # Cross Platform and Multi Architecture Advanced Binary Emulation Framework
 #
 
-import struct
-import time
+from qiling import Qiling
+from qiling.os.windows.api import *
 from qiling.os.windows.const import *
-from qiling.os.const import *
 from qiling.os.windows.fncc import *
-from qiling.os.windows.utils import *
-from qiling.os.windows.thread import *
-from qiling.os.windows.handle import *
-from qiling.exception import *
-
-
-dllname = 'kernel32_dll'
 
 # BOOL DeviceIoControl(
 #   HANDLE       hDevice,
@@ -26,11 +18,21 @@ dllname = 'kernel32_dll'
 #   LPDWORD      lpBytesReturned,
 #   LPOVERLAPPED lpOverlapped
 # );
-@winsdkapi(cc=STDCALL, dllname=dllname)
-def hook_DeviceIoControl(ql, address, params):
-    operation = params["dwIoControlCode"]
-    data = params["lpInBuffer"]
-    output = params["lpOutBuffer"]
-    output_size = params["lpBytesReturned"]
+@winsdkapi_new(cc=STDCALL, params={
+    'hDevice'         : HANDLE,
+    'dwIoControlCode' : DWORD,
+    'lpInBuffer'      : LPVOID,
+    'nInBufferSize'   : DWORD,
+    'lpOutBuffer'     : LPVOID,
+    'nOutBufferSize'  : DWORD,
+    'lpBytesReturned' : LPDWORD,
+    'lpOverlapped'    : LPOVERLAPPED
+})
+def hook_DeviceIoControl(ql: Qiling, address: int, params):
+    # operation = params["dwIoControlCode"]
+    # data = params["lpInBuffer"]
+    # output = params["lpOutBuffer"]
+    # output_size = params["lpBytesReturned"]
+
     # TODO implement operations. Did not find controlCodes values
     return 1
