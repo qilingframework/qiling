2e19e847b9a6ac992017107e3acfee85681012c1
elicn <elichn@gmail.com>
2021-07-19 23:07:46 +0300

    Refactor kernel32 (synchapi)

--- qiling/os/windows/dlls/kernel32/synchapi.py
+++ qiling/os/windows/dlls/kernel32/synchapi.py
@@ -3,116 +3,121 @@
 # Cross Platform and Multi Architecture Advanced Binary Emulation Framework
 #
 
-import struct
-import time
+from qiling import Qiling
+from qiling.exception import QlErrorNotImplemented
+from qiling.os.windows.api import *
 from qiling.os.windows.const import *
-from qiling.os.const import *
 from qiling.os.windows.fncc import *
-from qiling.os.windows.utils import *
-from qiling.os.windows.thread import *
-from qiling.os.windows.handle import *
-from qiling.exception import *
-from qiling.os.windows.structs import *
-from qiling.const import *
-
-
-dllname = 'kernel32_dll'
+from qiling.os.windows.handle import Handle
+from qiling.os.windows.structs import Mutex
 
 # void Sleep(
 #  DWORD dwMilliseconds
 # );
-@winsdkapi(cc=STDCALL, dllname=dllname)
-def hook_Sleep(ql, address, params):
+@winsdkapi_new(cc=STDCALL, params={
+    'dwMilliseconds' : DWORD
+})
+def hook_Sleep(ql: Qiling, address: int, params):
     # time.sleep(params["dwMilliseconds"] * 10**(-3))
     pass
-
 
 # void EnterCriticalSection(
 #  LPCRITICAL_SECTION lpCriticalSection
 # );
-@winsdkapi(cc=STDCALL, dllname=dllname)
-def hook_EnterCriticalSection(ql, address, params):
+@winsdkapi_new(cc=STDCALL, params={
+    'lpCriticalSection' : LPCRITICAL_SECTION
+})
+def hook_EnterCriticalSection(ql: Qiling, address: int, params):
     return 0
-
 
 # void LeaveCriticalSection(
 #  LPCRITICAL_SECTION lpCriticalSection
 # );
-@winsdkapi(cc=STDCALL, dllname=dllname)
-def hook_LeaveCriticalSection(ql, address, params):
+@winsdkapi_new(cc=STDCALL, params={
+    'lpCriticalSection' : LPCRITICAL_SECTION
+})
+def hook_LeaveCriticalSection(ql: Qiling, address: int, params):
     return 0
-
 
 # void DeleteCriticalSection(
 #   LPCRITICAL_SECTION lpCriticalSection
 # );
-@winsdkapi(cc=STDCALL, dllname=dllname)
-def hook_DeleteCriticalSection(ql, address, params):
+@winsdkapi_new(cc=STDCALL, params={
+    'lpCriticalSection' : LPCRITICAL_SECTION
+})
+def hook_DeleteCriticalSection(ql: Qiling, address: int, params):
     return 0
-
 
 # void InitializeCriticalSection(
 #   LPCRITICAL_SECTION lpCriticalSection
 # );
-@winsdkapi(cc=STDCALL, dllname=dllname)
-def hook_InitializeCriticalSection(ql, address, params):
+@winsdkapi_new(cc=STDCALL, params={
+    'lpCriticalSection' : LPCRITICAL_SECTION
+})
+def hook_InitializeCriticalSection(ql: Qiling, address: int, params):
     return 1
-
 
 # BOOL InitializeCriticalSectionEx(
 #   LPCRITICAL_SECTION lpCriticalSection,
 #   DWORD              dwSpinCount,
 #   DWORD              Flags
 # );
-@winsdkapi(cc=STDCALL, dllname=dllname)
-def hook_InitializeCriticalSectionEx(ql, address, params):
+@winsdkapi_new(cc=STDCALL, params={
+    'lpCriticalSection' : LPCRITICAL_SECTION,
+    'dwSpinCount'       : DWORD,
+    'Flags'             : DWORD
+})
+def hook_InitializeCriticalSectionEx(ql: Qiling, address: int, params):
     return 1
-
 
 # BOOL InitializeCriticalSectionAndSpinCount(
 #  LPCRITICAL_SECTION lpCriticalSection,
 #  DWORD              dwSpinCount
 # );
-@winsdkapi(cc=STDCALL, dllname=dllname, replace_params_type={'DWORD': 'UINT'})
-def hook_InitializeCriticalSectionAndSpinCount(ql, address, params):
+@winsdkapi_new(cc=STDCALL, params={
+    'lpCriticalSection' : LPCRITICAL_SECTION,
+    'dwSpinCount'       : DWORD
+})
+def hook_InitializeCriticalSectionAndSpinCount(ql: Qiling, address: int, params):
     return 1
-
 
 # DWORD WaitForSingleObject(
 #   HANDLE hHandle,
 #   DWORD  dwMilliseconds
 # );
-@winsdkapi(cc=STDCALL, dllname=dllname)
-def hook_WaitForSingleObject(ql, address, params):
-    ret = 0
+@winsdkapi_new(cc=STDCALL, params={
+    'hHandle'        : HANDLE,
+    'dwMilliseconds' : DWORD
+})
+def hook_WaitForSingleObject(ql: Qiling, address: int, params):
     hHandle = params["hHandle"]
-    dwMilliseconds = params["dwMilliseconds"]
-
     handle = ql.os.handle_manager.get(hHandle)
+
     if handle:
         target_thread = handle.obj
         ql.os.thread_manager.cur_thread.waitfor(target_thread)
 
-    return ret
-
+    return 0
 
 # DWORD WaitForSingleObjectEx(
 #   HANDLE hHandle,
 #   DWORD  dwMilliseconds
 #   BOOL   bAlertable
 # );
-@winsdkapi(cc=STDCALL, dllname=dllname)
-def hook_WaitForSingleObjectEx(ql, address, params):
-    ret = 0
+@winsdkapi_new(cc=STDCALL, params={
+    'hHandle'        : HANDLE,
+    'dwMilliseconds' : DWORD,
+    'bAlertable'     : BOOL
+})
+def hook_WaitForSingleObjectEx(ql: Qiling, address: int, params):
     hHandle = params["hHandle"]
-    dwMilliseconds = params["dwMilliseconds"]
-    alertable = params["bAlertable"]
+    handle = ql.os.handle_manager.get(hHandle)
 
-    target_thread = ql.os.handle_manager.get(hHandle).obj
-    ql.os.thread_manager.cur_thread.waitfor(target_thread)
+    if handle:
+        target_thread = handle.obj
+        ql.os.thread_manager.cur_thread.waitfor(target_thread)
 
-    return ret
-
+    return 0
 
 # DWORD WaitForMultipleObjects(
 #   DWORD        nCount,
@@ -120,30 +125,26 @@ def hook_WaitForSingleObjectEx(ql, address, params):
 #   BOOL         bWaitAll,
 #   DWORD        dwMilliseconds
 # );
-@winsdkapi(cc=STDCALL, dllname=dllname, replace_params_type={'HANDLE': 'POINTER'})
-def hook_WaitForMultipleObjects(ql, address, params):
-    ret = 0
+@winsdkapi_new(cc=STDCALL, params={
+    'nCount'         : DWORD,
+    'lpHandles'      : HANDLE,
+    'bWaitAll'       : BOOL,
+    'dwMilliseconds' : DWORD
+})
+def hook_WaitForMultipleObjects(ql: Qiling, address: int, params):
     nCount = params["nCount"]
     lpHandles = params["lpHandles"]
-    bWaitAll = params["bWaitAll"]
-    dwMilliseconds = params["dwMilliseconds"]
 
     for i in range(nCount):
         handle_value = ql.unpack(ql.mem.read(lpHandles + i * ql.pointersize, ql.pointersize))
-        if handle_value != 0:
+
+        if handle_value:
             thread = ql.os.handle_manager.get(handle_value).obj
             ql.os.thread_manager.cur_thread.waitfor(thread)
 
-    return ret
+    return 0
 
-
-# HANDLE OpenMutexW(
-#   DWORD   dwDesiredAccess,
-#   BOOL    bInheritHandle,
-#   LPCWSTR lpName
-# );
-@winsdkapi(cc=STDCALL, dllname=dllname)
-def hook_OpenMutexW(ql, address, params):
+def __OpenMutex(ql: Qiling, address: int, params):
     # The name can have a "Global" or "Local" prefix to explicitly open an object in the global or session namespace.
     # It can also have no prefix
     try:
@@ -158,75 +159,107 @@ def hook_OpenMutexW(ql, address, params):
         # example sample: Gandcrab e42431d37561cc695de03b85e8e99c9e31321742
         if handle is None:
             return 0xD10C
+
+        mutex = handle.obj
+
+        if mutex.isFree():
+            mutex.lock()
         else:
-            mutex = handle.obj
-            if mutex.isFree():
-                mutex.lock()
-            else:
-                raise QlErrorNotImplemented("API not implemented")
+            raise QlErrorNotImplemented("API not implemented")
     else:
         if handle is None:
             # If a named mutex does not exist, the function fails and GetLastError returns ERROR_FILE_NOT_FOUND.
             ql.os.last_error = ERROR_FILE_NOT_FOUND
             return 0
-        else:
-            raise QlErrorNotImplemented("API not implemented")
 
+        raise QlErrorNotImplemented("API not implemented")
 
-# HANDLE OpenMutexA(
-#   DWORD   dwDesiredAccess,
-#   BOOL    bInheritHandle,
-#   LPCSTR lpName
-# );
-@winsdkapi(cc=STDCALL, dllname=dllname)
-def hook_OpenMutexA(ql, address, params):
-    return hook_OpenMutexW.__wrapped__(ql, address, params)
-
-
-# HANDLE CreateMutexW(
-#   LPSECURITY_ATTRIBUTES lpMutexAttributes,
-#   BOOL                  bInitialOwner,
-#   LPCWSTR               lpName
-# );
-@winsdkapi(cc=STDCALL, dllname=dllname)
-def hook_CreateMutexW(ql, address, params):
+def __CreateMutex(ql: Qiling, address: int, params):
     try:
         _type, name = params["lpName"].split("\\")
     except ValueError:
         name = params["lpName"]
         _type = ""
 
-    owning = params["bInitialOwner"]
     handle = ql.os.handle_manager.search(name)
+
     if handle is not None:
         # ql.os.last_error = ERROR_ALREADY_EXISTS
         return 0
-    else:
-        mutex = Mutex(name, _type)
-        if owning:
-            mutex.lock()
-        handle = Handle(obj=mutex, name=name)
-        ql.os.handle_manager.append(handle)
+
+    owning = params["bInitialOwner"]
+    mutex = Mutex(name, _type)
+
+    if owning:
+        mutex.lock()
+
+    handle = Handle(obj=mutex, name=name)
+    ql.os.handle_manager.append(handle)
 
     return handle.id
 
+# HANDLE OpenMutexW(
+#   DWORD   dwDesiredAccess,
+#   BOOL    bInheritHandle,
+#   LPCWSTR lpName
+# );
+@winsdkapi_new(cc=STDCALL, params={
+    'dwDesiredAccess' : DWORD,
+    'bInheritHandle'  : BOOL,
+    'lpName'          : LPCWSTR
+})
+def hook_OpenMutexW(ql: Qiling, address: int, params):
+    return __OpenMutex(ql, address, params)
+
+# HANDLE OpenMutexA(
+#   DWORD   dwDesiredAccess,
+#   BOOL    bInheritHandle,
+#   LPCSTR lpName
+# );
+@winsdkapi_new(cc=STDCALL, params={
+    'dwDesiredAccess' : DWORD,
+    'bInheritHandle'  : BOOL,
+    'lpName'          : LPCSTR
+})
+def hook_OpenMutexA(ql: Qiling, address: int, params):
+    return __OpenMutex(ql, address, params)
+
+# HANDLE CreateMutexW(
+#   LPSECURITY_ATTRIBUTES lpMutexAttributes,
+#   BOOL                  bInitialOwner,
+#   LPCWSTR               lpName
+# );
+@winsdkapi_new(cc=STDCALL, params={
+    'lpMutexAttributes' : LPSECURITY_ATTRIBUTES,
+    'bInitialOwner'     : BOOL,
+    'lpName'            : LPCWSTR
+})
+def hook_CreateMutexW(ql: Qiling, address: int, params):
+    return __CreateMutex(ql, address, params)
 
 # HANDLE CreateMutexA(
 #   LPSECURITY_ATTRIBUTES lpMutexAttributes,
 #   BOOL                  bInitialOwner,
 #   LPCSTR               lpName
 # );
-@winsdkapi(cc=STDCALL, dllname=dllname)
-def hook_CreateMutexA(ql, address, params):
-    return hook_CreateMutexW.__wrapped__(ql, address, params)
+@winsdkapi_new(cc=STDCALL, params={
+    'lpMutexAttributes' : LPSECURITY_ATTRIBUTES,
+    'bInitialOwner'     : BOOL,
+    'lpName'            : LPCSTR
+})
+def hook_CreateMutexA(ql: Qiling, address: int, params):
+    return __CreateMutex(ql, address, params)
 
 # BOOL ReleaseMutex(
 #   HANDLE hMutex
 # );
-@winsdkapi(cc=STDCALL, dllname=dllname)
-def hook_ReleaseMutex(ql, address, params):
+@winsdkapi_new(cc=STDCALL, params={
+    'hMutex' : HANDLE
+})
+def hook_ReleaseMutex(ql: Qiling, address: int, params):
     hMutex = params["hMutex"]
     handle = ql.os.handle_manager.get(hMutex)
+
     if not handle:
         ql.os.last_error = ERROR_INVALID_HANDLE
         return 0
@@ -244,33 +277,47 @@ def hook_ReleaseMutex(ql, address, params):
     mutex.unlock()
     return 1
 
+def __CreateEvent(ql: Qiling, address: int, params):
+    # Implementation seems similar enough to Mutex to just use it
+
+    try:
+        namespace, name = params["lpName"].split("\\")
+    except ValueError:
+        name = params["lpName"]
+        namespace = ""
+
+    handle = ql.os.handle_manager.search(name)
+
+    if handle is not None:
+        ql.os.last_error = ERROR_ALREADY_EXISTS
+        # FIXME: fail with a nullptr?
+        # return 0
+    else:
+        mutex = Mutex(name, namespace)
+
+        if params['bInitialState']:
+            mutex.lock()
+
+        handle = Handle(obj=mutex, name=name)
+        ql.os.handle_manager.append(handle)
+
+    # FIXME: shouldn't it be 'id' instead of 'ID'?
+    return handle.ID
+
 # HANDLE CreateEventA(
 #  LPSECURITY_ATTRIBUTES lpEventAttributes,
 #  BOOL                  bManualReset,
 #  BOOL                  bInitialState,
 #  LPCSTR                lpName
 # );
-@winsdkapi(cc=STDCALL, dllname=dllname)
-def hook_CreateEventA(ql, address, params):
-    """
-    Implementation seems similar enough to Mutex to just use it
-    """
-    try:
-        namespace, name = params["lpName"].split("\\")
-    except ValueError:
-        name = params["lpName"]
-        namespace = ""
-    handle = ql.os.handle_manager.search(name)
-    if handle is not None:
-        ql.os.last_error = ERROR_ALREADY_EXISTS
-    else:
-        mutex = Mutex(name, namespace)
-        if params['bInitialState']:
-            mutex.lock()
-        handle = Handle(obj=mutex, name=name)
-        ql.os.handle_manager.append(handle)
-    return handle.ID
-
+@winsdkapi_new(cc=STDCALL, params={
+    'lpEventAttributes' : LPSECURITY_ATTRIBUTES,
+    'bManualReset'      : BOOL,
+    'bInitialState'     : BOOL,
+    'lpName'            : LPCSTR
+})
+def hook_CreateEventA(ql: Qiling, address: int, params):
+    return __CreateEvent(ql, address, params)
 
 # HANDLE CreateEventW(
 #  LPSECURITY_ATTRIBUTES lpEventAttributes,
@@ -278,46 +325,68 @@ def hook_CreateEventA(ql, address, params):
 #  BOOL                  bInitialState,
 #  LPCWSTR               lpName
 # );
-@winsdkapi(cc=STDCALL, dllname=dllname)
-def hook_CreateEventW(ql, address, params):
-    return hook_CreateEventA.__wrapped__(ql, address, params)
+@winsdkapi_new(cc=STDCALL, params={
+    'lpEventAttributes' : LPSECURITY_ATTRIBUTES,
+    'bManualReset'      : BOOL,
+    'bInitialState'     : BOOL,
+    'lpName'            : LPCWSTR
+})
+def hook_CreateEventW(ql: Qiling, address: int, params):
+    return __CreateEvent(ql, address, params)
 
+@winsdkapi_new(cc=STDCALL, params={
+    'SRWLock' : PSRWLOCK
+})
+def hook_TryAcquireSRWLockExclusive(ql: Qiling, address: int, params):
+    pass
+
+@winsdkapi_new(cc=STDCALL, params={
+    'SRWLock' : PSRWLOCK
+})
+def hook_TryAcquireSRWLockShared(ql: Qiling, address: int, params):
+    pass
 
 # void InitializeSRWLock(
 #  PSRWLOCK SRWLock
 # );
-@winsdkapi(cc=STDCALL, dllname=dllname)
-def hook_InitializeSRWLock(ql, address, params):
+@winsdkapi_new(cc=STDCALL, params={
+    'SRWLock' : PSRWLOCK
+})
+def hook_InitializeSRWLock(ql: Qiling, address: int, params):
     return
-
 
 # void AcquireSRWLockExclusive(
 #   PSRWLOCK SRWLock
 # );
-@winsdkapi(cc=STDCALL, dllname=dllname)
-def hook_AcquireSRWLockExclusive(ql, address, params):
-    return
-
+@winsdkapi_new(cc=STDCALL, params={
+    'SRWLock' : PSRWLOCK
+})
+def hook_AcquireSRWLockExclusive(ql: Qiling, address: int, params):
+   return
 
 # void AcquireSRWLockShared(
 #   PSRWLOCK SRWLock
 # );
-@winsdkapi(cc=STDCALL, dllname=dllname)
-def hook_AcquireSRWLockShared(ql, address, params):
+@winsdkapi_new(cc=STDCALL, params={
+    'SRWLock' : PSRWLOCK
+})
+def hook_AcquireSRWLockShared(ql: Qiling, address: int, params):
     return
-
 
 # void ReleaseSRWLockExclusive(
 #   PSRWLOCK SRWLock
 # );
-@winsdkapi(cc=STDCALL, dllname=dllname)
-def hook_ReleaseSRWLockExclusive(ql, address, params):
+@winsdkapi_new(cc=STDCALL, params={
+    'SRWLock' : PSRWLOCK
+})
+def hook_ReleaseSRWLockExclusive(ql: Qiling, address: int, params):
     return
-
 
 # void ReleaseSRWLockShared(
 #   PSRWLOCK SRWLock
 # );
-@winsdkapi(cc=STDCALL, dllname=dllname)
-def hook_ReleaseSRWLockShared(ql, address, params):
+@winsdkapi_new(cc=STDCALL, params={
+    'SRWLock' : PSRWLOCK
+})
+def hook_ReleaseSRWLockShared(ql: Qiling, address: int, params):
     return
