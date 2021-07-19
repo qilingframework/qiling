716dfd5cf7e895fe33791465581878fafc5eb52c
elicn <elichn@gmail.com>
2021-07-19 23:49:24 +0300

    Refactor kernel32 (tlhelp32)

--- qiling/os/windows/dlls/kernel32/tlhelp32.py
+++ qiling/os/windows/dlls/kernel32/tlhelp32.py
@@ -3,51 +3,50 @@
 # Cross Platform and Multi Architecture Advanced Binary Emulation Framework
 #
 
-import struct
-import time
-from qiling.os.windows.const import *
-from qiling.os.const import *
+from qiling import Qiling
+from qiling.exception import QlErrorNotImplemented
+from qiling.os.windows.api import *
+from qiling.os.windows.const import TH32CS_SNAPPROCESS
 from qiling.os.windows.fncc import *
-from qiling.os.windows.utils import *
-from qiling.os.windows.thread import *
-from qiling.os.windows.handle import *
-from qiling.exception import *
-from qiling.const import *
-
-
-dllname = 'kernel32_dll'
 
 # HANDLE CreateToolhelp32Snapshot(
 #   DWORD dwFlags,
 #   DWORD th32ProcessID
 # );
-@winsdkapi(cc=STDCALL, dllname=dllname)
-def hook_CreateToolhelp32Snapshot(ql, address, params):
+@winsdkapi_new(cc=STDCALL, params={
+    'dwFlags'       : DWORD,
+    'th32ProcessID' : DWORD
+})
+def hook_CreateToolhelp32Snapshot(ql: Qiling, address: int, params):
     # TODO thinking about implementing an handler, gonna see if is really necessary
     flag = params["dwFlags"]
-    if flag == TH32CS_SNAPPROCESS:
-        ql.log.debug("The target is checking every process!")
-    else:
-        raise QlErrorNotImplemented("API not implemented")
-    return 0xD10C
 
+    if flag != TH32CS_SNAPPROCESS:
+        raise QlErrorNotImplemented("API not implemented")
+
+    ql.log.debug("The target is checking every process!")
+
+    return 0xD10C
 
 # BOOL Process32FirstW(
 #   HANDLE            hSnapshot,
 #   LPPROCESSENTRY32W lppe
 # );
-@winsdkapi(cc=STDCALL, dllname=dllname)
-def hook_Process32FirstW(ql, address, params):
-    return 0x1
-
+@winsdkapi_new(cc=STDCALL, params={
+    'hSnapshot' : HANDLE,
+    'lppe'      : LPPROCESSENTRY32W
+})
+def hook_Process32FirstW(ql: Qiling, address: int, params):
+    return 1
 
 # BOOL Process32NextW(
 #   HANDLE            hSnapshot,
 #   LPPROCESSENTRY32W lppe
 # );
-@winsdkapi(cc=STDCALL, dllname=dllname)
-def hook_Process32NextW(ql, address, params):
+@winsdkapi_new(cc=STDCALL, params={
+    'hSnapshot' : HANDLE,
+    'lppe'      : LPPROCESSENTRY32W
+})
+def hook_Process32NextW(ql: Qiling, address: int, params):
     # Return True if more process, 0 else
-    if ql.os.syscall_count["Process32NextW"] >= 3:  # I don' know how many process the sample want's to cycle
-        return 0x0
-    return 0x1
+    return int(ql.os.syscall_count["Process32NextW"] < 3)  # I don' know how many process the sample want's to cycle
