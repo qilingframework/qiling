c2f3e447ce4a8815ddd9cf899b893402b2883ab0
elicn <elichn@gmail.com>
2021-07-19 22:24:37 +0300

    Refactor kernel32 (profileapi)

--- qiling/os/windows/dlls/kernel32/profileapi.py
+++ qiling/os/windows/dlls/kernel32/profileapi.py
@@ -3,32 +3,28 @@
 # Cross Platform and Multi Architecture Advanced Binary Emulation Framework
 #
 
-import struct
-import time
-from qiling.os.windows.const import *
-from qiling.os.const import *
+from qiling import Qiling
+from qiling.os.windows.api import *
 from qiling.os.windows.fncc import *
-from qiling.os.windows.utils import *
-from qiling.os.windows.thread import *
-from qiling.os.windows.handle import *
-from qiling.exception import *
-
-
-dllname = 'kernel32_dll'
 
 # BOOL QueryPerformanceCounter(
 #   LARGE_INTEGER *lpPerformanceCount
 # );
-@winsdkapi(cc=STDCALL, dllname=dllname)
-def hook_QueryPerformanceCounter(ql, address, params):
-    ret = 0
-    return ret
-
+@winsdkapi_new(cc=STDCALL, params={
+    'lpPerformanceCount' : POINTER
+})
+def hook_QueryPerformanceCounter(ql: Qiling, address: int, params):
+    return 0
 
 # BOOL QueryPerformanceFrequency(
 #  LARGE_INTEGER *lpFrequency
 # );
-@winsdkapi(cc=STDCALL, dllname=dllname)
-def hook_QueryPerformanceFrequency(ql, address, params):
-    ql.mem.write(params['lpFrequency'], (10000000).to_bytes(length=8, byteorder='little'))
+@winsdkapi_new(cc=STDCALL, params={
+    'lpFrequency' : POINTER
+})
+def hook_QueryPerformanceFrequency(ql: Qiling, address: int, params):
+    lpFrequency = params['lpFrequency']
+
+    ql.mem.write(lpFrequency, ql.pack64(10000000))
+
     return 1
