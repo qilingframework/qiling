13f52fa62a0547defb195fe94a61dc49534f8508
dataisland <dataisland@outlook.com>
2021-07-20 12:14:50 +0800

    The self-produced ihex loader is correct now

---
+++ qiling/arch/cortex_m.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+# 
+# Cross Platform and Multi Architecture Advanced Binary Emulation Framework
+#
+
+from unicorn import *
+from unicorn.arm_const import *
+
+from capstone import *
+
+from qiling.const import *
+from qiling.mcu.stm32.exceptions.manager import ExceptionManager
+from .arm import QlArchARM
+
+class QlArchCORTEX_M(QlArchARM):
+    def __init__(self, ql):
+        super().__init__(ql)
+
+        self.md = Cs(CS_ARCH_ARM, CS_MODE_THUMB | CS_MODE_MCLASS)
+
+        ## Exception Model
+        self.emgr = ExceptionManager(self)
+
+        ## Memory Model
+        self.BOOT = [0, 0]
+        self.boot_space = 0
+        self.mapinfo = {
+            'sram'      : (0x20000000, 0x20020000),
+            'system'    : (0x1FFF0000, 0x1FFF7800),            
+            'flash'     : (0x08000000, 0x08080000),             
+            'peripheral': (0x40000000, 0x40100000),
+            'core_perip': (0xE0000000, 0xE0100000),
+        }
+
+    def get_init_uc(self):
+        return Uc(UC_ARCH_ARM, UC_MODE_ARM + UC_MODE_MCLASS)
+
+    def setup(self):        
+        def hook_code(mu, address, size, user_data):     
+            code = mu.mem_read(address, size)
+            for i in self.md.disasm(code, address):
+                print(hex(i.address), i.mnemonic, i.op_str)
+
+        self.ql.uc.hook_add(UC_HOOK_CODE, hook_code)
+
+        for begin, end in self.mapinfo.values():
+            self.mem.map(begin, end - begin)
+        
+    def flash(self):
+        self.ql.loader.run()
+
+    def reset(self):
+        if self.BOOT[0] == 0:
+            self.boot_space = self.mapinfo['flash'][0]
+        elif self.BOOT[1] == 0:
+            self.boot_space = self.mapinfo['system'][0]
+        elif self.BOOT[1] == 1:
+            self.boot_space = self.mapinfo['sram'][0]
+
+        self.reg.write('lr', 0xffffffff)
+        self.reg.write('msp', self.mem.read_ptr(self.boot_space))
+        self.reg.write('pc', self.mem.read_ptr(self.boot_space + 0x4))
+        
+    def step(self):
+        self.emgr.interrupt()
+        self.ql.uc.emu_start(self.get_pc(), 0, count=1)
+
+    def run(self, count=-1):        
+        while count != 0:
+            self.step()
+            count -= 1
+
+    @property
+    def reg(self):
+        return self.ql.reg
+    
+    @property
+    def mem(self):
+        return self.ql.mem
--- qiling/loader/mcu.py
+++ qiling/loader/mcu.py
@@ -4,19 +4,62 @@
 # Built on top of Unicorn emulator (www.unicorn-engine.org) 
 
 
+import struct
+
 from qiling.const import *
 from qiling.core import Qiling
 
 from .loader import QlLoader
-from intelhex import IntelHex
 
+class IhexParser:
+    def __init__(self, path):
+        self.pc   = None
+        self.base = None
+        self.mem  = []
+        self.segments = []
+
+        with open(path, 'r') as f:
+            for line in f.read().splitlines():
+                self.parse_line(line)
+
+            begin, end, bindata = 0, 0, b''
+            for addr, data in self.mem:
+                if addr != end:
+                    self.add_segment(begin, end, bindata)
+                    begin, end, bindata = addr, addr + len(data), data
+                else:
+                    bindata += data
+                    end += len(data)
+            self.add_segment(begin, end, bindata)            
+                    
+    def add_segment(self, begin, end, bindata):
+        if len(bindata) > 0 and end - begin == len(bindata):
+            self.segments.append((begin, end, bindata))
+
+    def parse_line(self, line):
+        size = int(line[1: 3], 16)        
+        type = line[7: 9]
+
+        addr = bytes.fromhex(line[3: 7])
+        data = bytes.fromhex(line[9: 9 + size * 2])
+
+        if type == '01':
+            return
+        
+        if type == '04':            
+            self.base = struct.unpack('>I', data + b'\x00\x00')[0]            
+        elif type == '05':
+            self.pc = struct.unpack('>I', data)[0]            
+        elif type == '00':
+            offset = struct.unpack('>I', b'\x00\x00' + addr)[0]
+            self.mem.append((self.base + offset, data))
 
 class QlLoaderMCU(QlLoader):
     def __init__(self, ql:Qiling):
         super(QlLoaderMCU, self).__init__(ql)
         
-        self.ihexfile = IntelHex(self.argv[0])
+        self.ihexfile = IhexParser(self.argv[0])        
         
     def run(self):
-        for begin, end in self.ihexfile.segments():
-            self.ql.mem.write(begin, self.ihexfile.tobinstr(begin, end - 1))
+        for begin, end, data in self.ihexfile.segments:
+            self.ql.mem.write(begin, data)            
