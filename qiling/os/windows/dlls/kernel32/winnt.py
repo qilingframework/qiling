07a990dfd8919305950676b2145cded18c43f5d8
elicn <elichn@gmail.com>
2021-07-20 04:05:38 +0300

    Refactor kernel32 (winnt)

--- qiling/os/windows/dlls/kernel32/winnt.py
+++ qiling/os/windows/dlls/kernel32/winnt.py
@@ -3,68 +3,84 @@
 # Cross Platform and Multi Architecture Advanced Binary Emulation Framework
 #
 
-from qiling.os.const import *
+from qiling import Qiling
+from qiling.os.windows.api import *
 from qiling.os.windows.const import *
 from qiling.os.windows.fncc import *
-
-
-dllname = 'kernel32_dll'
 
 # LONG InterlockedExchange(
 #  LONG volatile *Target,
 #  LONG          Value
 # );
-@winsdkapi(cc=STDCALL, dllname=dllname, replace_params={"Target": POINTER, "Value": UINT})
-def hook_InterlockedExchange(ql, address, params):
-    old = int.from_bytes(ql.mem.read(params['Target'], ql.pointersize), byteorder='little')
-    ql.mem.write(params['Target'], params['Value'].to_bytes(length=ql.pointersize, byteorder='little'))
-    return old
+@winsdkapi_new(cc=STDCALL, params={
+    'Target' : POINTER,
+    'Value'  : INT # LONG
+})
+def hook_InterlockedExchange(ql: Qiling, address: int, params):
+    Target = params['Target']
+    Value = params['Value']
 
+    old = ql.mem.read_ptr(Target, 4)
+    ql.mem.write(Target, ql.pack32(Value))
+
+    return old
 
 # LONG InterlockedIncrement(
 #  LONG volatile *Target,
 # );
-@winsdkapi(cc=STDCALL, dllname=dllname, replace_params={"Target": POINTER})
-def hook_InterlockedIncrement(ql, address, params):
-    val = int.from_bytes(ql.mem.read(params['Target'], 4), byteorder='little')
-    val += 1 & (2 ** 32)  # increment and overflow back to 0 if applicable
-    ql.mem.write(params['Target'], val.to_bytes(length=4, byteorder='little'))
-    return val
+@winsdkapi_new(cc=STDCALL, params={
+    'Target' : POINTER
+})
+def hook_InterlockedIncrement(ql: Qiling, address: int, params):
+    Target = params['Target']
 
+    Value = ql.mem.read_ptr(Target, 4)
+    Value = (Value + 1) % (1 << 32)     # increment and handle overflow
+    ql.mem.write(Target, ql.pack32(Value))
+
+    return Value
 
 # LONG InterlockedDecrement(
 #  LONG volatile *Target,
 # );
-@winsdkapi(cc=STDCALL, dllname=dllname, replace_params={"Target": POINTER})
-def hook_InterlockedDecrement(ql, address, params):
-    val = int.from_bytes(ql.mem.read(params['Target'], 4), byteorder='little')
-    val -= 1
-    if val == -1:
-        val = 0xFFFFFFFF
-    ql.mem.write(params['Target'], val.to_bytes(length=4, byteorder='little'))
-    return val
+@winsdkapi_new(cc=STDCALL, params={
+    'Target' : POINTER
+})
+def hook_InterlockedDecrement(ql: Qiling, address: int, params):
+    Target = params['Target']
 
+    Value = ql.mem.read_ptr(Target, 4)
+    Value = (Value - 1) % (1 << 32)     # increment and handle underflow
+
+    ql.mem.write(Target, ql.pack32(Value))
+
+    return Value
 
 # NTSYSAPI ULONGLONG VerSetConditionMask(
-#   ULONGLONG ConditionMask, => 64bit param
+#   ULONGLONG ConditionMask,
 #   DWORD     TypeMask,
 #   BYTE      Condition
 # );
-@winsdkapi(cc=STDCALL, dllname=dllname, replace_params={"ConditionMask": ULONGLONG, "TypeMask": DWORD, "Condition": BYTE})
-def hook_VerSetConditionMask(ql, address, params):
+@winsdkapi_new(cc=STDCALL, params={
+    'ConditionMask' : ULONGLONG,
+    'TypeMask'      : DWORD,
+    'Condition'     : BYTE
+})
+def hook_VerSetConditionMask(ql: Qiling, address: int, params):
     # ConditionMask = params["ConditionMask"]
     TypeMask = params["TypeMask"]
     Condition = params["Condition"]
-    mask = params["ConditionMask"]
+
     ConditionMask = ql.os.hooks_variables.get("ConditionMask", {})
+
     if TypeMask == 0:
         ret = ConditionMask
     else:
         Condition &= VER_CONDITION_MASK
+
         if Condition == 0:
             ret = ConditionMask
         else:
-            ullCondMask = Condition
             if TypeMask & VER_PRODUCT_TYPE:
                 # ConditionMask |= ullCondMask << (7 * VER_NUM_BITS_PER_CONDITION_MASK)
                 ConditionMask[VER_PRODUCT_TYPE] = Condition
@@ -89,11 +105,14 @@ def hook_VerSetConditionMask(ql, address, params):
             elif TypeMask & VER_MINORVERSION:
                 # ConditionMask |= ullCondMask << (0 * VER_NUM_BITS_PER_CONDITION_MASK)
                 ConditionMask[VER_MINORVERSION] = Condition
+
             ret = 1
+
     # ConditionMask should be updated locally
     # https://docs.microsoft.com/it-it/windows/win32/sysinfo/verifying-the-system-version
     # But since we don't have the pointer to the variable, an hack is to use the environment.
     # Feel free to push a better solution
     # Since I can't work with bits, and since we had to work with the environment anyway, let's use a dict
     ql.os.hooks_variables["ConditionMask"] = ConditionMask
+
     return ret
