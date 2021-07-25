43dfe9c3636975bc4753bb53dac2a73748788e4d
Daniel Moos <daniel.moos@gmail.com>
2021-07-25 20:45:42 +0200

    Added QnxConn class
    QNX uses connections for communication between processes. An a
    follow-up commit a directory will be used to map the connection ID
    to instanes of this QnxConnection class.

--- qiling/os/qnx/helpers.py
+++ qiling/os/qnx/helpers.py
@@ -4,6 +4,28 @@
 #
 from ctypes import c_int32
 
+class QnxConn:
+    def __init__(self, pid, chid, fd = None):
+        self.__pid = pid   # Process Id
+        self.__chid = chid # Channel Id
+        self.__fd = fd     # File Descriptor
+
+    @property
+    def chid(self):
+        return self.__chid
+
+    @property
+    def fd(self):
+        return self.__fd
+
+    @fd.setter
+    def fd(self, value):
+        self.__fd = value
+
+    @property
+    def pid(self):
+        return self.__pid
+
 def get_message_body(ql, msg, parts):
     parts = c_int32(parts).value
     if parts >= 0:
