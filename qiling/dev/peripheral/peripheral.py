f695871fd249e2ffa3b49443b01dde770d1e4806
dataisland <dataisland@outlook.com>
2021-07-26 15:28:42 +0800

    Add gereral api for perip

--- qiling/dev/peripheral/peripheral.py
+++ qiling/dev/peripheral/peripheral.py
@@ -16,6 +16,14 @@ class Peripheral:
     def writeDoubleWord(self, offset, value):
         pass
 
+    def read(self, offset, size):
+        if size == 4:
+            return self.readDoubleWord(offset)
+
+    def write(self, offset, size, value):
+        if size == 4:
+            return self.writeDoubleWord(offset, value)
+
     @property
     def name(self):
         return 'unknown'
