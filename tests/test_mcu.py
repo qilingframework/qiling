9c046d9369441772b9f4e853ed1626fbf299af46
dataisland <dataisland@outlook.com>
2021-08-07 01:03:25 +0800

    Almost Finish USART Input and Output

--- tests/test_mcu.py
+++ tests/test_mcu.py
@@ -41,6 +41,39 @@ class MCUTest(unittest.TestCase):
 
         del ql
 
+    def test_mcu_usart_input_stm32f411(self):
+        ql = Qiling(["../examples/rootfs/stm32f411/hex/md5_server.hex"],                    
+            archtype="cortex_m", profile="stm32f411", verbose=QL_VERBOSE.OFF)
+        
+        ql.hw.create('USART', 'usart2', (0x40004400, 0x40004800))
+        ql.hw.create('STM32F4RCC', 'rcc', (0x40023800, 0x40023C00))
+
+
+        ql.run(count=1000)
+        
+        ql.hw.usart2.send(b'Hello\n')
+        ql.run(count=30000)
+
+        buf = ql.hw.usart2.recv()
+        print('[1] Received from usart: ', buf)
+        self.assertEqual(buf, b'8b1a9953c4611296a827abf8c47804d7\n')
+
+        ql.hw.usart2.send(b'USART\n')
+        ql.run(count=30000)
+
+        buf = ql.hw.usart2.recv()
+        print('[2] Received from usart: ', buf)
+        self.assertEqual(buf, b'2daeb613094400290a24fe5086c68f06\n')
+
+
+        ql.hw.usart2.send(b'Input\n')
+        ql.run(count=30000)
+        
+        buf = ql.hw.usart2.recv()
+        print('[3] Received from usart: ', buf)
+        self.assertEqual(buf, b'324118a6721dd6b8a9b9f4e327df2bf5\n')
+
+
 if __name__ == "__main__":
     unittest.main()
 
