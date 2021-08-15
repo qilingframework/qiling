1ddbe6db5162c89d963802f6b1336572b55d80d7
Glenn Washburn <development@efficientek.com>
2021-08-15 18:00:26 +0000

    Add Win32 16-bit compatibility file access API and constants

--- qiling/os/windows/const.py
+++ qiling/os/windows/const.py
@@ -687,3 +687,22 @@ ObjectAllTypesInformation = 0x3
 # https://docs.microsoft.com/en-us/windows/win32/api/handleapi/nf-handleapi-sethandleinformation
 HANDLE_FLAG_INHERIT = 0x1
 HANDLE_FLAG_PROTECT_FROM_CLOSE = 0x2
+
+# https://docs.microsoft.com/en-us/windows/win32/api/winbase/nf-winbase-openfile
+OF_READ      = 0x00000000
+OF_WRITE     = 0x00000001
+OF_READWRITE = 0x00000002
+OF_PARSE     = 0x00000100
+OF_DELETE    = 0x00000200
+OF_VERIFY    = 0x00000400
+OF_CANCEL    = 0x00000800
+OF_CREATE    = 0x00001000
+OF_PROMPT    = 0x00002000
+OF_EXIST     = 0x00004000
+OF_REOPEN    = 0x00008000
+
+OF_SHARE_COMPAT     = 0x00000000
+OF_SHARE_EXCLUSIVE  = 0x00000010
+OF_SHARE_DENY_WRITE = 0x00000020
+OF_SHARE_DENY_READ  = 0x00000030
+OF_SHARE_DENY_NONE  = 0x00000040
