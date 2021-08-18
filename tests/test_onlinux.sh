c6c0c373fe6e90dde208544b1aa324a3b768e97e
xwings <kj@xandora.net>
2021-08-18 17:10:38 +0800

    refactor ci code

--- tests/test_onlinux.sh
+++ tests/test_onlinux.sh
@@ -4,6 +4,15 @@
 # Cross Platform and Multi Architecture Advanced Binary Emulation Framework
 #
 
+pip3 install . 
+
+cd ../examples
+rm -rf rootfs
+wget https://github.com/qilingframework/rootfs/archive/refs/heads/master.zip
+unzip master.zip && mv rootfs-master rootfs
+cd rootfs/x86_linux/kernel && unzip -P infected m0hamed_rootkit.ko.zip
+cd ../../../tests
+
 python3 ./test_posix.py && 
 python3 ./test_elf_multithread.py &&
 python3 ./test_elf_ko.py &&
@@ -13,3 +22,4 @@ python3 ./test_uefi.py &&
 python3 ./test_shellcode.py && 
 python3 ./test_edl.py &&
 python3 ./test_qnx.py 
+
