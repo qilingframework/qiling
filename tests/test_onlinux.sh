57ef6b7e00816e6980709a0c5272082d6d05c011
xwings <kj@xandora.net>
2021-08-19 14:56:09 +0800

    latest onlinux test

--- tests/test_onlinux.sh
+++ tests/test_onlinux.sh
@@ -7,9 +7,12 @@
 python3 ./test_posix.py && 
 python3 ./test_elf_multithread.py &&
 python3 ./test_elf_ko.py &&
-python3 ./test_android.py && 
 python3 ./test_debugger.py && 
 python3 ./test_uefi.py && 
 python3 ./test_shellcode.py && 
 python3 ./test_edl.py &&
-python3 ./test_qnx.py 
+python3 ./test_qnx.py &&
+#python3 ./test_android.py &&
+
+[ -f test_mcu.py ] && python3 ./test_mcu.py &&
+[ -d ../qiling/engine/tests ] && cd ../qiling/engine/tests && python3 ./test_evm.py 
