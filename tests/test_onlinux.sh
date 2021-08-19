eeabbbc3d883e515b6a5a556c64f874152a11037
xwings <kj@xandora.net>
2021-08-19 12:22:10 +0800

    add test_android.py back

--- tests/test_onlinux.sh
+++ tests/test_onlinux.sh
@@ -21,7 +21,7 @@
 python3 ./test_posix.py && 
 python3 ./test_elf_multithread.py &&
 python3 ./test_elf_ko.py &&
-#python3 ./test_android.py && 
+python3 ./test_android.py && 
 python3 ./test_debugger.py && 
 python3 ./test_uefi.py && 
 python3 ./test_shellcode.py && 
