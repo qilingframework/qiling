7d881e789f6fa7071635366f3fb75da20f21edf5
xwings <kj@xandora.net>
2021-08-19 15:48:23 +0800

    update setup.py and get ready for evm

--- setup.py
+++ setup.py
@@ -22,6 +22,24 @@ requirements = [
     "gevent>=20.9.0"
 ]
 
+evm_extra = {
+    "evm": [
+         "blake2b-py>=0.1.2",
+         "cached-property>=1.5.2;python_version<'3.8'",
+         "typing-extensions>=3.7.4.3;python_version<'3.8'",
+         "eth-keys>=0.2.1",
+         "eth-typing>=2.2.0",
+         "eth-utils>=1.9.4",
+         "eth_abi>=2.1.1",
+         "lru-dict>=1.1.6",
+         "py-ecc>=1.4.7",
+         "rlp>=2",
+         "trie==2.0.0-alpha.5",
+         "eth-hash[pycryptodome]",
+         "numpy",
+     ] 
+}
+
 with open("README.md", "r", encoding="utf-8") as ld:
     long_description = ld.read()
 
@@ -67,4 +85,5 @@ setup(
     scripts=['qltool'],
     include_package_data=True,
     install_requires=requirements,
+    extras_require=evm_extra,
 )
