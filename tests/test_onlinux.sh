cea45face30c3a0e21831e12ac81e9b38bf93ca1
xwings <kj@xandora.net>
2021-08-20 22:47:38 +0800

    fix evm test

--- tests/test_onlinux.sh
+++ tests/test_onlinux.sh
@@ -13,3 +13,7 @@ python3 ./test_uefi.py &&
 python3 ./test_shellcode.py && 
 python3 ./test_edl.py &&
 python3 ./test_qnx.py && echo "Done OS Test"
+
+if [ -f  ./test_evm.py ]; then
+    python3 ./test_evm.py && echo "Done EVM test"
+fi
