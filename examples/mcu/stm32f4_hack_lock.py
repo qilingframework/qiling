3c279f9bea044d520fa974a81673f3c8f3f93d1e
dataisland <dataisland@outlook.com>
2021-08-25 14:15:40 +0800

    Add hack lock example

---
+++ examples/mcu/stm32f4_hack_lock.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# 
+# Cross Platform and Multi Architecture Advanced Binary Emulation Framework
+#
+
+
+import sys
+sys.path.append("..")
+
+from qiling.core import Qiling
+from qiling.const import QL_VERBOSE
+        
+
+ql = Qiling(["/home/moe/KJ_Lock/Project/KJ_Lock(mdk).hex"],                    
+                    archtype="cortex_m", profile="stm32f407", verbose=QL_VERBOSE.DEFAULT)
+
+ql.hw.show_info()
+ql.mem.show_mapinfo()
+
+ql.patch(0x80031e4, b'\x00\xBF' * 11)
+ql.patch(0x80032f8, b'\x00\xBF' * 13)
+ql.patch(0x80013b8, b'\x00\xBF' * 10)
+
+print('Start')
+ql.run(end=0x80031e0|1)
+ql.run(count=100, end=0x8003200|1)
+
+# ql.hw.usart1.send(b'618618\r')
+ql.hw.usart1.send(b'778899\r')
+
+for _ in range(7):
+    ql.run(end=0x8003216|1)
+    print('Input:', chr(ql.reg.read('r0')))
+
+ql.run(end=0x8003224|1)
+print('Success')
