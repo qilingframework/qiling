04bac26266981657d4ce64a3dfef02411a67a903
dataisland <dataisland@outlook.com>
2021-09-09 15:54:05 +0800

    Update the I2C setting

--- qiling/hw/i2c/stm32f4xx_i2c.py
+++ qiling/hw/i2c/stm32f4xx_i2c.py
@@ -8,30 +8,30 @@ from qiling.hw.peripheral import QlPeripheral
 
 
 class STM32F4xxI2c(QlPeripheral):
-    class Type(ctypes.Structure):
-        """ the structure available in :
-			stm32f413xx.h
-			stm32f469xx.h
-			stm32f446xx.h
-			stm32f427xx.h
-			stm32f401xc.h
-			stm32f412cx.h
-			stm32f410rx.h
-			stm32f410tx.h
-			stm32f439xx.h
-			stm32f412vx.h
-			stm32f479xx.h
-			stm32f429xx.h
-			stm32f412rx.h
+	class Type(ctypes.Structure):
+		""" the structure is available in :
 			stm32f423xx.h
-			stm32f437xx.h
-			stm32f412zx.h
-			stm32f401xe.h
+			stm32f469xx.h
+			stm32f427xx.h
+			stm32f479xx.h
+			stm32f413xx.h
+			stm32f429xx.h
+			stm32f439xx.h
+			stm32f412cx.h
+			stm32f412rx.h
+			stm32f410tx.h
 			stm32f410cx.h
+			stm32f412zx.h
+			stm32f446xx.h
+			stm32f401xc.h
+			stm32f437xx.h
+			stm32f401xe.h
+			stm32f412vx.h
+			stm32f410rx.h
 			stm32f411xe.h 
 		"""
 
-        _fields_ = [
+		_fields_ = [
 			('CR1'  , ctypes.c_uint32),  # I2C Control register 1,     Address offset: 0x00
 			('CR2'  , ctypes.c_uint32),  # I2C Control register 2,     Address offset: 0x04
 			('OAR1' , ctypes.c_uint32),  # I2C Own address register 1, Address offset: 0x08
@@ -43,3 +43,22 @@ class STM32F4xxI2c(QlPeripheral):
 			('TRISE', ctypes.c_uint32),  # I2C TRISE register,         Address offset: 0x20
 			('FLTR' , ctypes.c_uint32),  # I2C FLTR register,          Address offset: 0x24
 		]
+
+	def __init__(self, ql, label, ev_intn=None, er_intn=None):
+		super().__init__(ql, label)
+
+		self.i2c = self.struct(
+			TRISE = 0x0002
+		)
+
+		self.ev_intn = ev_intn
+		self.er_intn = er_intn
+
+	def read(self, offset, size):
+		buf = ctypes.create_string_buffer(size)
+		ctypes.memmove(buf, ctypes.addressof(self.i2c) + offset, size)
+		return int.from_bytes(buf.raw, byteorder='little')
+
+	def write(self, offset, size, value):
+		data = (value).to_bytes(size, 'little')
+		ctypes.memmove(ctypes.addressof(self.i2c) + offset, data, size)
