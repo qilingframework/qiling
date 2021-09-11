e8309500daa931834bd389c477baea44f796bf27
elicn <elichn@gmail.com>
2021-09-12 01:25:23 +0300

    Refactored prctl

--- qiling/os/posix/syscall/prctl.py
+++ qiling/os/posix/syscall/prctl.py
@@ -3,22 +3,28 @@
 # Cross Platform and Multi Architecture Advanced Binary Emulation Framework
 #
 
+from qiling import Qiling
+from qiling.arch.x86_const import FSMSR, GSMSR
 
-from qiling.const import *
-from qiling.os.linux.thread import *
-from qiling.const import *
-from qiling.os.posix.filestruct import *
-from qiling.os.filestruct import *
-from qiling.os.posix.const_mapping import *
-from qiling.exception import *
+def ql_syscall_arch_prctl(ql: Qiling, code: int, addr: int):
+    ARCH_SET_GS = 0x1001
+    ARCH_SET_FS = 0x1002
+    ARCH_GET_FS = 0x1003
+    ARCH_GET_GS = 0x1004
 
-def ql_syscall_arch_prctl(ql, ARCHX, ARCH_SET_FS, *args, **kw):
-    FSMSR = 0xC0000100
-    ql.reg.msr(FSMSR, ARCH_SET_FS)
-    regreturn = 0
-    return regreturn
+    handlers = {
+        ARCH_SET_GS : lambda : ql.reg.msr(GSMSR, addr),
+        ARCH_SET_FS : lambda : ql.reg.msr(FSMSR, addr),
+        ARCH_GET_FS : lambda : ql.mem.write(addr, ql.pack64(ql.reg.msr(FSMSR))),
+        ARCH_GET_GS : lambda : ql.mem.write(addr, ql.pack64(ql.reg.msr(GSMSR)))
+    }
 
+    if code not in handlers:
+        raise NotImplementedError(f'prctl code {code:#x} not implemented')
 
-def ql_syscall_prctl(ql, option, arg1, arg2, arg3, arg4, arg5, *args, **kw):
-    regreturn = 0
-    return regreturn
+    handlers[code]()
+
+    return 0
+
+def ql_syscall_prctl(ql: Qiling, option: int, arg2: int, arg3: int, arg4: int, arg5: int):
+    return 0
