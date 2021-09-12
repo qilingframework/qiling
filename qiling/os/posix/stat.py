6356fdb404267f578ed58c11746a22fcd71ed729
elicn <elichn@gmail.com>
2021-09-12 22:49:52 +0300

    Tidy up stat

--- qiling/os/posix/stat.py
+++ qiling/os/posix/stat.py
@@ -6,32 +6,30 @@
 import os
 
 class StatBase:
-    def __init__(self):
-        self._stat_buf = None
+    def __init__(self, stat: os.stat_result):
+        self._stat_buf = stat
 
     # Never iterate this object!
     def __getitem__(self, key):
         if type(key) is not str:
             raise TypeError
-        if not key.startswith("__") and key in dir(self._stat_buf):
+
+        if not key.startswith("__") and hasattr(self._stat_buf, key):
             return self._stat_buf.__getattribute__(key)
+
         return 0
-    
+
     def __getattr__(self, key):
         return self.__getitem__(key)
+
 class Stat(StatBase):
     def __init__(self, path):
-        super(Stat, self).__init__()
-        self._stat_buf = os.stat(path)
+        super().__init__(os.stat(path))
 
 class Fstat(StatBase):
-    def __init__(self, fd):
-        super(Fstat, self).__init__()
-        self._stat_buf = os.fstat(fd)
+    def __init__(self, fd: int):
+        super().__init__(os.fstat(fd))
 
 class Lstat(StatBase):
     def __init__(self, path):
-        super(Lstat, self).__init__()
-        self._stat_buf = os.lstat(path)
-
-
+        super().__init__(os.lstat(path))
