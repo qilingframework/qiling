78d63ce2a941df408214e6296ee7ddfa469297d7
dataisland <dataisland@outlook.com>
2021-10-04 09:57:07 +0800

    Adjust the create interface

--- examples/mcu/stm32f4_i2c.py
+++ examples/mcu/stm32f4_i2c.py
@@ -7,9 +7,12 @@ from qiling.const import QL_VERBOSE
 import pygame
 import threading
         
+
 class LCD:
     @classmethod
-    def get_raw_pattern(cls, lo, up):
+    def get_pattern(cls, ch):
+        lo = (ch >> 0) & 0xf
+        up = (ch >> 4) & 0xf
         with open('LCD1602A.txt') as f:
             lines = f.read().splitlines()[lo * 9 + 1: lo * 9 + 9]
             pattern = [line[up * 6 + 1: up * 6 + 6] for line in lines]
@@ -17,32 +20,14 @@ class LCD:
         return pattern
 
     @classmethod
-    def get_char_pattern(cls, ch):
-        lo = (ch >> 0) & 0xf
-        up = (ch >> 4) & 0xf
-        return LCD1602.get_raw_pattern(lo, up)
-
-    @classmethod
-    def make_single_line(cls, info):
-        patterns = []
-        for pack in info:
-            patterns.append(LCD1602.get_char_pattern(pack))
-            
-        screen = []    
-        for row in range(8):
-            line = ''
-            for pattern in patterns:
-                line += pattern[row]
-            screen.append(line)
-        
-        return screen
-
-    @classmethod
     def make_screen(cls, infos):
-        screen = []
+        sc = []
         for info in infos:
-            screen += LCD1602.make_single_line(info)
-        return screen
+            ps = [LCD.get_pattern(x) for x in info]            
+            ln  = [''.join(p[r] for p in ps) for r in range(8)]
+            sc += ln
+
+        return sc
 
 class LCD1602(LCD):    
     def __init__(self) -> None:
@@ -51,10 +36,9 @@ class LCD1602(LCD):
         self.rows, self.cols = 2, 16
         self.cheight, self.cwidth = 8, 5        
         
-        self.cur_row = 0
-        self.cur_col = 0
+        self.cur_row, self.cur_col = 0, 0
+         
         self.buf = []
-
         self.data = [[ord(' ') for _ in range(self.cols)] for _ in range(self.rows)]
         self.pixels = LCD.make_screen(self.data)
 
@@ -102,24 +86,25 @@ class LCD1602(LCD):
         if len(self.buf) == 4:
             up = self.buf[0] & 0xf0
             lo = self.buf[3] & 0xf0
-
             cmd = up | (lo >> 4)
+
             if self.buf[0] & 0x1:                
                 if self.cur_col < 16 and self.cur_row < 2:
                     self.data[self.cur_row][self.cur_col] = cmd                
                     self.cur_col += 1
                 self.pixels = LCD.make_screen(self.data)
+            
             elif cmd == 0x1:
                 self.data = [[ord(' ') for _ in range(self.cols)] for _ in range(self.rows)]
                 self.pixels = LCD.make_screen(self.data)
-            elif up == 0xc0:
-                self.cur_row = 1
-                self.cur_col = lo >> 4
+            
             elif up == 0x80:
-                self.cur_row = 0
-                self.cur_col = lo >> 4
+                self.cur_row, self.cur_col = 0, lo >> 4
 
-            self.buf.clear()        
+            elif up == 0xc0:
+                self.cur_row, self.cur_col = 1, lo >> 4                
+
+            self.buf = []
 
     def run(self):
         threading.Thread(target=self.render).start()
@@ -128,12 +113,10 @@ def make(path, lcd):
     ql = Qiling([path],
         archtype="cortex_m", profile="stm32f411", verbose=QL_VERBOSE.DEFAULT)
 
-    ql.hw.create('i2c1')
+    ql.hw.create('i2c1').connect(lcd)
     ql.hw.create('rcc')
     ql.hw.create('gpioa')
-    ql.hw.create('gpiob')
-
-    ql.hw.i2c1.connect(lcd)
+    ql.hw.create('gpiob')    
     
     return ql
 
