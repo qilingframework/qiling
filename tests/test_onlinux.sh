e73f87eefae29e0e5da6192725c9a0079f6f17dc
xwings <kj@xandora.net>
2021-10-07 19:37:58 +0800

    sync with git

--- tests/test_onlinux.sh
+++ tests/test_onlinux.sh
@@ -14,4 +14,5 @@ python3 ./test_edl.py &&
 python3 ./test_qnx.py && 
 python3 ./test_android.py &&
 python3 ./test_mcu.py &&
-test -f. ./test_evm.py && python3 ./test_evm.py
+python3 ./test_evm.py &&
+echo "Done Test"
