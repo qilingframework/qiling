f6c4d2fb7629a883fd86cdfa7504ef4c8d71bd1d
dataisland <dataisland@outlook.com>
2021-11-01 00:24:12 +0800

    Add support for device connect

--- qiling/hw/connectivity.py
+++ qiling/hw/connectivity.py
@@ -28,11 +28,17 @@ class QlConnectivityPeripheral(QlPeripheral):
         """        
         _fields_ = []
 
-    def __init__(self, ql: Qiling, label: str):
+    def __init__(self, ql: Qiling, label: str, limit:int = 1):
         super().__init__(ql, label)
         
         self.rtube = queue.Queue()
         self.wtube = queue.Queue()
+
+        self.limit = limit
+        self.device_list = []
+
+    def has_input(self):
+        return not self.rtube.empty()
 
     def send(self, data: bytes):
         """ Send data into the peripheral.
@@ -51,14 +57,20 @@ class QlConnectivityPeripheral(QlPeripheral):
                 data = ql.hw.i2c1.send()
         """
         data = bytearray()
-        while not self.wtube.empty() and numb != 0:
+        while self.can_recv() and numb != 0:
             data.append(self.wtube.get())
             numb -= 1
 
-        return bytes(data)
-    
+        return bytes(data) 
+
     def can_recv(self):
-        return not self.rtube.empty()
+        return not self.wtube.empty()
+
+    def send_to_user(self, data: int):
+        """ send single byte to user
+        """
+        
+        self.wtube.put(data)
 
     def recv_from_user(self) -> bytes:
         """ Read single byte from user input
@@ -66,8 +78,18 @@ class QlConnectivityPeripheral(QlPeripheral):
         
         return self.rtube.get()
 
-    def send_to_user(self, data: int):
-        """ send single byte to user
-        """
+    def connect(self, device):
+        if len(self.device_list) < self.limit:
+            self.device_list.append(device)
+
+    @staticmethod
+    def device_handler(func):
+        def wrapper(self):
+            func(self)
+
+            if self.device_list and self.can_recv():
+                data = self.recv(numb=1)			
+                for device in self.device_list:
+                    device.send(data)
         
-        self.wtube.put(data)
+        return wrapper
