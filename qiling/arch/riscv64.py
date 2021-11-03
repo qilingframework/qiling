8372355c454b055ed33778683a749ab8bb1128a7
dataisland <dataisland@outlook.com>
2021-11-03 15:57:00 +0800

    Add riscv 32/64 bit structure

---
+++ qiling/arch/riscv64.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# 
+# Cross Platform and Multi Architecture Advanced Binary Emulation Framework
+#
+
+from unicorn import Uc, UC_ARCH_RISCV, UC_MODE_RISCV64
+from capstone import Cs, CS_ARCH_RISCV, CS_MODE_RISCV64, CS_MODE_RISCVC
+from keystone import Ks
+
+from qiling import Qiling
+from qiling.arch.arch import QlArch
+from qiling.arch.riscv_const import *
+from qiling.exception import QlErrorNotImplemented
+
+
+class QlArchRISCV64(QlArch):
+    def __init__(self, ql: Qiling):
+        super().__init__(ql)
+
+        self.ql.reg.expand_mapping(reg_map)
+        self.ql.reg.register_sp(reg_map["sp"])
+        self.ql.reg.register_pc(reg_map["pc"])
+
+    # get initialized unicorn engine
+    def get_init_uc(self) -> Uc:
+        return Uc(UC_ARCH_RISCV, UC_MODE_RISCV64)
+
+    def create_disassembler(self) -> Cs:
+        # raise QlErrorNotImplemented("Capstone does not yet support riscv")
+        return Cs(CS_ARCH_RISCV, CS_MODE_RISCV64 + CS_MODE_RISCVC)
+
+    def create_assembler(self) -> Ks:
+        raise QlErrorNotImplemented("Keystone does not yet support riscv")
