161b200c93cf6d6958f9c856809a2201dcd17642
dataisland <dataisland@outlook.com>
2021-11-13 23:51:42 +0800

    Fix the gd32vf103_blink example

--- examples/mcu/gd32vf103_blink.py
+++ examples/mcu/gd32vf103_blink.py
@@ -3,9 +3,10 @@ sys.path.append("../..")
 
 from qiling.core import Qiling
 from qiling.const import QL_VERBOSE
+from qiling.extensions.soc.gd32vf1 import gd32vf103_env
 
 ql = Qiling(['../rootfs/mcu/gd32vf103/blink.hex'], archtype="riscv64", 
-                    profile="gd32vf103", verbose=QL_VERBOSE.DEBUG)
+                    env=gd32vf103_env, verbose=QL_VERBOSE.DEBUG)
 
 ql.hw.create('rcu')
 ql.hw.create('gpioa').watch()
