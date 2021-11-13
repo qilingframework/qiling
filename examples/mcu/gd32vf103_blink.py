d8abe2544bdcc1c60671b0eca94e532b94b24b57
dataisland <dataisland@outlook.com>
2021-11-13 16:19:29 +0800

    Add GD32 hello world example

---
+++ examples/mcu/gd32vf103_blink.py
@@ -0,0 +1,23 @@
+import sys
+sys.path.append("../..")
+
+from qiling.core import Qiling
+from qiling.const import QL_VERBOSE
+
+ql = Qiling(['../rootfs/mcu/gd32vf103/blink.hex'], archtype="riscv64", 
+                    profile="gd32vf103", verbose=QL_VERBOSE.DEBUG)
+
+ql.hw.create('rcu')
+ql.hw.create('gpioa').watch()
+ql.hw.create('gpioc').watch()
+
+delay_cycles_begin = 0x800015c
+delay_cycles_end = 0x800018c
+
+def skip_delay(ql):
+    ql.reg.pc = delay_cycles_end
+
+ql.hook_address(skip_delay, delay_cycles_begin)
+ql.hw.gpioc.hook_set(13, lambda : print('Set PC13'))
+
+ql.run(count=20000)
