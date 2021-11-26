58275c03d0e5109c34d3f0753298562031cdacbc
xwings <kj@xandora.net>
2021-11-26 11:52:13 +0800

    adding more test for ci

--- tests/test_httpd.py
+++ tests/test_httpd.py
@@ -1,19 +1,20 @@
 
-import os, socket, threading, requests
+import sys, os, socket, threading, requests
 
 sys.path.append("..")
 from qiling import Qiling
 from qiling.const import QL_VERBOSE
 
 def test_http_elf_linux_arm():
-    ql = Qiling(["../examples/rootfs/arm_linux/bin/vshttpd","-p","20011","-r","../www"], "../examples/rootfs/arm_linux", multithread=True)    
+    ql = Qiling(["../examples/rootfs/arm_linux/bin/vshttpd","-p","20011","-r","www"], "../examples/rootfs/arm_linux", multithread=True, verbose=QL_VERBOSE.DEBUG)    
     ql.run()
 
 
 if __name__ == "__main__":
-    test_http_elf_linux_arm_therad = threading.Thread(target=test_http_elf_linux_arm, daemon=True)
-    test_http_elf_linux_arm_therad.start()
+    # test_http_elf_linux_arm_therad = threading.Thread(target=test_http_elf_linux_arm, daemon=True)
+    # test_http_elf_linux_arm_therad.start()
 
 
-    r = requests.get('https://localhost:20011/')
-    print(r.text)[:200]
+    # r = requests.get('https://localhost:20011/')
+    # print(r.text)[:200]
+    test_http_elf_linux_arm()
