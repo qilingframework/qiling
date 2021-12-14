747413c710235f338ba98c1410a8dfaa91ddbf58
dataisland <dataisland@outlook.com>
2021-12-14 15:09:09 +0800

    Add stn32 DBG struct

--- qiling/hw/misc/__init__.py
+++ qiling/hw/misc/__init__.py
@@ -12,4 +12,5 @@ from .stm32f4xx_rcc_derive import (
 )
 from .stm32f4xx_syscfg import STM32F4xxSyscfg
 from .stm32f4xx_flash import STM32F4xxFlash
+from .stm32f4xx_dbg import STM32F4xxDbgmcu
 from .gd32vf1xx_rcu import GD32VF1xxRcu
