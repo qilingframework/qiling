e010183bb35baaa3ada8944a3ff61563dbde97e9
dataisland <dataisland@outlook.com>
2021-12-14 17:05:26 +0800

    Add CRC Support

--- qiling/hw/math/stm32f4xx_crc.py
+++ qiling/hw/math/stm32f4xx_crc.py
@@ -4,12 +4,13 @@
 #
 
 import ctypes
+import binascii
 from qiling.hw.peripheral import QlPeripheral
 
 
 class STM32F4xxCrc(QlPeripheral):
-    class Type(ctypes.Structure):
-        """ the structure available in :
+	class Type(ctypes.Structure):
+		""" the structure available in :
 			stm32f413xx.h
 			stm32f407xx.h
 			stm32f469xx.h
@@ -35,10 +36,41 @@ class STM32F4xxCrc(QlPeripheral):
 			stm32f411xe.h 
 		"""
 
-        _fields_ = [
+		_fields_ = [
 			('DR'       , ctypes.c_uint32),  # CRC Data register,             Address offset: 0x00
 			('IDR'      , ctypes.c_uint8),   # CRC Independent data register, Address offset: 0x04
 			('RESERVED0', ctypes.c_uint8),   # Reserved, 0x05
 			('RESERVED1', ctypes.c_uint8),   # Reserved, 0x06
 			('CR'       , ctypes.c_uint32),  # CRC Control register,          Address offset: 0x08
-        ]
+		]
+
+	def __init__(self, ql, label):
+		super().__init__(ql, label)
+
+		self.crc = self.struct(
+            DR    =  0xffffffff,
+        )
+
+	@QlPeripheral.monitor()
+	def read(self, offset: int, size: int) -> int:
+		buf = ctypes.create_string_buffer(size)
+		ctypes.memmove(buf, ctypes.addressof(self.crc) + offset, size)
+		return int.from_bytes(buf.raw, byteorder='little')
+    
+	@QlPeripheral.monitor()
+	def write(self, offset: int, size: int, value: int):
+		if offset == self.struct.CR.offset:
+			if value & 1: # RESET bit
+				self.crc.DR = 0xffffffff
+			return
+		
+		elif offset == self.struct.DR.offset:
+			for i in range(31, -1, -1):
+				if self.crc.DR & 0x80000000:
+					self.crc.DR <<= 1
+					self.crc.DR ^= 0x04c11db7
+				else:
+					self.crc.DR <<= 1
+
+				if value & (1 << i):
+					self.crc.DR ^= 0x04c11db7
