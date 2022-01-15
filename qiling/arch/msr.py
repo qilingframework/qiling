0ff8c1f5c8cf97132f46f99bfcdfe85e2923a047
elicn <elichn@gmail.com>
2022-01-15 18:30:36 +0200

    Extract msr from regs and make available only in QlArchIntel

---
+++ qiling/arch/msr.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# 
+# Cross Platform and Multi Architecture Advanced Binary Emulation Framework
+#
+
+from unicorn import Uc
+
+class QlMsrManager:
+    """Enables access to Intel MSR.
+    """
+
+    def __init__(self, uc: Uc) -> None:
+        self.uc = uc
+
+    def read(self, msr: int) -> int:
+        """Read a model-specific register value.
+        """
+
+        return self.uc.msr_read(msr)
+
+    def write(self, msr: int, value: int):
+        """Write a model-specific register value.
+        """
+
+        self.uc.msr_write(msr, value)
