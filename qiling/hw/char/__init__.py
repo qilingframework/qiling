4133f7e7d3ad2c84f7208d65c82cdb13dc416a9c
dataisland <dataisland@outlook.com>
2022-02-05 17:56:11 +0800

    Add stm32f1 usart

--- qiling/hw/char/__init__.py
+++ qiling/hw/char/__init__.py
@@ -3,5 +3,6 @@
 # Cross Platform and Multi Architecture Advanced Binary Emulation Framework
 #
 
+from .stm32f1xx_usart import STM32F1xxUsart
 from .stm32f4xx_usart import STM32F4xxUsart
 from .gd32vf1xx_usart import GD32VF1xxUsart
