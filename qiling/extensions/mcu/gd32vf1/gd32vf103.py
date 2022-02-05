ea92ed9c4abd7c513c5da56a9d2fbe7a750b82e7
dataisland <dataisland@outlook.com>
2022-02-05 13:05:05 +0800

    Format GD32 config format

--- qiling/extensions/mcu/gd32vf1/gd32vf103.py
+++ qiling/extensions/mcu/gd32vf1/gd32vf103.py
@@ -4,45 +4,180 @@
 #
 
 gd32vf103 = {
-    "ADC0": {
-        "base": 0x40012400,
-        "struct": "GD32VF1xxAdc",
-        "kwargs": {
-            "intn": 37
-        },
-        "type": "peripheral"
+    "ECLIC": {
+        "base": 0xd2000000,
+        "struct": "GD32VF1xxEclic",
+        "type": "core"
     },
-    "ADC1": {
-        "base": 0x40012800,
-        "struct": "GD32VF1xxAdc",
-        "kwargs": {
-            "intn": 37
-        },
-        "type": "peripheral"
-    },
-    "AFIO": {
-        "base": 0x40010000,
-        "struct": "GD32VF1xxAfio",
-        "type": "peripheral"
-    },
-    "BKP": {
-        "base": 0x40006c00,
-        "struct": "GD32VF1xxBkp",
-        "type": "peripheral"
+    "FLASH": {
+        "base": 0x8000000,
+        "size": 0x20000,
+        "type": "memory"
     },
     "BOOT": {
         "base": 0x1fffb000,
         "size": 0x5000,
         "type": "memory"
     },
+    "SRAM": {
+        "base": 0x20000000,
+        "size": 0x18000,
+        "type": "memory"
+    },
+    "PERIP": {
+        "base": 0x40000000,
+        "size": 0x40000,
+        "type": "mmio"
+    },
+    "USBFS": {
+        "base": 0x50000000,
+        "size": 0x40000,
+        "type": "mmio"
+    },
+    "TIMER1": {
+        "base": 0x40000000,
+        "struct": "GD32VF1xxTimer",
+        "kwargs": {
+            "intn": 0x2f
+        },
+        "type": "peripheral"
+    },
+    "TIMER2": {
+        "base": 0x40000400,
+        "struct": "GD32VF1xxTimer",
+        "kwargs": {
+            "intn": 0x30
+        },
+        "type": "peripheral"
+    },
+    "TIMER3": {
+        "base": 0x40000800,
+        "struct": "GD32VF1xxTimer",
+        "kwargs": {
+            "intn": 0x31
+        },
+        "type": "peripheral"
+    },
+    "TIMER4": {
+        "base": 0x40000c00,
+        "struct": "GD32VF1xxTimer",
+        "kwargs": {
+            "intn": 0x45
+        },
+        "type": "peripheral"
+    },
+    "TIMER5": {
+        "base": 0x40001000,
+        "struct": "GD32VF1xxTimer",
+        "kwargs": {
+            "intn": 0x49
+        },
+        "type": "peripheral"
+    },
+    "TIMER6": {
+        "base": 0x40001400,
+        "struct": "GD32VF1xxTimer",
+        "kwargs": {
+            "intn": 0x4a
+        },
+        "type": "peripheral"
+    },
+    "RTC": {
+        "base": 0x40002800,
+        "struct": "GD32VF1xxRtc",
+        "kwargs": {
+            "alarm_intn": 0x3c,
+            "intn": 0x16
+        },
+        "type": "peripheral"
+    },
+    "WWDGT": {
+        "base": 0x40002c00,
+        "struct": "GD32VF1xxWwdgt",
+        "kwargs": {
+            "intn": 0x0
+        },
+        "type": "peripheral"
+    },
+    "FWDGT": {
+        "base": 0x40003000,
+        "struct": "GD32VF1xxFwdgt",
+        "type": "peripheral"
+    },
+    "SPI1": {
+        "base": 0x40003800,
+        "struct": "GD32VF1xxSpi",
+        "kwargs": {
+            "intn": 0x37
+        },
+        "type": "peripheral"
+    },
+    "SPI2": {
+        "base": 0x40003c00,
+        "struct": "GD32VF1xxSpi",
+        "kwargs": {
+            "intn": 0x46
+        },
+        "type": "peripheral"
+    },
+    "USART1": {
+        "base": 0x40004400,
+        "struct": "GD32VF1xxUsart",
+        "kwargs": {
+            "intn": 0x39
+        },
+        "type": "peripheral"
+    },
+    "USART2": {
+        "base": 0x40004800,
+        "struct": "GD32VF1xxUsart",
+        "kwargs": {
+            "intn": 0x3a
+        },
+        "type": "peripheral"
+    },
+    "UART3": {
+        "base": 0x40004c00,
+        "struct": "GD32VF1xxUart",
+        "kwargs": {
+            "intn": 0x47
+        },
+        "type": "peripheral"
+    },
+    "UART4": {
+        "base": 0x40005000,
+        "struct": "GD32VF1xxUart",
+        "kwargs": {
+            "intn": 0x48
+        },
+        "type": "peripheral"
+    },
+    "I2C0": {
+        "base": 0x40005400,
+        "struct": "GD32VF1xxI2c",
+        "kwargs": {
+            "er_intn": 0x33,
+            "ev_intn": 0x32
+        },
+        "type": "peripheral"
+    },
+    "I2C1": {
+        "base": 0x40005800,
+        "struct": "GD32VF1xxI2c",
+        "kwargs": {
+            "er_intn": 0x35,
+            "ev_intn": 0x34
+        },
+        "type": "peripheral"
+    },
     "CAN0": {
         "base": 0x40006400,
         "struct": "GD32VF1xxCan",
         "kwargs": {
-            "ewmc_intn": 41,
-            "rx0_intn": 39,
-            "rx1_intn": 40,
-            "tx_intn": 38
+            "ewmc_intn": 0x29,
+            "rx0_intn": 0x27,
+            "rx1_intn": 0x28,
+            "tx_intn": 0x26
         },
         "type": "peripheral"
     },
@@ -50,16 +185,21 @@ gd32vf103 = {
         "base": 0x40006800,
         "struct": "GD32VF1xxCan",
         "kwargs": {
-            "ewmc_intn": 85,
-            "rx0_intn": 83,
-            "rx1_intn": 84,
-            "tx_intn": 82
+            "ewmc_intn": 0x55,
+            "rx0_intn": 0x53,
+            "rx1_intn": 0x54,
+            "tx_intn": 0x52
         },
         "type": "peripheral"
     },
-    "CRC": {
-        "base": 0x40023000,
-        "struct": "GD32VF1xxCrc",
+    "BKP": {
+        "base": 0x40006c00,
+        "struct": "GD32VF1xxBkp",
+        "type": "peripheral"
+    },
+    "PMU": {
+        "base": 0x40007000,
+        "struct": "GD32VF1xxPmu",
         "type": "peripheral"
     },
     "DAC": {
@@ -67,77 +207,23 @@ gd32vf103 = {
         "struct": "GD32VF1xxDac",
         "type": "peripheral"
     },
-    "DBG": {
-        "base": 0xe0042000,
-        "struct": "GD32VF1xxDbg",
-        "type": "peripheral"
-    },
-    "DMA0": {
-        "base": 0x40020000,
-        "struct": "GD32VF1xxDma",
-        "kwargs": {
-            "stream0_intn": 30,
-            "stream1_intn": 31,
-            "stream2_intn": 32,
-            "stream3_intn": 33,
-            "stream4_intn": 34,
-            "stream5_intn": 35,
-            "stream6_intn": 36
-        },
-        "type": "peripheral"
-    },
-    "DMA1": {
-        "base": 0x40020400,
-        "struct": "GD32VF1xxDma",
-        "kwargs": {
-            "stream0_intn": 75,
-            "stream1_intn": 76,
-            "stream2_intn": 77,
-            "stream3_intn": 78,
-            "stream4_intn": 79
-        },
-        "type": "peripheral"
-    },
-    "ECLIC": {
-        "base": 0xd2000000,
-        "struct": "GD32VF1xxEclic",
-        "type": "core"
-    },
-    "EXMC": {
-        "base": 0xa0000000,
-        "struct": "GD32VF1xxExmc",
+    "AFIO": {
+        "base": 0x40010000,
+        "struct": "GD32VF1xxAfio",
         "type": "peripheral"
     },
     "EXTI": {
         "base": 0x40010400,
         "struct": "GD32VF1xxExti",
         "kwargs": {
-            "line0_intn": 25,
-            "line15_10_intn": 59,
-            "line1_intn": 26,
-            "line2_intn": 27,
-            "line3_intn": 28,
-            "line4_intn": 29,
-            "line9_5_intn": 42
+            "line0_intn": 0x19,
+            "line15_10_intn": 0x3b,
+            "line1_intn": 0x1a,
+            "line2_intn": 0x1b,
+            "line3_intn": 0x1c,
+            "line4_intn": 0x1d,
+            "line9_5_intn": 0x2a
         },
-        "type": "peripheral"
-    },
-    "FLASH": {
-        "base": 0x08000000,
-        "size": 0x20000,
-        "type": "memory"
-    },
-    "FMC": {
-        "base": 0x40022000,
-        "struct": "GD32VF1xxFmc",
-        "kwargs": {
-            "intn": 23
-        },
-        "type": "peripheral"
-    },
-    "FWDGT": {
-        "base": 0x40003000,
-        "struct": "GD32VF1xxFwdgt",
         "type": "peripheral"
     },
     "GPIOA": {
@@ -165,48 +251,30 @@ gd32vf103 = {
         "struct": "GD32VF1xxGpio",
         "type": "peripheral"
     },
-    "I2C0": {
-        "base": 0x40005400,
-        "struct": "GD32VF1xxI2c",
+    "ADC0": {
+        "base": 0x40012400,
+        "struct": "GD32VF1xxAdc",
         "kwargs": {
-            "er_intn": 51,
-            "ev_intn": 50
+            "intn": 0x25
         },
         "type": "peripheral"
     },
-    "I2C1": {
-        "base": 0x40005800,
-        "struct": "GD32VF1xxI2c",
+    "ADC1": {
+        "base": 0x40012800,
+        "struct": "GD32VF1xxAdc",
         "kwargs": {
-            "er_intn": 53,
-            "ev_intn": 52
+            "intn": 0x25
         },
         "type": "peripheral"
     },
-    "PERIP": {
-        "base": 0x40000000,
-        "size": 0x40000,
-        "type": "mmio"
-    },
-    "PMU": {
-        "base": 0x40007000,
-        "struct": "GD32VF1xxPmu",
-        "type": "peripheral"
-    },
-    "RCU": {
-        "base": 0x40021000,
-        "struct": "GD32VF1xxRcu",
+    "TIMER0": {
+        "base": 0x40012c00,
+        "struct": "GD32VF1xxTimer",
         "kwargs": {
-            "intn": 24
-        },
-        "type": "peripheral"
-    },
-    "RTC": {
-        "base": 0x40002800,
-        "struct": "GD32VF1xxRtc",
-        "kwargs": {
-            "alarm_intn": 60,
-            "intn": 22
+            "brk_intn": 0x2b,
+            "channel_intn": 0x2e,
+            "trg_cmt_intn": 0x2d,
+            "up_intn": 0x2c
         },
         "type": "peripheral"
     },
@@ -214,103 +282,7 @@ gd32vf103 = {
         "base": 0x40013000,
         "struct": "GD32VF1xxSpi",
         "kwargs": {
-            "intn": 54
-        },
-        "type": "peripheral"
-    },
-    "SPI1": {
-        "base": 0x40003800,
-        "struct": "GD32VF1xxSpi",
-        "kwargs": {
-            "intn": 55
-        },
-        "type": "peripheral"
-    },
-    "SPI2": {
-        "base": 0x40003c00,
-        "struct": "GD32VF1xxSpi",
-        "kwargs": {
-            "intn": 70
-        },
-        "type": "peripheral"
-    },
-    "SRAM": {
-        "base": 0x20000000,
-        "size": 0x18000,
-        "type": "memory"
-    },
-    "TIMER0": {
-        "base": 0x40012c00,
-        "struct": "GD32VF1xxTimer",
-        "kwargs": {
-            "brk_intn": 43,
-            "channel_intn": 46,
-            "trg_cmt_intn": 45,
-            "up_intn": 44
-        },
-        "type": "peripheral"
-    },
-    "TIMER1": {
-        "base": 0x40000000,
-        "struct": "GD32VF1xxTimer",
-        "kwargs": {
-            "intn": 47
-        },
-        "type": "peripheral"
-    },
-    "TIMER2": {
-        "base": 0x40000400,
-        "struct": "GD32VF1xxTimer",
-        "kwargs": {
-            "intn": 48
-        },
-        "type": "peripheral"
-    },
-    "TIMER3": {
-        "base": 0x40000800,
-        "struct": "GD32VF1xxTimer",
-        "kwargs": {
-            "intn": 49
-        },
-        "type": "peripheral"
-    },
-    "TIMER4": {
-        "base": 0x40000c00,
-        "struct": "GD32VF1xxTimer",
-        "kwargs": {
-            "intn": 69
-        },
-        "type": "peripheral"
-    },
-    "TIMER5": {
-        "base": 0x40001000,
-        "struct": "GD32VF1xxTimer",
-        "kwargs": {
-            "intn": 73
-        },
-        "type": "peripheral"
-    },
-    "TIMER6": {
-        "base": 0x40001400,
-        "struct": "GD32VF1xxTimer",
-        "kwargs": {
-            "intn": 74
-        },
-        "type": "peripheral"
-    },
-    "UART3": {
-        "base": 0x40004c00,
-        "struct": "GD32VF1xxUart",
-        "kwargs": {
-            "intn": 71
-        },
-        "type": "peripheral"
-    },
-    "UART4": {
-        "base": 0x40005000,
-        "struct": "GD32VF1xxUart",
-        "kwargs": {
-            "intn": 72
+            "intn": 0x36
         },
         "type": "peripheral"
     },
@@ -318,34 +290,55 @@ gd32vf103 = {
         "base": 0x40013800,
         "struct": "GD32VF1xxUsart",
         "kwargs": {
-            "intn": 56
+            "intn": 0x38
         },
         "type": "peripheral"
     },
-    "USART1": {
-        "base": 0x40004400,
-        "struct": "GD32VF1xxUsart",
+    "DMA0": {
+        "base": 0x40020000,
+        "struct": "GD32VF1xxDma",
         "kwargs": {
-            "intn": 57
+            "stream0_intn": 0x1e,
+            "stream1_intn": 0x1f,
+            "stream2_intn": 0x20,
+            "stream3_intn": 0x21,
+            "stream4_intn": 0x22,
+            "stream5_intn": 0x23,
+            "stream6_intn": 0x24
         },
         "type": "peripheral"
     },
-    "USART2": {
-        "base": 0x40004800,
-        "struct": "GD32VF1xxUsart",
+    "DMA1": {
+        "base": 0x40020400,
+        "struct": "GD32VF1xxDma",
         "kwargs": {
-            "intn": 58
+            "stream0_intn": 0x4b,
+            "stream1_intn": 0x4c,
+            "stream2_intn": 0x4d,
+            "stream3_intn": 0x4e,
+            "stream4_intn": 0x4f
         },
         "type": "peripheral"
     },
-    "USBFS": {
-        "base": 0x50000000,
-        "size": 0x40000,
-        "type": "mmio"
+    "RCU": {
+        "base": 0x40021000,
+        "struct": "GD32VF1xxRcu",
+        "kwargs": {
+            "intn": 0x18
+        },
+        "type": "peripheral"
     },
-    "USBFS_DEVICE": {
-        "base": 0x50000800,
-        "struct": "GD32VF1xxUsbfs",
+    "FMC": {
+        "base": 0x40022000,
+        "struct": "GD32VF1xxFmc",
+        "kwargs": {
+            "intn": 0x17
+        },
+        "type": "peripheral"
+    },
+    "CRC": {
+        "base": 0x40023000,
+        "struct": "GD32VF1xxCrc",
         "type": "peripheral"
     },
     "USBFS_GLOBAL": {
@@ -358,17 +351,24 @@ gd32vf103 = {
         "struct": "GD32VF1xxUsbfs",
         "type": "peripheral"
     },
+    "USBFS_DEVICE": {
+        "base": 0x50000800,
+        "struct": "GD32VF1xxUsbfs",
+        "type": "peripheral"
+    },
     "USBFS_PWRCLK": {
         "base": 0x50000e00,
         "struct": "GD32VF1xxUsbfs",
         "type": "peripheral"
     },
-    "WWDGT": {
-        "base": 0x40002c00,
-        "struct": "GD32VF1xxWwdgt",
-        "kwargs": {
-            "intn": 0
-        },
+    "EXMC": {
+        "base": 0xa0000000,
+        "struct": "GD32VF1xxExmc",
+        "type": "peripheral"
+    },
+    "DBG": {
+        "base": 0xe0042000,
+        "struct": "GD32VF1xxDbg",
         "type": "peripheral"
     }
 }
