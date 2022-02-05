f3886f1d75ad44e3f3bc0fe68d93f4c77e18dc43
dataisland <dataisland@outlook.com>
2022-02-05 16:31:12 +0800

    Add CortexM3 Timer

---
+++ qiling/hw/timer/cm3_systick.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# 
+# Cross Platform and Multi Architecture Advanced Binary Emulation Framework
+#
+
+import ctypes
+
+from qiling.hw.timer.cm_systick import CortexMSysTick
+
+
+class CortexM3SysTick(CortexMSysTick):
+    pass
