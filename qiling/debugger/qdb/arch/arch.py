fe21dbb9596f16b74c787a176d4f96292b744149
ucgJhe <ucg.Jhe@gmail.com>
2022-02-16 00:38:18 +0000

    revis read_insn signature

--- qiling/debugger/qdb/arch/arch.py
+++ qiling/debugger/qdb/arch/arch.py
@@ -9,12 +9,13 @@ class Arch:
     """
     base class for arch
     """
-    def __init__(self):
-        pass
+    def __init__(self, ql):
+        self.ql = ql
+        self.default_insn_size = 4
 
     @property
     def archtype(self):
         return self.ql.archtype
 
-    def read_insn(self, address: int , insn_size: int = 4):
-        return self.read_mem(address, insn_size)
+    def read_insn(self, address: int):
+        return self.read_mem(address, self.default_insn_size)
