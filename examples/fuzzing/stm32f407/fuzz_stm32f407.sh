c686811a8da522a55038a89c421a8ee3f11fe05e
kabeor <1597915586@qq.com>
2022-03-07 21:48:38 +0800

    correct afl++ path

--- examples/fuzzing/stm32f407/fuzz_stm32f407.sh
+++ examples/fuzzing/stm32f407/fuzz_stm32f407.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-AFL_AUTORESUME=1 AFL_PATH="/home/kabeor/Dev/qiling/examples/fuzzing/qnx_arm/AFLplusplus" PATH="$AFL_PATH:$PATH" afl-fuzz -i afl_inputs -o afl_outputs -U -- python3 ./fuzz_stm32f407.py @@
+AFL_AUTORESUME=1 AFL_PATH="$(realpath ./AFLplusplus)" PATH="$AFL_PATH:$PATH" afl-fuzz -i afl_inputs -o afl_outputs -U -- python3 ./fuzz_stm32f407.py @@
