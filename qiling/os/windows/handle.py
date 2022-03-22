105916b705d5ceb75d2633e2ba78e5906446e36f
elicn <elichn@gmail.com>
2022-03-22 12:34:36 +0200

    Refresh handle module

--- qiling/os/windows/handle.py
+++ qiling/os/windows/handle.py
@@ -4,68 +4,64 @@
 #
 # A Simple Windows Handle Simulation
 
+from typing import Any, MutableMapping, Optional
 
 class Handle:
     ID = 0xa0000000
 
-    def __init__(self, id=None, obj=None,
-                 name=None, permissions=None):
+    def __init__(self, id: Optional[int] = None, obj: Any = None, name: Optional[str] = None, permissions: Optional[int] = None):
         if id is None:
-            self.id = Handle.ID
+            id = Handle.ID
             Handle.ID += 1
-        else:
-            self.id = id
+
+        self.id = id
         self.obj = obj
         self.name = name
         self.permissions = permissions
- 
-    # rewrite "="
-    def __eq__(self, other):
+
+    # overload "=="
+    def __eq__(self, other: 'Handle'):
         return self.id == other.id
 
 
 class HandleManager:
     # IO
-    STD_INPUT_HANDLE = Handle(id=0xfffffff6)
+    STD_INPUT_HANDLE  = Handle(id=0xfffffff6)
     STD_OUTPUT_HANDLE = Handle(id=0xfffffff5)
-    STD_ERROR_HANDLE = Handle(id=0xfffffff4)
+    STD_ERROR_HANDLE  = Handle(id=0xfffffff4)
 
     # Register
-    HKEY_CLASSES_ROOT = Handle(id=0x80000000)
-    HKEY_CURRENT_CONFIG = Handle(id=0x80000005)
-    HKEY_CURRENT_USER = Handle(id=0x80000001)
+    HKEY_CLASSES_ROOT        = Handle(id=0x80000000)
+    HKEY_CURRENT_CONFIG      = Handle(id=0x80000005)
+    HKEY_CURRENT_USER        = Handle(id=0x80000001)
     HKEY_CURRENT_USER_LOCAL_SETTINGS = Handle(id=0x80000007)
-    HKEY_LOCAL_MACHINE = Handle(id=0x80000002)
-    HKEY_PERFORMANCE_DATA = Handle(id=0x80000004)
+    HKEY_LOCAL_MACHINE       = Handle(id=0x80000002)
+    HKEY_PERFORMANCE_DATA    = Handle(id=0x80000004)
     HKEY_PERFORMANCE_NLSTEXT = Handle(id=0x80000060)
-    HKEY_PERFORMANCE_TEXT = Handle(id=0x80000050)
-    HKEY_USERS = Handle(id=0x80000003)
+    HKEY_PERFORMANCE_TEXT    = Handle(id=0x80000050)
+    HKEY_USERS               = Handle(id=0x80000003)
 
     def __init__(self):
-        self.handles = {}
+        self.handles: MutableMapping[int, Handle] = {}
+
         self.append(HandleManager.STD_INPUT_HANDLE)
         self.append(HandleManager.STD_OUTPUT_HANDLE)
         self.append(HandleManager.STD_ERROR_HANDLE)
 
-    def append(self, handle):
+    def append(self, handle: Handle) -> None:
         self.handles[handle.id] = handle
 
-    def get(self, id):
+    def get(self, id: int) -> Optional[Handle]:
         return self.handles.get(id, None)
 
-    def delete(self, id):
-        key = self.handles.get(id, None)
+    def delete(self, id: int) -> None:
+        key = self.get(id)
+
         if key is not None:
             del self.handles[id]
 
-    def search(self, name):
-        for handle in self.handles.values():
-            if handle.name == name:
-                return handle
-        return None
+    def search(self, name: str) -> Optional[Handle]:
+        return next((handle for handle in self.handles.values() if handle.name == name), None)
 
-    def search_by_obj(self, obj):
-        for handle in self.handles.values():
-            if handle.obj == obj:
-                return handle
-        return None
+    def search_by_obj(self, obj: Any) -> Optional[Handle]:
+        return next((handle for handle in self.handles.values() if handle.obj == obj), None)
