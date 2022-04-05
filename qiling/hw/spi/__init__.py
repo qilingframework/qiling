9c21628b2e87eab9931127ef68822b8e17369d0d
dataisland <dataisland@outlook.com>
2022-04-05 16:57:54 +0800

    Add SAM3XA SPI Testcase

--- qiling/hw/spi/__init__.py
+++ qiling/hw/spi/__init__.py
@@ -3,6 +3,7 @@
 # Cross Platform and Multi Architecture Advanced Binary Emulation Framework
 #
 
+from .sam3xa_spi import SAM3xaSpi
 from .stm32f1xx_spi import STM32F1xxSpi
 from .stm32f4xx_spi import STM32F4xxSpi
 from .gd32vf1xx_spi import GD32VF1xxSpi
