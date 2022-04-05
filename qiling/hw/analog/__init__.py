386d75c6278842292e89a1cdeeb4f83eb4031c94
dataisland <dataisland@outlook.com>
2022-04-05 16:38:17 +0800

    Add Arduino Due PWM Test

--- qiling/hw/analog/__init__.py
+++ qiling/hw/analog/__init__.py
@@ -5,5 +5,6 @@
 
 from .sam3xa_adc import SAM3xaAdc
 from .sam3xa_dac import SAM3xaDac
+from .sam3xa_pwm import SAM3xaPwm
 from .stm32f1xx_adc import STM32F1xxAdc
 from .mk64f12_adc import MK64F12Adc
