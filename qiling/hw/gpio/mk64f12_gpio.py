15bee55110c41ad0c1bae7ac81eb7db6c671181c
dataisland <dataisland@outlook.com>
2022-04-08 16:32:49 +0800

    Add MK64F12 GPIO setting

--- qiling/hw/gpio/mk64f12_gpio.py
+++ qiling/hw/gpio/mk64f12_gpio.py
@@ -6,9 +6,10 @@
 import ctypes
 
 from qiling.hw.peripheral import QlPeripheral
+from qiling.hw.gpio.hooks import GpioHooks
 
 
-class MK64F12Gpio(QlPeripheral):
+class MK64F12Gpio(QlPeripheral, GpioHooks):
     class Type(ctypes.Structure):
         """ General Purpose Input/Output """  
         _fields_ = [
@@ -21,4 +22,44 @@ class MK64F12Gpio(QlPeripheral):
         ]
 
     def __init__(self, ql, label, intn=None):
-        super().__init__(ql, label)
+        QlPeripheral.__init__(self, ql, label)
+        GpioHooks.__init__(self, ql, 32)
+
+        self.intn = intn
+
+    @QlPeripheral.monitor()
+    def write(self, offset: int, size: int, value: int):
+        if   offset == self.struct.PSOR.offset:
+            for i in range(32):
+                self.set_pin(i)   
+        
+        elif offset == self.struct.PCOR.offset:            
+            for i in range(32):
+                self.reset_pin(i)
+
+        else:
+            self.raw_write(offset, size, value)
+
+    def set_pin(self, i):
+        self.ql.log.debug(f'[{self.label}] Set P{self.label[-1].upper()}{i}')
+        
+        if self.instance.PDDR:
+            self.instance.PDOR |= 1 << i
+        else:
+            self.instance.PDIR |= 1 << i
+        self.call_hook_set(i)
+    
+    def reset_pin(self, i):
+        self.ql.log.debug(f'[{self.label}] Reset P{self.label[-1].upper()}{i}')
+        
+        if self.instance.PDDR:
+            self.instance.PDOR &= ~(1 << i)
+        else:
+            self.instance.PDIR &= ~(1 << i)
+        self.call_hook_reset(i)
+        
+    def pin(self, index):
+        if self.instance.PDDR:
+            return (self.instance.PDOR >> index) & 1
+        else:
+            return (self.instance.PDIR >> index) & 1
