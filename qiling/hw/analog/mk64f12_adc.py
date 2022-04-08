8dabd602fd94dc7863ec8e9a38ba8957402eac7c
dataisland <dataisland@outlook.com>
2022-04-08 15:54:06 +0800

    Add MK64F12 ADC Logic

--- qiling/hw/analog/mk64f12_adc.py
+++ qiling/hw/analog/mk64f12_adc.py
@@ -6,17 +6,20 @@
 import ctypes
 
 from qiling.hw.peripheral import QlPeripheral
+from qiling.hw.const.mk64f12_adc import SC1, SC3
 
 
 class MK64F12Adc(QlPeripheral):
     class Type(ctypes.Structure):
         """ Analog-to-Digital Converter """  
         _fields_ = [
-            ("SC1" , ctypes.c_uint32), # ADC Status and Control Registers 1
+            ("SC1" , ctypes.c_uint32 * 2), # ADC Status and Control Registers 1
             ("CFG1", ctypes.c_uint32), # ADC Configuration Register 1
             ("CFG2", ctypes.c_uint32), # ADC Configuration Register 2
-            ("R"   , ctypes.c_uint32), # ADC Data Result Register
-            ("CV"  , ctypes.c_uint32), # Compare Value Registers
+            ("R1"  , ctypes.c_uint32), # ADC Data Result Register 1
+            ("R2"  , ctypes.c_uint32), # ADC Data Result Register 2
+            ("CV1" , ctypes.c_uint32), # Compare Value Registers 1
+            ("CV2" , ctypes.c_uint32), # Compare Value Registers 2
             ("SC2" , ctypes.c_uint32), # Status and Control Register 2
             ("SC3" , ctypes.c_uint32), # Status and Control Register 3
             ("OFS" , ctypes.c_uint32), # ADC Offset Correction Register
@@ -29,6 +32,7 @@ class MK64F12Adc(QlPeripheral):
             ("CLP2", ctypes.c_uint32), # ADC Plus-Side General Calibration Value Register
             ("CLP1", ctypes.c_uint32), # ADC Plus-Side General Calibration Value Register
             ("CLP0", ctypes.c_uint32), # ADC Plus-Side General Calibration Value Register
+            ("RESERVED0", ctypes.c_uint32),
             ("CLMD", ctypes.c_uint32), # ADC Minus-Side General Calibration Value Register
             ("CLMS", ctypes.c_uint32), # ADC Minus-Side General Calibration Value Register
             ("CLM4", ctypes.c_uint32), # ADC Minus-Side General Calibration Value Register
@@ -42,3 +46,24 @@ class MK64F12Adc(QlPeripheral):
         super().__init__(ql, label)
 
         self.intn = intn
+        self.instance = self.struct(
+            R1 = 0x7ff,
+            R2 = 0x7ff,
+        )
+
+    @QlPeripheral.monitor()
+    def write(self, offset, size, value):
+        if offset == self.struct.SC1.offset:
+            return
+
+        else:
+            self.raw_write(offset, size, value)
+
+        if offset == self.struct.SC3.offset:
+            if value & SC3.CAL:
+                self.instance.SC3 &= ~SC3.CAL
+                self.instance.SC1[0] |= SC1.COCO
+                self.instance.SC1[1] |= SC1.COCO
+            
+            if value & SC3.CALF:
+                self.instance.SC3 &= ~SC3.CALF
