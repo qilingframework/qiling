4aa6bde2ee9e6a4f922f593bf6fd2b42dc2671ce
dataisland <dataisland@outlook.com>
2022-04-13 22:56:49 +0800

    Add MK64F12 SPI

--- qiling/hw/spi/__init__.py
+++ qiling/hw/spi/__init__.py
@@ -7,3 +7,4 @@ from .sam3xa_spi import SAM3xaSpi
 from .stm32f1xx_spi import STM32F1xxSpi
 from .stm32f4xx_spi import STM32F4xxSpi
 from .gd32vf1xx_spi import GD32VF1xxSpi
+from .mk64f12_spi import MK64F12Spi
