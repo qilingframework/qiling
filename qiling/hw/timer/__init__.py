28e6dcf8cdce67c1217834ebd9136ff707b1b5fa
dataisland <dataisland@outlook.com>
2022-04-13 23:27:42 +0800

    Add mk64f12 timer

--- qiling/hw/timer/__init__.py
+++ qiling/hw/timer/__init__.py
@@ -11,5 +11,6 @@ from .stm32f4xx_rtc import STM32F4xxRtc
 from .stm32f4xx_tim import STM32F4xxTim
 from .gd32vf1xx_rtc import GD32VF1xxRtc
 from .gd32vf1xx_timer import GD32VF1xxTimer
+from .mk64f12_ftm import MK64F12Ftm
 from .mk64f12_osc import MK64F12Osc
 from .mk64f12_rtc import MK64F12Rtc
---
+++ qiling/hw/timer/mk64f12_ftm.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# 
+# Cross Platform and Multi Architecture Advanced Binary Emulation Framework
+#
+
+
+import ctypes
+from typing import Optional
+from qiling.core import Qiling
+
+from qiling.hw.peripheral import QlPeripheral
+from qiling.hw.timer.timer import QlTimerPeripheral
+
+
+class Control(ctypes.Structure):
+    _fields_ = [
+        ('CnSC', ctypes.c_uint32), # Channel (n) Status And Control
+        ('CnV' , ctypes.c_uint32), # Channel (n) Value
+    ]
+
+
+class MK64F12Ftm(QlTimerPeripheral):
+    class Type(ctypes.Structure):
+        """ FlexTimer Module """  
+        _fields_ = [
+            ("SC"      , ctypes.c_uint32), # Status And Control
+            ("CNT"     , ctypes.c_uint32), # Counter
+            ("MOD"     , ctypes.c_uint32), # Modulo
+            ("CONTROLS", Control * 8),
+            ("CNTIN"   , ctypes.c_uint32), # Counter Initial Value
+            ("STATUS"  , ctypes.c_uint32), # Capture And Compare Status
+            ("MODE"    , ctypes.c_uint32), # Features Mode Selection
+            ("SYNC"    , ctypes.c_uint32), # Synchronization
+            ("OUTINIT" , ctypes.c_uint32), # Initial State For Channels Output
+            ("OUTMASK" , ctypes.c_uint32), # Output Mask
+            ("COMBINE" , ctypes.c_uint32), # Function For Linked Channels
+            ("DEADTIME", ctypes.c_uint32), # Deadtime Insertion Control
+            ("EXTTRIG" , ctypes.c_uint32), # FTM External Trigger
+            ("POL"     , ctypes.c_uint32), # Channels Polarity
+            ("FMS"     , ctypes.c_uint32), # Fault Mode Status
+            ("FILTER"  , ctypes.c_uint32), # Input Capture Filter Control
+            ("FLTCTRL" , ctypes.c_uint32), # Fault Control
+            ("QDCTRL"  , ctypes.c_uint32), # Quadrature Decoder Control And Status
+            ("CONF"    , ctypes.c_uint32), # Configuration
+            ("FLTPOL"  , ctypes.c_uint32), # FTM Fault Input Polarity
+            ("SYNCONF" , ctypes.c_uint32), # Synchronization Configuration
+            ("INVCTRL" , ctypes.c_uint32), # FTM Inverting Control
+            ("SWOCTRL" , ctypes.c_uint32), # FTM Software Output Control
+            ("PWMLOAD" , ctypes.c_uint32), # FTM PWM Load
+        ]
+
+    def __init__(self, ql, label, intn=None):
+        super().__init__(ql, label)
+
+        self.intn = intn
