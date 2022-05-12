c2d657f181313c8cc950101c5392a8ba9113c710
elicn <elichn@gmail.com>
2022-05-12 22:45:34 +0300

    Skip tests the unittest way

--- tests/test_pathutils.py
+++ tests/test_pathutils.py
@@ -28,11 +28,8 @@ def posix_to_native(rootfs: str, cwd: str, path: str) -> str:
 
 
 class TestPathUtils(unittest.TestCase):
+    @unittest.skipUnless(is_posix_host, 'POSIX host only')
     def test_convert_nt_to_posix(self):
-        # test only on a POSIX host
-        if not is_posix_host:
-            self.skipTest('POSIX host only')
-
         rootfs = PurePosixPath(r'../examples/rootfs/x86_windows')
 
         expected = str(realpath(rootfs) / 'test')
@@ -74,11 +71,8 @@ class TestPathUtils(unittest.TestCase):
         self.assertEqual(expected, nt_to_native(str(rootfs), 'C:\\Windows\\System32\\drivers', '..\\..\\test'))
         self.assertEqual(expected, nt_to_native(str(rootfs), 'C:\\Windows\\System32', '..\\xxxx\\..\\test'))
 
+    @unittest.skipUnless(is_nt_host, 'NT host only')
     def test_convert_posix_to_nt(self):
-        # test only on a Windows host
-        if not is_nt_host:
-            self.skipTest('NT host only')
-
         rootfs = PureWindowsPath(r'../examples/rootfs/x86_linux')
 
         expected = str(realpath(rootfs) / 'test')
