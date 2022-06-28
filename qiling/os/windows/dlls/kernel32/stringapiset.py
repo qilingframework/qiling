618a6edb2f127a61ea6a21801b2c98f7b3092677
elicn <elichn@gmail.com>
2022-06-28 22:25:56 +0300

    Fix WideCharToMultiByte and MultiByteToWideChar

--- qiling/os/windows/dlls/kernel32/stringapiset.py
+++ qiling/os/windows/dlls/kernel32/stringapiset.py
@@ -7,6 +7,7 @@ from qiling import Qiling
 from qiling.os.windows.api import *
 from qiling.os.windows.fncc import *
 from qiling.os.windows.utils import cmp
+from qiling.os.windows.const import CP_ACP, CP_OEMCP, CP_THREAD_ACP, CP_UTF16, CP_UTF16BE, CP_ASCII, CP_UTF7, CP_UTF8
 
 # BOOL GetStringTypeW(
 #   DWORD                         dwInfoType,
@@ -43,6 +44,42 @@ def hook_GetStringTypeExA(ql: Qiling, address: int, params):
     # TODO: implement
     return 1
 
+def __encoding_name(codepage: int) -> str:
+    '''Get python encoding name from codepage value.
+
+    @see: https://docs.python.org/3.8/library/codecs.html#standard-encodings
+    '''
+
+    # mapping of special codepage values to encodings
+    encodings = {
+        # available only on windows hosts
+        CP_ACP        : 'mbcs',
+        CP_OEMCP      : 'oem',
+        CP_THREAD_ACP : 'mbcs',
+
+        # avaiable everywhere
+        CP_UTF16      : 'utf-16',
+        CP_UTF16BE    : 'utf-16be',
+        CP_ASCII      : 'ascii',
+        CP_UTF7       : 'utf-7',
+        CP_UTF8       : 'utf-8'
+    }
+
+    if codepage in encodings:
+        encname = encodings[codepage]
+
+    else:
+        encname = f'cp{codepage}'
+
+    # encoding might break on the hosting system; test it and
+    # fallback to windows-1252 ('western') if it fails
+    try:
+        _ = '\x00'.encode(encname)
+    except LookupError:
+        encname = 'cp1252'
+
+    return encname
+
 # int WideCharToMultiByte(
 #   UINT                               CodePage,
 #   DWORD                              dwFlags,
@@ -56,7 +93,7 @@ def hook_GetStringTypeExA(ql: Qiling, address: int, params):
 @winsdkapi(cc=STDCALL, params={
     'CodePage'          : UINT,
     'dwFlags'           : DWORD,
-    'lpWideCharStr'     : WSTRING, # LPCWCH
+    'lpWideCharStr'     : POINTER, # WSTRING
     'cchWideChar'       : INT,
     'lpMultiByteStr'    : LPSTR,
     'cbMultiByte'       : INT,
@@ -64,16 +101,47 @@ def hook_GetStringTypeExA(ql: Qiling, address: int, params):
     'lpUsedDefaultChar' : LPBOOL
 })
 def hook_WideCharToMultiByte(ql: Qiling, address: int, params):
-    cbMultiByte = params["cbMultiByte"]
-    s_lpWideCharStr = params["lpWideCharStr"]
+    CodePage = params['CodePage']
+    lpWideCharStr = params["lpWideCharStr"]
+    cchWideChar = params['cchWideChar']
     lpMultiByteStr = params["lpMultiByteStr"]
+    cbMultiByte = params["cbMultiByte"]
 
-    s = (s_lpWideCharStr + "\x00").encode("utf-16le")
+    if not cchWideChar:
+        # TODO: set last error
+        return 0
 
-    if cbMultiByte != 0 and lpMultiByteStr != 0:
-        ql.mem.write(lpMultiByteStr, s)
+    # -1 indicates the string is null-terminated. the string is
+    # read along with its null-terminator
+    elif cchWideChar == 0xffffffff:
+        wcstr = bytearray()
+        ch = 1
 
-    return len(s)
+        while ch != b'\x00\x00':
+            ch = ql.mem.read(lpWideCharStr, 2)
+            wcstr.extend(ch)
+
+            lpWideCharStr += 2
+
+    # read exactly cbMultiByte bytes. that may or may not include
+    # a null-terminator
+    else:
+        wcstr = ql.mem.read(lpWideCharStr, cchWideChar * 2)
+
+    decoded = wcstr.decode('utf-16le')
+
+    encname = __encoding_name(CodePage)
+    encoded = decoded.encode(encname)
+
+    if not cbMultiByte:
+        return len(encoded)
+
+    if not lpMultiByteStr:
+        return 0
+
+    ql.mem.write(lpMultiByteStr, encoded[:cbMultiByte])
+
+    return min(len(encoded), cbMultiByte)
 
 # int MultiByteToWideChar(
 #  UINT                              CodePage,
@@ -86,18 +154,53 @@ def hook_WideCharToMultiByte(ql: Qiling, address: int, params):
 @winsdkapi(cc=STDCALL, params={
     'CodePage'       : UINT,
     'dwFlags'        : DWORD,
-    'lpMultiByteStr' : WSTRING, # LPCCH
+    'lpMultiByteStr' : POINTER, # STRING,
     'cbMultiByte'    : INT,
     'lpWideCharStr'  : LPWSTR,
     'cchWideChar'    : INT
 })
 def hook_MultiByteToWideChar(ql: Qiling, address: int, params):
-    wide_str = (params['lpMultiByteStr'] + "\x00").encode('utf-16le')
+    CodePage = params['CodePage']
+    lpMultiByteStr = params['lpMultiByteStr']
+    cbMultiByte = params['cbMultiByte']
+    lpWideCharStr = params['lpWideCharStr']
+    cchWideChar = params['cchWideChar']
 
-    if params['cchWideChar'] != 0:
-        ql.mem.write(params['lpWideCharStr'], wide_str)
+    if not cbMultiByte:
+        # TODO: set last error
+        return 0
 
-    return len(wide_str)
+    # -1 indicates the string is null-terminated. the string is
+    # read along with its null-terminator
+    elif cbMultiByte == 0xffffffff:
+        mbstr = bytearray()
+        ch = 1
+
+        while ch != b'\x00':
+            ch = ql.mem.read(lpMultiByteStr, 1)
+            mbstr.extend(ch)
+
+            lpMultiByteStr += 1
+
+    # read exactly cbMultiByte bytes. that may or may not include
+    # a null-terminator
+    else:
+        mbstr = ql.mem.read(lpMultiByteStr, cbMultiByte)
+
+    # use specified code page to translate bytes into string
+    encname = __encoding_name(CodePage)
+    decoded = mbstr.decode(encname)
+
+    # this is a dry-run; just return the amount of chars
+    if not cchWideChar:
+        return len(decoded)
+
+    if not lpWideCharStr:
+        return 0
+
+    ql.mem.write(lpWideCharStr, decoded[:cchWideChar].encode('utf-16le'))
+
+    return min(len(decoded), cchWideChar)
 
 def __CompareString(ql: Qiling, address: int, params) -> int:
     lpString1 = params["lpString1"]
