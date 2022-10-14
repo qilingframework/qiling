2de22d1b8e7cb441cb25e357080c32ab47fbda29
chinggg <24590067+chinggg@users.noreply.github.com>
2022-10-14 22:49:33 +0800

    chore: add example source code for deflat

--- examples/extensions/r2/deflat_r2.py
+++ examples/extensions/r2/deflat_r2.py
@@ -15,6 +15,7 @@ from qiling.extensions.r2 import R2Qiling
 
 if __name__ == "__main__":
     # a program obfuscated by OLLVM CFF flatten, which should print 4 when argv[1] is 1
+    # see source code at examples/src/linux/fla_test.c
     ql = R2Qiling(['rootfs/x86_linux/bin/test_fla_argv', '1'], 'rootfs/x86_linux', verbose=QL_VERBOSE.DEFAULT)
     r2 = ql.r2
     # now r2 has only rbuf but no symbol info
