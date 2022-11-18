34dbadce8a9a1f81d11c7efa133d468d1538dfa2
owl <owl129@126.com>
2022-11-18 21:51:38 +0800

    Add exception handling for poll syscall

--- qiling/os/posix/syscall/poll.py
+++ qiling/os/posix/syscall/poll.py
@@ -8,24 +8,30 @@ import select
 
 def ql_syscall_poll(ql: Qiling, fds: int, nfds: int, timeout: int):
     fn_map = {}
-    p = select.poll()
-    for i in range(nfds):
-        fd = ql.mem.read_ptr(fds + i*8, 4)
-        event =  ql.mem.read_ptr(fds + i*8 + 4, 2)
-        # clear revent field
-        ql.mem.write_ptr(fds + i*8 + 6, 0, 2)
+    try:
+        p = select.poll()
+        for i in range(nfds):
+            fd = ql.mem.read_ptr(fds + i*8, 4)
+            event = ql.mem.read_ptr(fds + i*8 + 4, 2)
+            # clear revent field
+            ql.mem.write_ptr(fds + i*8 + 6, 0, 2)
 
-        ql.log.debug(f"register poll fd {fd}, event {event}")
+            ql.log.debug(f"register poll fd {fd}, event {event}")
 
-        fileno = ql.os.fd[fd].fileno()
-        fn_map[fileno] = {'idx': i, 'fd': fd}
-        p.register(fileno, event)
+            fileno = ql.os.fd[fd].fileno()
+            fn_map[fileno] = {'idx': i, 'fd': fd}
+            p.register(fileno, event)
 
-    res_list = p.poll(timeout)
-    
-    for fd, revent in res_list:
-        ql.log.debug(f"receive event on fd {fn_map[fd]['fd']}, revent {revent}")
-        idx = fn_map[fd]['idx']
-        ql.mem.write_ptr(fds + idx*8 +6, revent, 2)
+        res_list = p.poll(timeout)
+        regreturn = len(res_list)
+
+        for fd, revent in res_list:
+            ql.log.debug(f"receive event on fd {fn_map[fd]['fd']}, revent {revent}")
+            idx = fn_map[fd]['idx']
+            ql.mem.write_ptr(fds + idx*8 +6, revent, 2)
         
-    return len(res_list)
+    except Exception as e:
+        ql.log.error(f'{e} {fds=}, {nfds=}, {timeout=}')
+        regreturn = -1
+            
+    return regreturn
