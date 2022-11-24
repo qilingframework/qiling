3ce6eb8600165c2be250dfa85dea6192418c8c26
owl <owl129@126.com>
2022-11-24 21:04:26 +0800

    Move pollfd struct definition

--- qiling/os/posix/syscall/poll.py
+++ qiling/os/posix/syscall/poll.py
@@ -5,42 +5,35 @@
 
 from qiling import Qiling
 from qiling.const import QL_OS, QL_ENDIAN
-from qiling.os import struct
+from qiling.os.posix.structs import *
 import select
 import ctypes
 
 
 def ql_syscall_poll(ql: Qiling, fds: int, nfds: int, timeout: int):
-    base = struct.BaseStructEL if ql.arch.endian == QL_ENDIAN.EL else struct.BaseStructEB
-
-    class Pollfd(base):
-        _fields_ = (
-            ('fd', ctypes.c_int32),
-            ('events', ctypes.c_int16),
-            ('revents', ctypes.c_int16)
-        )
+    pollfd = make_pollfd(ql.arch.bits, ql.arch.endian)
 
     if ql.host.os == QL_OS.LINUX:
         fn_map = {}
         try:
             p = select.poll()
             for i in range(nfds):
-                with Pollfd.ref(ql.mem, fds + ctypes.sizeof(Pollfd) * i) as pollfd:
+                with pollfd.ref(ql.mem, fds + ctypes.sizeof(pollfd) * i) as pf:
                     # clear revents field
-                    pollfd.revents = 0
+                    pf.revents = 0
 
-                    ql.log.debug(f"register poll fd {pollfd.fd}, event {pollfd.events}")
-                    fileno = ql.os.fd[pollfd.fd].fileno()
+                    ql.log.debug(f"register poll fd {pf.fd}, event {pf.events}")
+                    fileno = ql.os.fd[pf.fd].fileno()
                     fn_map[fileno] = i
-                    p.register(fileno, pollfd.events)
+                    p.register(fileno, pf.events)
 
             res_list = p.poll(timeout)
             regreturn = len(res_list)
 
             for fn, revent in res_list:
-                with Pollfd.ref(ql.mem, fds + ctypes.sizeof(Pollfd) * fn_map[fn]) as pollfd:
-                    ql.log.debug(f"receive event on fd {pollfd.fd}, revent {revent}")
-                    pollfd.revents = revent
+                with pollfd.ref(ql.mem, fds + ctypes.sizeof(pollfd) * fn_map[fn]) as pf:
+                    ql.log.debug(f"receive event on fd {pf.fd}, revent {revent}")
+                    pf.revents = revent
         except Exception as e:
             ql.log.error(f'{e} {fds=}, {nfds=}, {timeout=}')
             regreturn = -1
