b7a68d7949f9e4dba5d93617e56e5467fd6933f6
me <me>
2023-02-28 13:59:47 -0500

    brought back the list comprehension (thanks @elicn) and reimplemented history functions to return capstone objects because they are nicer to look at than just integers (addresses)

--- qiling/extensions/coverage/formats/history.py
+++ qiling/extensions/coverage/formats/history.py
@@ -2,15 +2,18 @@ from typing import List
 from qiling import Qiling
 from qiling.core_hooks_types import HookRet
 import re
+from capstone import CsInsn, Cs
 
 class History:
     history_hook_handle: HookRet = None
-    history: List[int] = []
+    history: List[CsInsn] = []
     ql: Qiling
+    md: Cs
 
     def __init__(self, ql: Qiling) -> None:
         self.ql = ql
         self.track_block_coverage()
+        self.md = ql.arch.disassembler
 
     def clear_history(self) -> None:
         """Clears the current state of the history 
@@ -37,7 +40,13 @@ class History:
             self.clear_hooks()
         
         def __hook_block(ql, address, size):
-            self.history.append(address)
+            #0x10 is way more than enough bytes to grab a single instruction
+            ins_bytes = ql.mem.read(address, 0x10)
+            try:                
+                self.history.append(next(self.md.disasm(ins_bytes, address)))
+            except StopIteration: 
+                #if this ever happens, then the unicorn/qiling is going to crash because it tried to execute an instruction that it cant, so we are just not going to do anything
+                pass
 
         self.history_hook_handle = self.ql.hook_block(__hook_block)
 
@@ -49,20 +58,26 @@ class History:
         """
         if self.history_hook_handle:
             self.clear_hooks()
-        
+
         def __hook_block(ql, address, size):
-            self.history.append(address)
+            #0x10 is way more than enough bytes to grab a single instruction
+            ins_bytes = ql.mem.read(address, 0x10)
+            try:                
+                self.history.append(next(self.md.disasm(ins_bytes, address)))
+            except StopIteration: 
+                #if this ever happens, then the unicorn/qiling is going to crash because it tried to execute an instruction that it cant, so we are just not going to do anything
+                pass
 
         self.history_hook_handle = self.ql.hook_code(__hook_block)
 
-    def get_ins_only_lib(self, libs: List[str]) -> List[int]:
+    def get_ins_only_lib(self, libs: List[str]) -> List[CsInsn]:
         """Returns a list of addresses that have been executed that are only in mmaps for objects that match the regex of items in the list
         
         Args:
             libs (List[str]): A list of regex strings to match against the library names in the memory maps
 
         Returns:
-            List[int]: A list of addresses that have been executed and in the memory maps that match the regex
+            List[capstone.CsInsn]: A list of CsInsn that have been executed and are only in the memory maps that match the regex
 
         Examples:
             >>> history.get_ins_only_lib([".*libc.so.*", ".*libpthread.so.*"])
@@ -70,40 +85,30 @@ class History:
 
         executable_maps = self.get_regex_matching_exec_maps(libs)
         
-        return [x for x in self.history if any([x >= start and x <= end for start, end, _, _, _ in executable_maps])]
+        return [x for x in self.history if any([x.address >= start and x.address <= end for start, end, _, _, _ in executable_maps])]
 
-    def get_ins_exclude_lib(self, libs: list) -> List:
+    def get_ins_exclude_lib(self, libs: List[str]) -> List[CsInsn]:
         '''Returns a list of history instructions that are not in the libraries that match the regex in the libs list
         
         Args:
-            libs (List): A list of regex strings to match against the library names in the memory maps
+            libs (List[str]): A list of regex strings to match against the library names in the memory maps
         
         Returns:
-            List: A list of addresses that have been executed and are not in the memory maps that match the regex
+            List[capstone.CsInsn]: A list of CsInsn that have been executed and are not in the memory maps that match the regex
 
         Examples:
             >>> history.get_ins_exclude_lib([".*libc.so.*", ".*libpthread.so.*"])
         '''
 
         executable_maps = self.get_regex_matching_exec_maps(libs)
-        instructions = []
-        for h in self.history:
-            save = True
-            for start, end, _, _, _ in executable_maps:
-                if h >= start and h <= end:
-                    save = False
-                    break
-
-            if save:
-                instructions.append(h)
-        return instructions 
+        return [h for h in self.history if not any(start <= h.address <= end for start, end, _, _, _ in executable_maps)]
 
     
-    def get_mem_map_from_addr(self, ins: int) -> tuple:
+    def get_mem_map_from_addr(self, ins) -> tuple:
         '''Returns the memory map that contains the instruction
 
         Args:
-            ins (int): The instruction address to search for
+            ins: The instruction address to search for, can be either an int or a capstone.CsInsn
 
         Returns:
             tuple: A tuple that contains the memory map that contains the instruction
@@ -112,6 +117,11 @@ class History:
         Examples: 
             >>> history.get_mem_map_from_addr(0x7ffff7dd1b97)
         '''
+
+        if isinstance(ins, CsInsn):
+            ins = ins.address
+
+        assert isinstance(ins, int)
 
         #get the memory map that contains the instruction
         mem_map = [x for x in self.ql.mem.get_mapinfo() if x[0] <= ins and x[1] >= ins]
@@ -122,13 +132,13 @@ class History:
         # i sure hope theres not more than one map that contains the instruction lol
         return mem_map[0]
 
-    def get_regex_matching_exec_maps(self, libs: List) -> List:
+    def get_regex_matching_exec_maps(self, libs: List[str]) -> List:
         '''Returns a list of tuples for current mmaps whose names match the regex of libs in the list
         
         This is a wrapper around ql.mem.get_mapinfo() and just filters the results by the regex of the library names and also only returns maps that are executable
 
         Args:
-            libs (List): A list of regex strings to match against the library names in the memory maps
+            libs (List[str]): A list of regex strings to match against the library names in the memory maps
 
         Returns:
             List: A list of tuples that match the regex and are executable
