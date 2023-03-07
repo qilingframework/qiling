2a78192c028436b54adbc2d933ac5bc857d6aeea
me <me>
2023-03-06 19:05:19 -0500

    cleaned up some pylint stuff, consolidated the arm and other arch block callback

--- qiling/extensions/coverage/formats/history.py
+++ qiling/extensions/coverage/formats/history.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import List, Tuple, TYPE_CHECKING, Union
+from typing import List, Tuple, TYPE_CHECKING, Union, Optional, Any
 
 if TYPE_CHECKING:
     from qiling import Qiling
@@ -7,12 +7,11 @@ if TYPE_CHECKING:
     from capstone import Cs
 
 import re
-from functools import partial
 from capstone import CsInsn
-from qiling.const import QL_ARCH
+
 
 class History:
-    history_hook_handle: HookRet = None
+    history_hook_handle: HookRet
     history: List[CsInsn] = []
     ql: Qiling
     md: Cs
@@ -39,39 +38,25 @@ class History:
 
         self.ql.hook_del(self.history_hook_handle)
 
-    #a python function hook block to be used in both track_block_coverage and track_instruction_coverage
-    def __hook_block(self, ql: Qiling, address: int, size: int):
+    def __hook_block(self, ql: Qiling, address: int, size: int, *context: Any) -> Any:
         '''
         The unicorn block/instruction hook function for the track_block_coverage and track_instruction_coverage functions. This just give us a way to append capstone objects to the history list
-        
-        NOTE: this is not supposed to be used in arm architecture, use __hook_block_arm instead (if you use this, things are going to be hella slow)
         '''
-        #0x10 is way more than enough bytes to grab a single instruction
+
+        # get the current state of the thumb mode
+        
+        if self.arm_is_thumb is not getattr(ql.arch, "is_thumb", False):
+            # the thumb mode has changed, so we need to update the disassembler
+            self.arm_is_thumb = not self.arm_is_thumb
+            self.md = self.ql.arch.disassembler
+
+        # 0x10 is way more than enough bytes to grab a single instruction
         ins_bytes = ql.mem.read(address, 0x10)
         try:
             self.history.append(next(self.md.disasm(ins_bytes, address)))
         except StopIteration:
-            #if this ever happens, then the unicorn/qiling is going to crash because it tried to execute an instruction that it cant, so we are just not going to do anything
-            pass
-
-    # in arm we have to worry about thumb mode, so we have our own hook function
-    def __hook_block_arm(self, ql: Qiling, address: int, size: int):
-        '''
-        The unicorn block/instruction hook function for the track_block_coverage and track_instruction_coverage functions. This just give us a way to append capstone objects to the history list
-        '''
-
-        #get the current state of the thumb mode
-        if self.arm_is_thumb != ql.arch.is_thumb:
-            #the thumb mode has changed, so we need to update the disassembler
-            self.arm_is_thumb = ql.arch.is_thumb
-            self.md = self.ql.arch.disassembler
-
-        #0x10 is way more than enough bytes to grab a single instruction
-        ins_bytes = ql.mem.read(address, 0x10)
-        try:                
-            self.history.append(next(self.md.disasm(ins_bytes, address)))
-        except StopIteration: 
-            #if this ever happens, then the unicorn/qiling is going to crash because it tried to execute an instruction that it cant, so we are just not going to do anything
+            # if this ever happens, then the unicorn/qiling is going to crash because it tried to execute 
+            # an instruction that it cant, so we are just not going to do anything
             pass
 
     def track_block_coverage(self) -> None:
@@ -80,14 +65,10 @@ class History:
         Returns:
             None
         """
-        if self.history_hook_handle:
+        if getattr(self, 'history_hook_handle', None):
             self.clear_hooks()
 
-        hook = self.__hook_block
-        if self.ql.arch.type == QL_ARCH.ARM:
-            hook = self.__hook_block_arm
-
-        self.history_hook_handle = self.ql.hook_block(hook)
+        self.history_hook_handle = self.ql.hook_block(self.__hook_block)
 
     def track_instruction_coverage(self) -> None:
         """Configures the history plugin to track all of the instructions that are executed. Removes any existing hooks
@@ -95,14 +76,10 @@ class History:
         Returns:
             None
         """
-        if self.history_hook_handle:
+        if getattr(self, 'history_hook_handle', None):
             self.clear_hooks()
 
-        hook = self.__hook_block
-        if self.ql.arch.type == QL_ARCH.ARM:
-            hook = self.__hook_block_arm
-
-        self.history_hook_handle = self.ql.hook_code(hook)
+        self.history_hook_handle = self.ql.hook_code(self.__hook_block)
 
     def get_ins_only_lib(self, libs: List[str]) -> List[CsInsn]:
         """Returns a list of addresses that have been executed that are only in mmaps for objects that match the regex of items in the list
@@ -137,15 +114,14 @@ class History:
         executable_maps = self.get_regex_matching_exec_maps(libs)
         return [h for h in self.history if not any(start <= h.address <= end for start, end, _, _, _ in executable_maps)]
 
-    
-    def get_mem_map_from_addr(self, ins: Union[int, CsInsn]) -> Tuple:
+    def get_mem_map_from_addr(self, ins: Union[int, CsInsn]) -> Optional[Tuple]:
         '''Returns the memory map that contains the instruction
 
         Args:
             ins (Union[int, CsInsn]): The instruction address to search for, can be either an int or a capstone.CsInsn
 
         Returns:
-            tuple: A tuple that contains the memory map that contains the instruction
+            Optional[Tuple]: A tuple that contains the memory map that contains the instruction
                 this tuple is in the format of (start_addr, end_addr, perms, name, path)
 
         Examples: 
@@ -159,16 +135,17 @@ class History:
 
         return next((x for x in self.ql.mem.get_mapinfo() if x[0] <= ins and x[1] >= ins), None)
 
-    def get_regex_matching_exec_maps(self, libs: List[str]) -> List:
+    def get_regex_matching_exec_maps(self, libs: List[str]) -> List[Tuple]:
         '''Returns a list of tuples for current mmaps whose names match the regex of libs in the list
         
-        This is a wrapper around ql.mem.get_mapinfo() and just filters the results by the regex of the library names and also only returns maps that are executable
+        This is a wrapper around ql.mem.get_mapinfo() and just filters the results by the regex of the library names 
+        and also only returns maps that are executable
 
         Args:
             libs (Union[str, Collection[str]]): A list of regex strings to match against the library names in the memory maps
 
         Returns:
-            List: A list of tuples that match the regex and are executable
+            List[Tuple]: A list of tuples that match the regex and are executable
 
         Examples:
             >>> history.get_regex_matching_exec_maps([".*libc.so.*", ".*libpthread.so.*"])
@@ -184,9 +161,7 @@ class History:
 
         # filter the list of tuples 
         # so that we return only the ones where the library name matches the regex
-        regex_matching_libs = [x for x in self.ql.mem.get_mapinfo() if any([r.match(x[3]) for r in regex])]
+        regex_matching_libs = [x for x in self.ql.mem.get_mapinfo() if any(r.match(x[3]) for r in regex)]
 
         # filter viable_libs for items that have the executable bit set
-        executable_maps = [x for x in regex_matching_libs if 'x' in x[2]]
-
-        return executable_maps
+        return [x for x in regex_matching_libs if 'x' in x[2]]
