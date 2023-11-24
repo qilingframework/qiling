edbbbec5f8c54745b509b1289cc079a021822d80
Ninja3047 <1284324+Ninja3047@users.noreply.github.com>
2023-11-24 02:41:58 -0500

    fix overflow with wait options
    python expects options to be a signed 32bit integer

--- qiling/os/posix/syscall/wait.py
+++ qiling/os/posix/syscall/wait.py
@@ -11,6 +11,8 @@ from qiling.os.posix.const import ECHILD
 def ql_syscall_wait4(ql: Qiling, pid: int, wstatus: int, options: int, rusage: int):
     # convert to signed (pid_t is 32bit)
     pid = ql.unpack32s(ql.pack32(pid))
+    # python expects options to be a signed 32bit int
+    options = ql.unpack32s(ql.pack32(options))
 
     try:
         spid, status, _ = os.wait4(pid, options)
