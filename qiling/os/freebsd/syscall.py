8a3b157c6e2bb1c6ac103d762b70a541b6db71f6
elicn <elichn@gmail.com>
2024-02-21 02:51:40 +0200

    Support reading and writing signed values through existing accessor methods

--- qiling/os/freebsd/syscall.py
+++ qiling/os/freebsd/syscall.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
-# 
+#
 # Cross Platform and Multi Architecture Advanced Binary Emulation Framework
 #
 
-
-from qiling.arch.x86_const import *
-from qiling.os.freebsd.const import *
-from qiling.os.posix.syscall.unistd import ql_syscall_getcwd
-from qiling.os.posix.syscall.stat import ql_syscall_newfstatat
+import ctypes
 from datetime import datetime
 from math import floor
-import ctypes
+
+from qiling import Qiling
+from qiling.arch.x86_const import *
+from qiling.os.freebsd.const import *
+from qiling.os.posix.const import ENOENT
+from qiling.os.posix.syscall.unistd import ql_syscall_getcwd
+from qiling.os.posix.syscall.stat import ql_syscall_newfstatat
 
 class timespec(ctypes.Structure):
     _fields_ = [
@@ -59,20 +61,21 @@ def ql_syscall___getcwd(ql, path_buff, path_buffsize, *args, **kw):
 def ql_syscall_fstatat(ql, newfstatat_dirfd, newfstatat_path, newfstatat_buf_ptr, newfstatat_flag, *args, **kw):
     return ql_syscall_newfstatat(ql, newfstatat_dirfd, newfstatat_path, newfstatat_buf_ptr, newfstatat_flag, *args, **kw)
 
-def ql_syscall___sysctl(ql, name, namelen, old, oldlenp, new_arg, newlen):
+def ql_syscall___sysctl(ql: Qiling, name: int, namelen: int, old: int, oldlenp: int, new_arg: int, newlen: int):
     ql.log.debug("__sysctl(name: 0x%x, namelen: 0x%x, old: 0x%x, oldlenp: 0x%x, new: 0x%x, newlen: 0x%x)" % (
         name, namelen, old, oldlenp, new_arg, newlen
     ))
-    vecs = []
-    for i in range(namelen):
-        vecs.append(ql.unpack32s(ql.mem.read(name + i*4, 4)))
+
+    vecs = [ql.mem.read_ptr(name + i * 4, 4, signed=True) for i in range(namelen)]
+
     ql.log.debug(f"__sysctl vectors: {vecs}")
     if vecs[0] == CTL_SYSCTL:
         if vecs[1] == CTL_SYSCTL_NAME2OID:
             # Write oid to old and oldlenp
-            sysctl_name = ql.mem.string(new_arg)
+            sysctl_name = ql.os.utils.read_cstring(new_arg)
             out_vecs = []
             out_len = 0
+
             # TODO: Implement oid<-->name as many as possible from FreeBSD source.
             #       Search SYSCTL_ADD_NODE etc.
             if sysctl_name == "hw.pagesizes":
@@ -80,16 +83,21 @@ def ql_syscall___sysctl(ql, name, namelen, old, oldlenp, new_arg, newlen):
                 out_len = 2
             else:
                 ql.log.warning("Unknown oid name!")
+
             for i, v in enumerate(out_vecs):
-                ql.mem.write(old + 4*i, ql.pack32s(v))
-            ql.mem.write(oldlenp, ql.pack32s(out_len))
-        return 2 # ENOENT
+                ql.mem.write_ptr(old + i * 4, v, 32, signed=True)
+
+            ql.mem.write_ptr(oldlenp, out_len, 32, signed=True)
+
+        return -ENOENT
+
     if vecs[0] == CTL_KERN:
         if vecs[1] == KERN_OSRELDATE:
-            if old == 0 or  oldlenp == 0:
+            if old == 0 or oldlenp == 0:
                 return -1
+
             # Ignore oldlenp check.
-            ql.mem.write(old, ql.pack32s(FREEBSD_OSRELDATE))
+            ql.mem.write_ptr(old, FREEBSD_OSRELDATE, 32, signed=True)
+
             return 0
     return 0
-    
