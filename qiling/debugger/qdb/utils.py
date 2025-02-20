273d79d038938c587ac37307c4d0a069233486e3
elicn <elichn@gmail.com>
2025-02-20 17:25:50 +0200

    Import necessary types

--- qiling/debugger/qdb/utils.py
+++ qiling/debugger/qdb/utils.py
@@ -4,7 +4,7 @@
 #
 
 from __future__ import annotations
-from typing import Callable, Optional, Mapping, Tuple
+from typing import TYPE_CHECKING, Callable, Dict, Mapping, Tuple, Type
 
 from capstone import CsInsn
 
