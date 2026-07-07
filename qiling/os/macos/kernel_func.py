b1125506393e36efa6f52a6c59510f8b8fab7bb3
ciciplusplus <ciciplusplus@gmail.com>
2026-07-07 22:08:25 +0200

    Increase commpage size to 2 pages for x86 macos

--- qiling/os/macos/kernel_func.py
+++ qiling/os/macos/kernel_func.py
@@ -27,7 +27,7 @@ def map_commpage(ql):
         addr_size = 0x1000
     elif ql.arch.type == QL_ARCH.X86:
         addr_base = X86_COMM_PAGE_START_ADDRESS
-        addr_size = 0x1000
+        addr_size = 0x2000
     else:
         raise NotImplementedError
     ql.mem.map(addr_base, addr_size, info="[commpage]")
