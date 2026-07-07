d60e1658367fd8df529118c9c4a984d379a79a28
ciciplusplus <ciciplusplus@gmail.com>
2026-07-08 00:19:44 +0200

    Cleanup

--- qiling/os/macos/map_syscall.py
+++ qiling/os/macos/map_syscall.py
@@ -60,8 +60,6 @@ x86_syscall_table = {
     0xffffffe5: 'thread_self_trap',
     0xffffffe6: 'mach_reply_port',
     # my stuff...
-    0x0000fffb: 'my_OSAtomicCompareAndSwap32',
-    0x0000fffc: 'my_OSAtomicCompareAndSwap64',
     0x0000fffd: 'my_mach_absolute_time',
     0x0000fffe: 'my_memcpy',
     0x0000ffff: 'my_bzero',
